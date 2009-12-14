a044abc4ccc580284d81603126ed327a3a896a85
Pascal Monnier <plmonnier@gmail.com>
2009-12-14 14:11:17 +0100

    - Ajout de commentaire dasn fwMath - Ajout des license block manquants.
    --HG--
    branch : fw4spl_0.5

--- SrcLib/core/fwMath/include/fwMath/MeshFunctions.hpp
+++ SrcLib/core/fwMath/include/fwMath/MeshFunctions.hpp
@@ -1,3 +1,9 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
 #ifndef _FWMATH_MESHFUNCTIONS_HPP_
 #define _FWMATH_MESHFUNCTIONS_HPP_
 
@@ -11,13 +17,21 @@
 
 namespace fwMath
 {
+
+/**
+ * @brief
+ */
 FWMATH_API bool IsInclosedVolume(const fwVertexPosition _vertex, const fwVertexIndex _vertexIndex, const fwVec3d &_P );
 
-//intersection du triangle composé des vertex vert1, vert2, vert3  avec la droite parallèle à (Oz) passant par P.
-// En cas de point d'intersection, la méthode renvoie la valeur true (sinon, false) et t correspond à la distance orientée de P au point d'intersction.
-// u, et v correspondent aux coordonnées  du point d'intersection dans le repère du triangle (O,u,v). Avec :
-// O = vert1. u =  vert2 - O. v =  vert3 - O.
-//"Fast, Minimum Storage Ray/Triangle Intersection", Tomas Möller, Ben Trumbore.
+/**
+ * @brief Compute the intersection between triangle(define by threes vertex vert1, vert2, vert3) and the Oz parallel line and cross by the point P.
+ * The function return true if intersection is found (false otherwise).
+ * t is the oriented distance of P to the intersection point.
+ * u and v is the coordinate of the intersection point in the (O, u, v) triangle coordinate system with :
+ * O = vert1. u =  vert2 - O. v =  vert3 - O.
+ * "Fast, Minimum Storage Ray/Triangle Intersection", Tomas Möller, Ben Trumbore.
+ *
+ */
 FWMATH_API bool intersect_triangle(fwVec3d _orig, fwVec3d _dir, fwVec3d _vert0, fwVec3d _vert1, fwVec3d _vert2, double &_t, double &_u, double &_v);
 }
 
