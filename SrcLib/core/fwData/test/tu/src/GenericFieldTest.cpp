328854bb428ac1d2acd989fbea986e138123965f
Nicolas Philipps <nicolasph@gmail.com>
2010-02-02 10:26:54 +0100

    Updated license headers
    --HG--
    branch : fw4spl_0.6

--- SrcLib/core/fwData/test/tu/src/GenericFieldTest.cpp
+++ SrcLib/core/fwData/test/tu/src/GenericFieldTest.cpp
@@ -1,7 +1,7 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009.
- * Distributed under the terms of the GNU Lesser General Public License (LGPL) as 
- * published by the Free Software Foundation.  
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
 
 #include <fwData/GenericField.hpp>
