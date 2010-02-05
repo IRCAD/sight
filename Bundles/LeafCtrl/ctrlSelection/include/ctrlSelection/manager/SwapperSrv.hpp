fab69fe846662d2ec11e2e1ef6559a0dfabf22f4
Julien Waechter <julien.waechter@gmail.com>
2010-02-05 18:32:33 +0100

    * Added : new Swapper Manager service
    --HG--
    branch : fw4spl_0.6

---
+++ Bundles/LeafCtrl/ctrlSelection/include/ctrlSelection/manager/SwapperSrv.hpp
@@ -0,0 +1,92 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef _CTRLSELECTION_MANAGER_SWAPPER_HPP_
+#define _CTRLSELECTION_MANAGER_SWAPPER_HPP_
+
+#include <fwRuntime/ConfigurationElement.hpp>
+#include <fwServices/Iservice.hpp>
+#include "ctrlSelection/config.hpp"
+#include "ctrlSelection/IManagerSrv.hpp"
+
+namespace ctrlSelection
+{
+
+namespace manager
+{
+
+/**
+ * @class	SwapperSrv.
+ * @author IRCAD (Research and Development Team).
+
+ * @date	2007-2009.
+ */
+class CTRLSELECTION_CLASS_API SwapperSrv : public ::ctrlSelection::IManagerSrv
+{
+
+public :
+
+	fwCoreServiceClassDefinitionsMacro ( (SwapperSrv)(::ctrlSelection::IManagerSrv::Baseclass) ) ;
+
+	/// Constructor.  Do nothing.
+	CTRLSELECTION_API SwapperSrv() throw() ;
+
+	/// Destructor. Do nothing.
+	CTRLSELECTION_API virtual ~SwapperSrv() throw() ;
+
+	/// Implements starting method derived from IService. Do nothing.
+	CTRLSELECTION_API virtual void starting()  throw ( ::fwTools::Failed );
+
+	/// Implements stopping method derived from IService. Do nothing.
+	CTRLSELECTION_API virtual void stopping()  throw ( ::fwTools::Failed );
+
+	/// Implements configuring method derived from IService. Do nothing.
+	CTRLSELECTION_API virtual void configuring()  throw ( ::fwTools::Failed );
+
+	/// Implements reconfiguring method derived from IService. Do nothing.
+	CTRLSELECTION_API virtual void reconfiguring()  throw ( ::fwTools::Failed );
+
+	/// Implements updating method derived from IService. Do nothing.
+	CTRLSELECTION_API virtual void updating() throw ( ::fwTools::Failed );
+
+	/// Implements info method derived from IService. Print classname.
+	CTRLSELECTION_API virtual void info( std::ostream &_sstream );
+
+	CTRLSELECTION_API virtual void updating( ::fwServices::ObjectMsg::csptr _msg ) throw ( ::fwTools::Failed );
+
+protected:
+
+	typedef ::fwRuntime::ConfigurationElement::sptr ConfigurationType;
+	typedef std::string ObjectIdType;
+	typedef std::vector<SubService> SubServicesVecType;
+	typedef std::map< ObjectIdType, SubServicesVecType > SubServicesMapType ;
+
+	class SubService
+	{
+	public:
+	    SubService()
+	    { }
+	    ~SubService()
+	    { }
+	    SPTR (::fwServices::IService) getService()
+	                { return m_service.lock(); }
+
+	    ConfigurationType m_config;
+	    WPTR(::fwServices::IService) m_service;
+	};
+
+    void configureObject( ConfigurationType conf );
+
+private:
+	ConfigurationType m_managerConfiguration;
+	SubServicesMapType m_objectsSubServices;
+
+};
+
+} // manager
+} // ctrlSelection
+
+#endif // _CTRLSELECTION_MANAGER_SWAPPER_HPP_
---
+++ Bundles/LeafCtrl/ctrlSelection/src/ctrlSelection/manager/SwapperSrv.cpp
@@ -0,0 +1,234 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#include <boost/foreach.hpp>
+
+#include <fwTools/UUID.hpp>
+
+#include <fwServices/helper.hpp>
+#include <fwServices/macros.hpp>
+#include <fwServices/op/Add.hpp>
+
+#include <fwComEd/CompositeMsg.hpp>
+#include <fwData/Composite.hpp>
+
+#include "ctrlSelection/manager/SwapperSrv.hpp"
+
+namespace ctrlSelection
+{
+
+namespace manager
+{
+
+//-----------------------------------------------------------------------------
+
+REGISTER_SERVICE( ::ctrlSelection::IManagerSrv, ::ctrlSelection::manager::SwapperSrv, ::fwData::Composite ) ;
+
+//-----------------------------------------------------------------------------
+
+SwapperSrv::SwapperSrv() throw()
+{
+    addNewHandlingEvent( ::fwComEd::CompositeMsg::MODIFIED_FIELDS );
+}
+
+//-----------------------------------------------------------------------------
+
+SwapperSrv::~SwapperSrv() throw()
+{}
+
+//-----------------------------------------------------------------------------
+
+void SwapperSrv::updating( ::fwServices::ObjectMsg::csptr message ) throw ( ::fwTools::Failed )
+{
+    SLM_TRACE_FUNC();
+
+    ::fwComEd::CompositeMsg::csptr compositeMsg = ::fwComEd::CompositeMsg::dynamicConstCast(message);
+
+    if(compositeMsg && compositeMsg->hasEvent( ::fwComEd::CompositeMsg::MODIFIED_FIELDS ) )
+    {
+        std::vector< std::string > objectIds = compositeMsg->getEventModifiedFields();
+        std::vector< std::string >::iterator iter;
+
+        SLM_ASSERT ("No services configuration!", m_managerConfiguration );
+
+        BOOST_FOREACH( std::string objectId, objectIds)
+        {
+            BOOST_FOREACH( ConfigurationType cfg, m_managerConfiguration->find("object", "id", objectId))
+            {
+                this->configureObject(cfg);
+            }
+        }
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+void SwapperSrv::starting()  throw ( ::fwTools::Failed )
+{
+    SLM_TRACE_FUNC();
+
+    for( SubServicesMapType::iterator iterMap = m_objectsSubServices.begin(); iterMap != m_objectsSubServices.end(); ++iterMap )
+    {
+        SubServicesVecType subServices = iterMap->second;
+        for( SubServicesVecType::iterator iterVect = subServices.begin(); iterVect != subServices.end(); ++iterVect )
+        {
+            SubService &subSrv = *iterVect;
+            SLM_ASSERT("SubService expired !", subSrv.getService() );
+            subSrv.getService()->start();
+        }
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+void SwapperSrv::stopping()  throw ( ::fwTools::Failed )
+{
+    SLM_TRACE_FUNC();
+
+    for( SubServicesMapType::iterator iterMap = m_objectsSubServices.begin(); iterMap != m_objectsSubServices.end(); ++iterMap )
+    {
+        SubServicesVecType subServices = iterMap->second;
+        for( SubServicesVecType::iterator iterVect = subServices.begin(); iterVect != subServices.end(); ++iterVect )
+        {
+            SubService &subSrv = *iterVect;
+            SLM_ASSERT("SubService expired !", subSrv.getService() );
+            subSrv.getService()->stop();
+            ::fwServices::erase(subSrv.getService());
+            subSrv.m_service.reset();
+        }
+    }
+    m_objectsSubServices.clear();
+}
+
+//-----------------------------------------------------------------------------
+
+void SwapperSrv::configuring()  throw ( ::fwTools::Failed )
+{
+    SLM_TRACE_FUNC();
+    std::vector < ConfigurationType > vectConfig = m_configuration->find("config");
+    SLM_ASSERT("Missing <config> tag!", !vectConfig.empty());
+    m_managerConfiguration = vectConfig.at(0);
+
+    ::fwRuntime::ConfigurationElementContainer::Iterator iter;
+    for (iter = m_managerConfiguration->begin() ; iter != m_managerConfiguration->end() ; ++iter)
+    {
+        if ((*iter)->getName() == "object")
+        {
+            this->configureObject( *iter );
+        }
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+void SwapperSrv::configureObject( ConfigurationType conf )
+{
+    SLM_ASSERT("Missing <object> tag!", conf->getName() == "object");
+    ::fwData::Composite::sptr composite = this->getObject< ::fwData::Composite >() ;
+
+    const std::string objectId      = conf->getAttributeValue("id");
+    const std::string objectType    = conf->getAttributeValue("type");
+
+    SLM_ASSERT( "'objectId' required attribute missing or empty", !objectId.empty() );
+    SLM_ASSERT( "'type' required attribute missing or empty", !objectType.empty() );
+
+    const unsigned int compositeObjectCount = composite->getRefMap().count(objectId);
+
+    OSLM_TRACE_IF(objectId << " not found in composite.", !(compositeObjectCount == 1));
+
+    ::fwTools::Object::sptr object;
+    if (compositeObjectCount)
+    {
+        object = ::fwTools::Object::dynamicCast(composite->getRefMap()[objectId]);
+    }
+
+    // Object's not registered in manager and exists in Composite
+    if ( m_objectsSubServices.count(objectId) == 0 && object )
+    {
+        OSLM_ASSERT("ObjectType "<<objectType<<" does not match ObjectType in Composite "<<object->getClassname(), objectType == object->getClassname());
+        SubServicesVecType subVecSrv;
+        BOOST_FOREACH( ConfigurationType cfg, conf->find("service"))
+        {
+            ::fwServices::IService::sptr srv = ::fwServices::add( object, cfg );
+            OSLM_ASSERT("Instantiation Service failed on object "<<objectId, srv);
+            srv->configure();
+            SubService subSrv;
+            subSrv.m_config = *(cfg->begin());
+            subSrv.m_service = srv;
+            subVecSrv.push_back(subSrv);
+         }
+        m_objectsSubServices[objectId] = subVecSrv;
+    }
+    // Object's already registered in manager
+    else if(m_objectsSubServices.count(objectId) == 1)
+    {
+        // Object exists in Composite, so we swap all associated subServices
+        if (object)
+        {
+            SubServicesVecType subServices = m_objectsSubServices[objectId];
+            for( SubServicesVecType::iterator iter = subServices.begin(); iter != subServices.end(); ++iter )
+            {
+                SubService &subSrv = *iter;
+
+                SLM_ASSERT("SubService expired !", subSrv.getService() );
+                OSLM_ASSERT( ::fwTools::UUID::get(subSrv.getService()) <<  " is not started ", subSrv.getService()->isStarted());
+
+                OSLM_TRACE ("Swapping subService " << subSrvId << " on "<< objectId );
+                if(subSrv.getService()->getObject() != object)
+                {
+                    subSrv.getService()->swap(object);
+                }
+                else
+                {
+                    OSLM_WARN( ::fwTools::UUID::get(subSrv.getService())
+                            << "'s object already is '"
+                            << subSrv.getService()->getObject()->getUUID()
+                            << "', no need to swap");
+                }
+            }
+        }
+        //Object was removed from Composite, so we remove all associated subServices
+        else
+        {
+            SubServicesVecType subServices = m_objectsSubServices[objectId];
+            for( SubServicesVecType::iterator iter = subServices.begin(); iter != subServices.end(); ++iter )
+            {
+                SubService &subSrv = *iter;
+                SLM_ASSERT("SubService expired !", subSrv.getService() );
+                OSLM_ASSERT( ::fwTools::UUID::get(subSrv.getService()) <<  " is not started ", subSrv.getService()->isStarted());
+                subSrv.getService()->stop();
+                ::fwServices::erase(subSrv.getService());
+                subSrv.m_service.reset();
+            }
+            m_objectsSubServices.erase(objectId);
+        }
+    }
+    // Object isn't present in the Composite and any subServices have been registered with it.
+    else
+    {
+        OSLM_TRACE ( "'"<< objectId << "' nonexistent'");
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+void SwapperSrv::reconfiguring()  throw ( ::fwTools::Failed )
+{}
+
+//-----------------------------------------------------------------------------
+
+void SwapperSrv::updating() throw ( ::fwTools::Failed )
+{}
+
+//-----------------------------------------------------------------------------
+
+void SwapperSrv::info( std::ostream &_sstream )
+{}
+
+//-----------------------------------------------------------------------------
+
+} // manager
+} // ctrlSelection
