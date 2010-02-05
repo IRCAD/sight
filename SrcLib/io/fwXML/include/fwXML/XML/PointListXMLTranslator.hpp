fc6a004035f309f40a9fb92ec459fdd881ae4a81
Nicolas Philipps <nicolasph@gmail.com>
2010-02-05 18:46:53 +0100

    Added license files and added license header
    --HG--
    branch : fw4spl_0.6

--- SrcLib/io/fwXML/include/fwXML/XML/PointListXMLTranslator.hpp
+++ SrcLib/io/fwXML/include/fwXML/XML/PointListXMLTranslator.hpp
@@ -1,3 +1,9 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
 #ifndef _fwXML_XML_POINTLISTXMLTRANSLATOR_HPP_
 #define _fwXML_XML_POINTLISTXMLTRANSLATOR_HPP_
 
--- SrcLib/io/fwXML/src/fwXML/XML/TrivialXMLTranslator.cpp
+++ SrcLib/io/fwXML/src/fwXML/XML/TrivialXMLTranslator.cpp
@@ -1,3 +1,9 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
 #include "fwXML/XML/TrivialXMLTranslator.hpp"
 #include "fwXML/XML/XMLTranslatorHelper.hpp"
 #include "libxml/tree.h"
