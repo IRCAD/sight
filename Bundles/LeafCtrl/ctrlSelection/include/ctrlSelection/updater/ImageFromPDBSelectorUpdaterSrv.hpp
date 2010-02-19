ec588082f794500fa258e5559a648c78d9fc4082
arnaud.charnoz@gmail.com <arnaud.charnoz@gmail.com>
2010-02-19 18:14:44 +0100

    Add new IUpdater to manage PatientDB
    --HG--
    branch : fw4spl_0.6

---
+++ Bundles/LeafCtrl/ctrlSelection/include/ctrlSelection/updater/ImageFromPDBSelectorUpdaterSrv.hpp
@@ -0,0 +1,71 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef _CTRLSELECTION_IMAGEFROMPDBSELECTORUPDATERSRV_HPP_
+#define _CTRLSELECTION_IMAGEFROMPDBSELECTORUPDATERSRV_HPP_
+
+#include <fwData/Image.hpp>
+#include <fwData/PatientDB.hpp>
+
+#include "ctrlSelection/config.hpp"
+#include "ctrlSelection/IUpdaterSrv.hpp"
+
+namespace ctrlSelection
+{
+
+namespace updater
+{
+
+/**
+ * @class   ImageFromPDBSelectorUpdaterSrv.
+ * @author IRCAD (Research and Development Team).
+
+ * @date    2007-2009.
+ */
+class CTRLSELECTION_CLASS_API ImageFromPDBSelectorUpdaterSrv : public ::ctrlSelection::IUpdaterSrv
+{
+
+public :
+
+    fwCoreServiceClassDefinitionsMacro ( (ImageFromPDBSelectorUpdaterSrv)(::ctrlSelection::IUpdaterSrv::Baseclass) ) ;
+
+    /// Constructor.  Do nothing.
+    CTRLSELECTION_API ImageFromPDBSelectorUpdaterSrv() throw() ;
+
+    /// Destructor. Do nothing.
+    CTRLSELECTION_API virtual ~ImageFromPDBSelectorUpdaterSrv() throw() ;
+
+protected:
+
+    /// Implements starting method derived from IService. Do nothing.
+    CTRLSELECTION_API virtual void starting()  throw ( ::fwTools::Failed );
+
+    /// Implements stopping method derived from IService. Do nothing.
+    CTRLSELECTION_API virtual void stopping()  throw ( ::fwTools::Failed );
+
+    /// Implements configuring method derived from IService. Do nothing.
+    CTRLSELECTION_API virtual void configuring()  throw ( ::fwTools::Failed );
+
+    /// Implements reconfiguring method derived from IService. Do nothing.
+    CTRLSELECTION_API virtual void reconfiguring()  throw ( ::fwTools::Failed );
+
+    /// Implements updating method derived from IService. Do nothing.
+    CTRLSELECTION_API virtual void updating() throw ( ::fwTools::Failed );
+
+    /// Implements info method derived from IService. Print classname.
+    CTRLSELECTION_API virtual void info( std::ostream &_sstream );
+
+    CTRLSELECTION_API virtual void updating( ::fwServices::ObjectMsg::csptr _msg ) throw ( ::fwTools::Failed );
+
+private:
+
+    ::fwData::Image::sptr getImage(::fwData::PatientDB::sptr patientDB, ::fwServices::ObjectMsg::csptr _msg);
+};
+
+} // updater
+} // ctrlSelection
+
+#endif // _CTRLSELECTION_IMAGEFROMPDBSELECTORUPDATERSRV_HPP_
