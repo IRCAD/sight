fad1c6e3708e8be217ce1a41ca5d76557ff2bdcc
Vincent Agnus <vincent.agnus@gmail.com>
2010-03-16 15:09:20 +0100

    * FwXMLGenericWriterService : a service which can be attached to any kind of object and serilize it using fxz IO device
    --HG--
    branch : fw4spl_0.6.1
    extra : rebase_source : 1ab55dd304119a90dcb1e4a311daa640130cddda

---
+++ Bundles/LeafIO/ioXML/include/ioXML/FwXMLGenericWriterService.hpp
@@ -0,0 +1,89 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef _IOXML_FWXMLGENERICWRITERSERVICE_HPP_
+#define _IOXML_FWXMLGENERICWRITERSERVICE_HPP_
+
+#include <string>
+#include <boost/filesystem/path.hpp>
+
+#include <fwXML/writer/FwXMLObjectWriter.hpp>
+#include <io/IWriter.hpp>
+
+#include "ioXML/config.hpp"
+
+
+namespace fwData
+{
+    class Generic;
+}
+
+
+namespace ioXML
+{
+
+class IOXML_CLASS_API FwXMLGenericWriterService : public ::io::IWriter
+{
+
+public :
+    fwCoreServiceClassDefinitionsMacro ( (FwXMLGenericWriterService)(::io::IWriter::Baseclass) ) ;
+
+    /**
+     * @brief   constructor
+     *
+     */
+    IOXML_API FwXMLGenericWriterService() throw();
+
+    /**
+     * @brief   destructor
+     */
+    IOXML_API virtual ~FwXMLGenericWriterService() throw();
+
+protected:
+
+    /// Override
+    IOXML_API virtual void configuring() throw(::fwTools::Failed) ;
+
+    /// Override
+    IOXML_API virtual void starting() throw(::fwTools::Failed);
+
+    /// Override
+    IOXML_API virtual void stopping() throw(::fwTools::Failed);
+
+    /// Override
+    IOXML_API void updating() throw(::fwTools::Failed);
+
+    /// Override
+    IOXML_API virtual void updating( ::boost::shared_ptr< const ::fwServices::ObjectMsg > _msg ) throw(::fwTools::Failed) {} ;
+
+    /// Override
+    IOXML_API void info(std::ostream &_sstream ) ;
+
+    /// Override
+    IOXML_API std::string getCfgExtensionPoint() ;
+
+    /// Override
+    IOXML_API virtual std::string getPersistanceId() ;
+
+
+    /// Override
+    IOXML_API virtual void configureWithIHM();
+
+private :
+
+    void saveData( const ::boost::filesystem::path path, ::fwTools::Object::sptr _obj );
+
+    bool isAnFwxmlArchive( const ::boost::filesystem::path filePath );
+    void manageZipAndSaveData( const ::boost::filesystem::path path, ::fwTools::Object::sptr _obj );
+    ::boost::filesystem::path correctFileFormat( const ::boost::filesystem::path _filePath ) const;
+
+    ::fwXML::writer::FwXMLObjectWriter m_writer;
+
+};
+
+} // namespace ioXML
+
+#endif //_IOXML_FWXMLGENERICWRITERSERVICE_HPP_
---
+++ Bundles/LeafIO/ioXML/src/ioXML/FwXMLGenericWriterService.cpp
@@ -0,0 +1,214 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#include <wx/wx.h>
+#include <wx/version.h>
+
+#include <boost/filesystem/convenience.hpp>
+#include <boost/filesystem/operations.hpp>
+
+#include <fwServices/macros.hpp>
+#include <fwServices/helper.hpp>
+#include <fwServices/ObjectServiceRegistry.hpp>
+
+#include <io/IWriter.hpp>
+
+#include <fwCore/base.hpp>
+
+#include <fwTools/System.hpp>
+
+#include <fwData/Acquisition.hpp>
+
+#include <fwXML/writer/FwXMLObjectWriter.hpp>
+
+#include <fwWX/ProgressTowx.hpp>
+#include <fwWX/wxZipFolder.hpp>
+
+#include "ioXML/FwXMLGenericWriterService.hpp"
+
+namespace ioXML
+{
+
+REGISTER_SERVICE( ::io::IWriter , ::ioXML::FwXMLGenericWriterService , ::fwTools::Object ) ;
+
+//------------------------------------------------------------------------------
+
+FwXMLGenericWriterService::FwXMLGenericWriterService() throw()
+{}
+
+//------------------------------------------------------------------------------
+
+FwXMLGenericWriterService::~FwXMLGenericWriterService() throw()
+{}
+
+//------------------------------------------------------------------------------
+
+void FwXMLGenericWriterService::configuring() throw(::fwTools::Failed)
+{
+	 m_writer.setFile( ::boost::filesystem::path("SAVEDGRAPH.fxz") );
+}
+
+//------------------------------------------------------------------------------
+
+void FwXMLGenericWriterService::configureWithIHM()
+{
+    static wxString _sDefaultPath = _("");
+    wxString title = _("Choose a fxz or a xml file");
+    wxString folder = wxFileSelector(
+            title,
+            _sDefaultPath,
+            wxT(""),
+            wxT(""),
+            wxT("fwXML archive (*.fxz)|*.fxz|fwXML (*.xml)|*.xml"),
+#if wxCHECK_VERSION(2, 8, 0)
+            wxFD_SAVE,
+#else
+            wxSAVE,
+#endif
+            wxTheApp->GetTopWindow() );
+
+    if( folder.IsEmpty() == false)
+    {
+        m_writer.setFile(  ::boost::filesystem::path( wxConvertWX2MB(folder), ::boost::filesystem::native ) );
+        _sDefaultPath = wxConvertMB2WX( m_writer.getFile().branch_path().string().c_str() );
+    }
+}
+
+//------------------------------------------------------------------------------
+
+void FwXMLGenericWriterService::starting() throw(::fwTools::Failed)
+{
+    SLM_TRACE_FUNC();
+}
+
+//------------------------------------------------------------------------------
+
+void FwXMLGenericWriterService::stopping() throw(::fwTools::Failed)
+{
+    SLM_TRACE_FUNC();
+}
+
+//------------------------------------------------------------------------------
+
+void FwXMLGenericWriterService::info(std::ostream &_sstream )
+{
+    _sstream << "FwXMLGenericWriterService::info" ;
+}
+
+//------------------------------------------------------------------------------
+
+std::string FwXMLGenericWriterService::getCfgExtensionPoint()
+{
+    return "" ;
+}
+
+//------------------------------------------------------------------------------
+
+std::string FwXMLGenericWriterService::getPersistanceId()
+{
+    return "ioITK::FwXMLGenericWriterService" ;
+}
+
+//------------------------------------------------------------------------------
+
+void FwXMLGenericWriterService::saveData( const ::boost::filesystem::path path, ::fwTools::Object::sptr _obj )
+{
+    SLM_TRACE_FUNC();
+    ::fwXML::writer::FwXMLObjectWriter myWriter;
+
+    myWriter.setObject(_obj);
+    myWriter.setFile(path);
+
+    try
+    {
+        ::fwWX::ProgressTowx progressMeterGUI("Saving data ");
+        myWriter.addHandler( progressMeterGUI );
+        myWriter.write();
+    }
+    catch (const std::exception & e)
+    {
+        std::stringstream ss;
+        ss << "Warning during loading : " << e.what();
+        wxString wxStmp( ss.str().c_str(), wxConvLocal );
+        wxMessageBox( wxStmp, _("Warning"), wxOK|wxICON_WARNING );
+    }
+    catch( ... )
+    {
+        std::stringstream ss;
+        ss << "Warning during loading : ";
+        wxString wxStmp( ss.str().c_str(), wxConvLocal );
+        wxMessageBox( wxStmp, _("Warning"), wxOK|wxICON_WARNING );
+    }
+}
+
+//------------------------------------------------------------------------------
+
+void FwXMLGenericWriterService::updating() throw(::fwTools::Failed)
+{
+    SLM_TRACE("FwXMLGenericWriterService::updating()");
+
+    if( !m_writer.getFile().empty() )
+    {
+        // Retrieve dataStruct associated with this service
+        ::fwData::Object::sptr obj = this->getObject< ::fwData::Object >();
+        SLM_ASSERT("data is null", obj);
+
+        wxBeginBusyCursor();
+        m_writer.setFile( correctFileFormat( m_writer.getFile() ));
+        if ( isAnFwxmlArchive( m_writer.getFile() ) )
+        {
+            manageZipAndSaveData( m_writer.getFile(), obj);
+        }
+        else
+        {
+            saveData(m_writer.getFile(), obj);
+        }
+        wxEndBusyCursor();
+    }
+}
+
+//------------------------------------------------------------------------------
+
+::boost::filesystem::path FwXMLGenericWriterService::correctFileFormat( const ::boost::filesystem::path _filePath ) const
+{
+    ::boost::filesystem::path newPath = _filePath;
+    if ( ::boost::filesystem::extension(_filePath) != ".fxz" && ::boost::filesystem::extension(_filePath) != ".xml" )
+    {
+        newPath = _filePath.string() + ".fxz";
+    }
+
+    return newPath;
+}
+
+//------------------------------------------------------------------------------
+
+bool FwXMLGenericWriterService::isAnFwxmlArchive( const ::boost::filesystem::path filePath )
+{
+    return ( ::boost::filesystem::extension(filePath) == ".fxz" );
+}
+
+//------------------------------------------------------------------------------
+
+void FwXMLGenericWriterService::manageZipAndSaveData( const ::boost::filesystem::path path, ::fwTools::Object::sptr _obj )
+{
+    // Save in tmp folder
+    ::boost::filesystem::path srcFolder = ::fwTools::System::getTemporaryFolder() / "fwxmlArchiveFolder";
+    ::boost::filesystem::path xmlfile = srcFolder / "root.xml";
+    ::boost::filesystem::create_directories( srcFolder );
+    saveData(xmlfile,_obj);
+
+    // Zip
+    wxString destZipFileName ( wxConvertMB2WX( path.string().c_str() ) );
+    wxString srcFolderName ( wxConvertMB2WX( srcFolder.string().c_str() ) );
+    ::fwWX::wxZipFolder::packFolder( srcFolderName, destZipFileName );
+
+    // Remove temp folder
+    ::boost::filesystem::remove_all( srcFolder );
+}
+
+//------------------------------------------------------------------------------
+
+} // namespace ioXML
