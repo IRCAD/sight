be0686726f010eb62b45992752c3a98a6be5bd28
Julien Waechter <julien.waechter@gmail.com>
2010-03-22 08:33:51 +0100

    * Cleaning code (removed std::cout, added boost sptr macro)
    --HG--
    branch : fw4spl_0.6.1

--- SrcLib/core/fwComEd/src/fwComEd/objectConfigurationParser.cpp
+++ SrcLib/core/fwComEd/src/fwComEd/objectConfigurationParser.cpp
@@ -15,15 +15,18 @@
 
 #include "fwComEd/objectConfigurationParser.hpp"
 
-REGISTER_SERVICE( ::fwServices::IXMLParser , ::fwComEd::ICompositeParser , ::fwData::Composite );
-REGISTER_SERVICE( ::fwServices::IXMLParser , ::fwComEd::IProcessObjectParser ,  ::fwData::ProcessObject );
+REGISTER_SERVICE( ::fwServices::IXMLParser, ::fwComEd::ICompositeParser, ::fwData::Composite );
+REGISTER_SERVICE( ::fwServices::IXMLParser, ::fwComEd::IProcessObjectParser, ::fwData::ProcessObject );
 
 namespace fwComEd
 {
+
+//------------------------------------------------------------------------------
+
 void ICompositeParser::updating( ) throw(fwTools::Failed)
 {
-    ::boost::shared_ptr< ::fwRuntime::ConfigurationElement >_cfgElement = this->m_configuration;
-    ::boost::shared_ptr< ::fwData::Composite > dataComposite = this->getObject< ::fwData::Composite >() ;
+    ::fwRuntime::ConfigurationElement::sptr _cfgElement = this->m_configuration;
+    ::fwData::Composite::sptr dataComposite = this->getObject< ::fwData::Composite >() ;
     assert( dataComposite ) ;
 
     for( ::fwRuntime::ConfigurationElement::Iterator configEltIter = _cfgElement->begin() ; !(configEltIter == _cfgElement->end()) ; ++configEltIter)
@@ -31,61 +34,65 @@ void ICompositeParser::updating( ) throw(fwTools::Failed)
         if( (*configEltIter)->getName() == "object" )
         {
             // Configuring the input/output
-            ::boost::shared_ptr< fwTools::Object > localObj = fwServices::New( (*configEltIter) ) ;
+            ::fwTools::Object::sptr localObj = ::fwServices::New( (*configEltIter) ) ;
             assert( localObj ) ;
             // Inserting initialized object in processObject
-            assert( (*configEltIter)->hasAttribute("id") ) ;
+            SLM_ASSERT("Sorry, attribute id is missing", (*configEltIter)->hasAttribute("id") ) ;
             std::string _id = (*configEltIter)->getExistingAttributeValue("id") ;
-            assert( ::boost::dynamic_pointer_cast< ::fwData::Object >(localObj ));
+            assert( ::fwData::Object::dynamicCast(localObj ) );
             assert( dataComposite->getRefMap().find(_id) == dataComposite->getRefMap().end() ) ;
-            dataComposite->getRefMap()[_id] = ::boost::dynamic_pointer_cast< ::fwData::Object >(localObj ) ;
+            dataComposite->getRefMap()[_id] = ::fwData::Object::dynamicCast(localObj ) ;
         }
     }
 }
 
+//------------------------------------------------------------------------------
 
 void IProcessObjectParser::updating( ) throw(fwTools::Failed)
 {
-     ::boost::shared_ptr< ::fwRuntime::ConfigurationElement >_cfgElement = this->m_configuration ;
-     ::boost::shared_ptr< ::fwData::ProcessObject > dataProcessObject = this->getObject< ::fwData::ProcessObject >() ;
-//   ::boost::shared_ptr< ::fwData::ProcessObject > dataProcessObject = ::boost::dynamic_pointer_cast< ::fwData::ProcessObject>(_obj) ;
+    ::fwRuntime::ConfigurationElement::sptr _cfgElement = this->m_configuration ;
+    ::fwData::ProcessObject::sptr dataProcessObject = this->getObject< ::fwData::ProcessObject >() ;
     assert( dataProcessObject ) ;
 
     ///// INPUTS
-    const ::boost::shared_ptr< ::fwRuntime::ConfigurationElement> inputParameterList = _cfgElement->findConfigurationElement("inputs");
+    ::fwRuntime::ConfigurationElement::sptr inputParameterList = _cfgElement->findConfigurationElement("inputs");
     if( inputParameterList )
     {
         assert( inputParameterList ) ;
-        this->configureIO(dataProcessObject->getInputs() , inputParameterList ) ;
+        this->configureIO(dataProcessObject->getInputs(), inputParameterList ) ;
     }
     ///// OUTPUTS
-    const ::boost::shared_ptr< ::fwRuntime::ConfigurationElement> outputParameterList = _cfgElement->findConfigurationElement("outputs");
+    ::fwRuntime::ConfigurationElement::sptr outputParameterList = _cfgElement->findConfigurationElement("outputs");
     if( outputParameterList )
     {
-    assert( outputParameterList ) ;
-    this->configureIO(dataProcessObject->getOutputs() , outputParameterList ) ;
+        assert( outputParameterList ) ;
+        this->configureIO(dataProcessObject->getOutputs(), outputParameterList ) ;
     }
 }
 
-void IProcessObjectParser::configureIO( ::boost::shared_ptr< fwTools::Object > io, const ::boost::shared_ptr< ::fwRuntime::ConfigurationElement> list)
+//------------------------------------------------------------------------------
+
+void IProcessObjectParser::configureIO( ::fwTools::Object::sptr io, const ::fwRuntime::ConfigurationElement::sptr list)
 {
     for( ::fwRuntime::ConfigurationElement::Iterator configEltIter = list->begin() ; !(configEltIter == list->end()) ; ++configEltIter)
     {
-        std::cout << "ENTERING NAME " << (*configEltIter)->getName() << std::endl;
+        OSLM_TRACE("ENTERING NAME " << (*configEltIter)->getName());
         /// Configuring the input/output
-         ::boost::shared_ptr< fwTools::Object > localObj = fwServices::New( (*configEltIter) ) ;
+        ::fwTools::Object::sptr localObj = ::fwServices::New( (*configEltIter) ) ;
         assert( localObj ) ;
         // Inserting initialized object in processObject
-        assert( (*configEltIter)->hasAttribute("id") ) ;
+        SLM_ASSERT("Sorry, attribute id is missing", (*configEltIter)->hasAttribute("id") ) ;
         std::string _id = (*configEltIter)->getExistingAttributeValue("id") ;
-//      assert( ::boost::dynamic_pointer_cast< ::fwData::Object >(localObj ));
-        assert( ::boost::dynamic_pointer_cast< ::fwTools::Field >(io ));
-        if ( ::boost::dynamic_pointer_cast< ::fwTools::Field >(io )->getField( _id ).get() == NULL )
+
+        ::fwTools::Field::sptr ioField = ::fwTools::Field::dynamicCast(io);
+        assert( ioField);
+        if ( ioField->getField( _id ).get() == NULL )
         {
-            ::boost::dynamic_pointer_cast< ::fwTools::Field >(io )->addFieldElement( _id, localObj ) ;
+            ioField->addFieldElement( _id, localObj ) ;
         }
     }
 }
 
+//------------------------------------------------------------------------------
 
 }
