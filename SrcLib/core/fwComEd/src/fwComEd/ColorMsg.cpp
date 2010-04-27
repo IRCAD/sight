d746c387060af538c15e23960a1cae20786c93ed
Julien Waechter <julien.waechter@gmail.com>
2010-04-27 17:52:58 +0200

    * Added ColorMsg
    --HG--
    branch : fw4spl_0.6.2

---
+++ SrcLib/core/fwComEd/src/fwComEd/ColorMsg.cpp
@@ -0,0 +1,29 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#include "fwComEd/ColorMsg.hpp"
+
+namespace fwComEd
+{
+
+//-----------------------------------------------------------------------------
+
+std::string ColorMsg::VALUE_IS_MODIFIED = "VALUE_IS_MODIFIED";
+
+//-----------------------------------------------------------------------------
+
+ColorMsg::ColorMsg() throw()
+{}
+
+//-----------------------------------------------------------------------------
+
+ColorMsg::~ColorMsg() throw()
+{}
+
+//-----------------------------------------------------------------------------
+
+} // namespace fwComEd
+
