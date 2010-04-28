6d97c5cfdf49a669743246d9baddefdd88dc2e26
Julien Waechter <julien.waechter@gmail.com>
2010-04-28 15:18:54 +0200

    see #1292 (1)  * Added PointListMsg  * Updated Long and Double fwWX::Validator
    --HG--
    branch : fw4spl_0.6.2

---
+++ SrcLib/core/fwComEd/include/fwComEd/PointListMsg.hpp
@@ -0,0 +1,52 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef _FWCOMED_POINTLISTMSG_HPP_
+#define _FWCOMED_POINTLISTMSG_HPP_
+
+#include <fwServices/ObjectMsg.hpp>
+
+#include "fwComEd/export.hpp"
+
+
+namespace fwComEd
+{
+
+/**
+ * @brief   Object event message specialized for PointList : store modification information
+ * @class   PointListMsg
+ * @author  IRCAD (Research and Development Team).
+ * @date    2007-2009.
+ * @see     ::fwServices::ObjectMsg
+ */
+class FWCOMED_CLASS_API PointListMsg : public fwServices::ObjectMsg
+{
+
+public:
+
+    fwCoreClassDefinitionsWithFactoryMacro((PointListMsg)(::fwServices::ObjectMsg), ( ( ) ), new PointListMsg );
+
+    /// Event identifier used to inform that the value is modified.
+    FWCOMED_API static std::string PointListMsg::ELEMENT_MODIFIED;
+    FWCOMED_API static std::string PointListMsg::ELEMENT_ADDED;
+    FWCOMED_API static std::string PointListMsg::ELEMENT_REMOVED;
+
+    /**
+     * @brief   Constructor : does nothing.
+     */
+    FWCOMED_API PointListMsg() throw();
+
+    /**
+     * @brief   Destructor : does nothing.
+     */
+    FWCOMED_API virtual ~PointListMsg() throw();
+};
+
+
+} // namespace fwComEd
+
+#endif //_FWCOMED_POINTLISTMSG_HPP_
+
---
+++ SrcLib/core/fwComEd/src/fwComEd/PointListMsg.cpp
@@ -0,0 +1,31 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#include "fwComEd/PointListMsg.hpp"
+
+namespace fwComEd
+{
+
+//-----------------------------------------------------------------------------
+
+std::string PointListMsg::ELEMENT_MODIFIED = "ELEMENT_MODIFIED";
+std::string PointListMsg::ELEMENT_ADDED    = "ELEMENT_ADDED";
+std::string PointListMsg::ELEMENT_REMOVED  = "ELEMENT_REMOVED";
+
+//-----------------------------------------------------------------------------
+
+PointListMsg::PointListMsg() throw()
+{}
+
+//-----------------------------------------------------------------------------
+
+PointListMsg::~PointListMsg() throw()
+{}
+
+//-----------------------------------------------------------------------------
+
+} // namespace fwComEd
+
