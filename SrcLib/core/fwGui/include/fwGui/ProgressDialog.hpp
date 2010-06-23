59808c8661453822ed89e3936e60560095f9da17
Vincent Agnus <vincent.agnus@gmail.com>
2010-06-23 17:35:09 +0200

    =Gui Abstraction  * fwGui::ProgressDialog class
    --HG--
    branch : multiGui

---
+++ SrcLib/core/fwGui/include/fwGui/ProgressDialog.hpp
@@ -0,0 +1,58 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef _FWGUI_ProgressDialog_HPP_
+#define _FWGUI_ProgressDialog_HPP_
+
+#include <fwCore/base.hpp>
+
+#include "fwGui/config.hpp"
+#include "fwGui/IProgressDialog.hpp"
+namespace fwGui
+{
+
+/**
+ * @brief   Defines the generic file/folder dialog for IHM.
+ * Use the Delegate design pattern. The specific implementation selection is ensured by ::fwClassFactoryRegistry
+ * The specific implementation are in fwWX and fwQT libraries
+ * @class   ProgressDialog.
+ * @author  IRCAD (Research and Development Team).
+ * @date    2009-2010.
+ *
+ */
+class FWGUI_CLASS_API ProgressDialog : public IProgressDialog
+{
+
+public:
+
+    /// will instanciate the concrete implementation
+    FWGUI_API ProgressDialog( const std::string &title = std::string() ,  const std::string &msg = std::string() );
+
+    /// override
+    FWGUI_API void setTitle(const std::string &title);
+
+    /// override
+    FWGUI_API void setMessage(const std::string &message);
+
+    /// override
+    FWGUI_API void operator()(float percent,std::string msg);
+
+
+protected :
+
+    ::boost::shared_ptr< IProgressDialog > m_implementation;
+
+
+
+
+
+};
+
+} // namespace fwGui
+
+#endif /*_FWGUI_ProgressDialog_HPP_*/
+
+
---
+++ SrcLib/core/fwGui/src/fwGui/IProgressDialog.cpp
@@ -0,0 +1,27 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+
+#include "fwGui/IProgressDialog.hpp"
+
+namespace fwGui
+{
+
+const IProgressDialog::FactoryRegistryKeyType
+IProgressDialog::factoryRegistryKey =  "an unique key for all IProgressDialog (factories are exclusives)";
+
+IProgressDialog::IProgressDialog()
+{}
+
+
+IProgressDialog::~IProgressDialog()
+{}
+
+
+} // namespace fwGui
+
+
+
