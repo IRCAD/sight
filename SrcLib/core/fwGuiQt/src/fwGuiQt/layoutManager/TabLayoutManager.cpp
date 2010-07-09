21c4fd24eb84de81b42e67c88fc71596ace62f4f
Nicolas Philipps <nicolasph@gmail.com>
2010-07-09 17:50:14 +0200

    Added Qt side of TabLayoutManager
    --HG--
    branch : multiGui
    extra : rebase_source : 5bbf2d6f40773bbd02cb9b221be0851102bafc95

---
+++ SrcLib/core/fwGuiQt/src/fwGuiQt/layoutManager/TabLayoutManager.cpp
@@ -0,0 +1,89 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#include <boost/foreach.hpp>
+
+#include <QTabWidget>
+#include <QBoxLayout>
+
+#include <fwCore/base.hpp>
+#include <fwTools/ClassRegistrar.hpp>
+
+#include "fwGuiQt/layoutManager/TabLayoutManager.hpp"
+
+
+REGISTER_BINDING( ::fwGui::layoutManager::IViewLayoutManager,
+        ::fwGui::TabLayoutManager,
+        ::fwGui::layoutManager::TabLayoutManagerBase::RegistryKeyType,
+        ::fwGui::layoutManager::TabLayoutManagerBase::REGISTRY_KEY );
+
+
+namespace fwGui
+{
+
+//-----------------------------------------------------------------------------
+
+TabLayoutManager::TabLayoutManager()
+{}
+
+//-----------------------------------------------------------------------------
+
+TabLayoutManager::~TabLayoutManager()
+{}
+
+//-----------------------------------------------------------------------------
+
+void TabLayoutManager::createLayout( ::fwGui::fwContainer::sptr parent )
+{
+    SLM_TRACE_FUNC();
+    m_parentContainer = ::fwGuiQt::container::QtContainer::dynamicCast(parent);
+    SLM_ASSERT("dynamicCast fwContainer to QtContainer failed", m_parentContainer);
+
+    QWidget* qtContainer = m_parentContainer->getQtContainer();
+    m_tabWidget = new QTabWidget(qtContainer);
+
+    QBoxLayout *layout = new QBoxLayout(QBoxLayout::TopToBottom);
+    if(qtContainer->layout())
+    {
+        qtContainer->layout()->deleteLater();
+    }
+    qtContainer->setLayout(layout);
+
+    layout->addWidget( m_tabWidget );
+
+    const std::list< ViewInfo> &views = this->getViewsInfo();
+
+    BOOST_FOREACH ( ViewInfo viewInfo, views)
+    {
+        QWidget *widget = new QWidget(m_tabWidget);
+
+        ::fwGuiQt::container::QtContainer::NewSptr subContainer;
+        subContainer->setQtContainer(widget);
+        m_subViews.push_back(subContainer);
+
+        int idx = m_tabWidget->addTab( widget, QString::fromStdString(viewInfo.m_caption));
+        if (viewInfo.m_isSelect )
+        {
+            m_tabWidget->setCurrentIndex(idx);
+        }
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+void TabLayoutManager::destroyLayout()
+{
+    this->destroySubViews();
+    m_tabWidget->clear();
+    m_parentContainer->clean();
+}
+
+//-----------------------------------------------------------------------------
+
+} // namespace fwGui
+
+
+
