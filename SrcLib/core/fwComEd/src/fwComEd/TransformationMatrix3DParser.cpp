a8529c2a648e4b27d28c982eb997c5bfa899d3da
arnaud.charnoz@gmail.com <arnaud.charnoz@gmail.com>
2010-09-08 11:06:51 +0200

    Add a transformation matrix 3d parser
    --HG--
    branch : fw4spl_0.6.3

---
+++ SrcLib/core/fwComEd/src/fwComEd/TransformationMatrix3DParser.cpp
@@ -0,0 +1,55 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#include <fwTools/ClassRegistrar.hpp>
+#include <fwTools/Factory.hpp>
+
+#include <fwServices/macros.hpp>
+#include <fwServices/Factory.hpp>
+
+#include <fwData/TransformationMatrix3D.hpp>
+
+#include "fwComEd/TransformationMatrix3DParser.hpp"
+
+REGISTER_SERVICE( ::fwServices::IXMLParser, ::fwComEd::TransformationMatrix3DParser, ::fwData::TransformationMatrix3D );
+
+namespace fwComEd
+{
+
+//------------------------------------------------------------------------------
+
+void TransformationMatrix3DParser::updating( ) throw(fwTools::Failed)
+{
+    ::fwRuntime::ConfigurationElement::sptr _cfgElement = this->m_configuration;
+    ::fwData::TransformationMatrix3D::sptr matrix = this->getObject< ::fwData::TransformationMatrix3D >() ;
+    assert( matrix ) ;
+
+    for(    ::fwRuntime::ConfigurationElement::Iterator configEltIter = _cfgElement->begin();
+            !(configEltIter == _cfgElement->end());
+            ++configEltIter )
+    {
+        if ( (*configEltIter)->getName() == "matrix" )
+        {
+            std::string input = (*configEltIter)->getValue();
+            std::istringstream inputString( input );
+
+            matrix->getRefCoefficients().clear();
+            matrix->getRefCoefficients().reserve(16);
+
+            double coef;
+            for ( unsigned int k; k < 16; k++)
+            {
+                inputString >> coef;
+                OSLM_WARN("Coef = " <<  coef );
+                matrix->getRefCoefficients().push_back( coef );
+            }
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+
+}
