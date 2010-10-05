6466d7b99fa33f8b1d86ed564e256535a8845586
arnaud.charnoz@gmail.com <arnaud.charnoz@gmail.com>
2010-10-05 09:37:36 +0200

    Support compilation under fwTools/fwID with g++-4.4.3
    --HG--
    branch : fw4spl_0.7.0

--- SrcLib/core/fwTools/src/fwTools/fwID.cpp
+++ SrcLib/core/fwTools/src/fwTools/fwID.cpp
@@ -24,57 +24,58 @@ fwID::Dictionary fwID::m_dictionary;
 
 fwID::~fwID()
 {
-	resetID();
+    resetID();
 }
 
 
 bool fwID::exist( IDType _id)
 {
-	return m_dictionary.find( _id ) != m_dictionary.end();
+    return m_dictionary.find( _id ) != m_dictionary.end();
 }
 
 
 
 bool fwID::hasID( ) const
 {
-	return !m_id.empty();
+    return !m_id.empty();
 }
 
 
 
 void fwID::setID( IDType newID )
 {
-	resetID();
-	// note we use a static cast for a down cast because we do not use the classical polyvi morphic approach
-	m_dictionary[ newID ] = (static_cast< Object *>(this))->getSptr();
-	m_id = newID;
+    resetID();
+    // note we use a static cast for a down cast because we do not use the classical polyvi morphic approach
+    // m_dictionary[ newID ] = (static_cast< Object *>(this))->getSptr();
+    m_dictionary[ newID ] = ((Object*)(this))->getSptr();
+    m_id = newID;
 }
 
 
 fwID::IDType fwID::getID( Policy policy) const
 {
-	if ( m_id.empty() ) // no id set
-	{
-		if ( policy==GENERATE )
-		{
-			IDType newID = generate();
-			const_cast<fwID *>(this)->setID(newID);
-		}
-		else if  ( policy == EMPTY )
-		{ /* nothing to do*/ }
-		else if ( policy == MUST_EXIST )
-		{
-			throw fwTools::Failed( "fwID::getID() no id set" );
-		}
-	}
-	return m_id;
+    if ( m_id.empty() ) // no id set
+    {
+        if ( policy==GENERATE )
+        {
+            IDType newID = generate();
+            const_cast<fwID *>(this)->setID(newID);
+        }
+        else if  ( policy == EMPTY )
+        { /* nothing to do*/ }
+        else if ( policy == MUST_EXIST )
+        {
+            throw fwTools::Failed( "fwID::getID() no id set" );
+        }
+    }
+    return m_id;
 }
 
 
 
 fwID::IDType fwID::generate() const
 {
-	IDType newID;
+    IDType newID;
     std::string prefix = this->getRootedClassname();
     do
     {
@@ -88,39 +89,39 @@ fwID::IDType fwID::generate() const
 
 ::fwTools::Object::sptr fwID::getObject( fwID::IDType requestID )
 {
-	Dictionary::iterator it = m_dictionary.find(requestID);
-	if ( it!=m_dictionary.end() )
-	{
-		SLM_ASSERT(  "expired object in fwID::Dictionary for id=" + requestID ,  !it->second.expired() );
-		return it->second.lock();
-	}
-	else
-	{
-		return ::fwTools::Object::NewSptr();
-	}
+    Dictionary::iterator it = m_dictionary.find(requestID);
+    if ( it!=m_dictionary.end() )
+    {
+        SLM_ASSERT(  "expired object in fwID::Dictionary for id=" + requestID ,  !it->second.expired() );
+        return it->second.lock();
+    }
+    else
+    {
+        return ::fwTools::Object::NewSptr();
+    }
 }
 
 
 
 void fwID::resetID()
 {
-	if ( !m_id.empty() )
-	{
-		m_dictionary.erase(m_id);
-	}
-	m_id.clear();
+    if ( !m_id.empty() )
+    {
+        m_dictionary.erase(m_id);
+    }
+    m_id.clear();
 }
 
 
 
 void fwID::swapID( ::fwTools::Object::sptr   obj2 )
 {
-	IDType id1 = getID(GENERATE);
-	IDType id2 = obj2->getID(GENERATE);
-	resetID();
-	obj2->resetID();
-	setID(id2);
-	obj2->setID(id1);
+    IDType id1 = getID(GENERATE);
+    IDType id2 = obj2->getID(GENERATE);
+    resetID();
+    obj2->resetID();
+    setID(id2);
+    obj2->setID(id1);
 }
 
 
