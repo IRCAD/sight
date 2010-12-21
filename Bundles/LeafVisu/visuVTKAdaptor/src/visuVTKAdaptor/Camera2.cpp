8b272b28a7b95b7555b68e25795ff14080ff5deb
Johan Moreau <johan.moreau@gmail.com>
2010-12-21 09:27:06 +0100

    fix bug on camera following
    --HG--
    branch : workshop-tw-2010

--- Bundles/LeafVisu/visuVTKAdaptor/src/visuVTKAdaptor/Camera2.cpp
+++ Bundles/LeafVisu/visuVTKAdaptor/src/visuVTKAdaptor/Camera2.cpp
@@ -53,7 +53,7 @@ void Camera2::configuring() throw(fwTools::Failed)
     this->setRenderId( m_configuration->getAttributeValue("renderer") );
     if ( m_configuration->hasAttribute( "transform" ) )
     {
-    	this->setTransformId ( m_configuration->getAttributeValue ( "transform" ) );
+        this->setTransformId ( m_configuration->getAttributeValue ( "transform" ) );
     }
     
     
@@ -62,15 +62,19 @@ void Camera2::configuring() throw(fwTools::Failed)
 void Camera2::doStart() throw(fwTools::Failed)
 {
     vtkCamera* camera = this->getRenderer()->GetActiveCamera();
-    camera->SetPosition (0, 0, 0);
-    camera->SetFocalPoint(0, 0, 10);
-    camera->SetViewUp(0, -1, 0);
-    vtkIdentityTransform* trans = vtkIdentityTransform::New();
-    trans->Identity();
-    camera->SetUserViewTransform( trans );
-    camera->SetClippingRange(0.1, 10000);
+ 
+    //vtkIdentityTransform* trans = vtkIdentityTransform::New();
+    //trans->Identity();
     
-    camera->SetUserViewTransform( this->getTransform() );
+    //camera->SetUserTransform( trans );
+    //camera->SetUserViewTransform( trans );
+ 
+    camera->SetPosition (0, 0, 0);
+    camera->SetFocalPoint(1, 0, 0);
+    camera->SetViewUp(0, 0, 1);
+    //camera->SetClippingRange(0.1, 10000);
+    
+ //   camera->SetUserViewTransform( this->getTransform() );
 }
 
 void Camera2::doUpdate() throw(fwTools::Failed)
@@ -90,12 +94,54 @@ void Camera2::doStop() throw(fwTools::Failed)
 
 void Camera2::doUpdate( ::fwServices::ObjectMsg::csptr msg) throw(fwTools::Failed)
 {
-
-/*  
     if( msg->hasEvent( ::fwComEd::TransformationMatrix3DMsg::MATRIX_IS_MODIFIED ) )
     {
         vtkCamera* camera = this->getRenderer()->GetActiveCamera();
-	
+        ::fwData::TransformationMatrix3D::sptr transMat =
+            this->getObject< ::fwData::TransformationMatrix3D >();
+
+        vtkMatrix4x4* mat = vtkMatrix4x4::New();
+
+        for(int lt=0; lt<4; lt++)
+        {
+            for(int ct=0; ct<4; ct++)
+            {
+                mat->SetElement(lt, ct, transMat->getCoefficient(lt,ct));
+                OSLM_ERROR("val = " << transMat->getCoefficient(lt,ct));
+            }
+        }
+        OSLM_ERROR(" ")
+
+        double focal[]={1.0, 0.0, 0.0, 1.0};
+        double rfocal[4];
+        double position[]={0.0, 0.0, 0.0, 1.0};
+        double rposition[4];
+        double viewUp[]={0.0, 0.0, 1.0, 1.0};
+        double rviewUp[4];
+
+        mat->MultiplyPoint( focal, rfocal);
+        mat->MultiplyPoint( position, rposition);
+        mat->MultiplyPoint( viewUp, rviewUp);
+        for ( int i = 0; i < 3; i ++ )
+        {
+            //rfocal[i] = rfocal[i] - rposition[i];
+            rviewUp[i] = rviewUp[i] - rposition[i];
+        }
+
+        mat->Delete();
+
+        camera->SetPosition(rposition);
+        camera->SetFocalPoint(rfocal);
+        camera->SetViewUp(rviewUp);
+
+        this->getRenderer()->ResetCameraClippingRange();
+        this->setVtkPipelineModified();
+    }
+/*
+    if( msg->hasEvent( ::fwComEd::TransformationMatrix3DMsg::MATRIX_IS_MODIFIED ) )
+    {
+        vtkCamera* camera = this->getRenderer()->GetActiveCamera();
+
         ::fwData::TransformationMatrix3D::sptr transMat =
             this->getObject< ::fwData::TransformationMatrix3D >();
 
@@ -106,10 +152,10 @@ void Camera2::doUpdate( ::fwServices::ObjectMsg::csptr msg) throw(fwTools::Faile
             for(int ct=0; ct<4; ct++)
             {
                 //mat->SetElement(lt, ct, transMat->getCoefficient(lt,ct));
-		camera->GetUserViewTransform()->GetMatrix()->SetElement(lt, ct, transMat->getCoefficient(lt,ct));
+        camera->GetUserViewTransform()->GetMatrix()->SetElement(lt, ct, transMat->getCoefficient(lt,ct));
             }
         }
-	camera->GetUserViewTransform()->Modified();
+    camera->GetUserViewTransform()->Modified();
         this->setVtkPipelineModified();
     }*/
 }
