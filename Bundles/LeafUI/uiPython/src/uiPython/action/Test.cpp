9c6ee1610ce13471b2ab1210c68378308b3dd9db
Nicolas Philipps <nicolasph@gmail.com>
2011-02-09 12:33:29 +0100

    Added uiPython Bundle with a Test "hello world" action
    --HG--
    branch : fw4spl_0.7.3_python

---
+++ Bundles/LeafUI/uiPython/src/uiPython/action/Test.cpp
@@ -0,0 +1,83 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#include <fwCore/base.hpp>
+
+#include <fwTools/ClassFactoryRegistry.hpp>
+#include <fwServices/macros.hpp>
+#include <fwServices/ObjectServiceRegistry.hpp>
+
+#include <fwPython/Interpreter.hpp>
+
+#include "uiPython/action/Test.hpp"
+
+namespace uiPython
+{
+namespace action
+{
+
+REGISTER_SERVICE( ::fwGui::IActionSrv , ::uiPython::action::Test , ::fwTools::Object ) ;
+
+//-----------------------------------------------------------------------------
+
+Test::Test() throw()
+{}
+
+//-----------------------------------------------------------------------------
+
+Test::~Test() throw()
+{}
+
+//-----------------------------------------------------------------------------
+
+void Test::configuring() throw( ::fwTools::Failed )
+{
+    SLM_TRACE_FUNC() ;
+    this->initialize();
+}
+
+//-----------------------------------------------------------------------------
+
+void Test::updating( ::fwServices::ObjectMsg::csptr _msg ) throw( ::fwTools::Failed )
+{
+    SLM_TRACE_FUNC();
+}
+
+//-----------------------------------------------------------------------------
+
+void Test::starting() throw( ::fwTools::Failed )
+{
+    SLM_TRACE_FUNC();
+    this->actionServiceStarting();
+}
+
+//-----------------------------------------------------------------------------
+
+void Test::stopping() throw( ::fwTools::Failed )
+{
+    SLM_TRACE_FUNC();
+    this->actionServiceStopping();
+}
+
+//-----------------------------------------------------------------------------
+
+void Test::info(std::ostream &_sstream )
+{
+    _sstream << "Test Action" << std::endl;
+}
+
+//-----------------------------------------------------------------------------
+
+void Test::updating() throw( ::fwTools::Failed )
+{
+    ::fwPython::Interpreter python;
+    python.execute("print \"Hello fw4spl world\"");
+}
+
+//-----------------------------------------------------------------------------
+
+}
+}
