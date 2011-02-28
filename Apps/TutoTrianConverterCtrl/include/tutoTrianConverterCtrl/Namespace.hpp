04d72a1c435e524c0ca5563c8c6454cbdffc95c7
Julien Waechter <julien.waechter@gmail.com>
2011-02-28 16:58:23 +0100

    see #1475 Add TutoTrianConverterCtrl bundle: apps in command line without configuration xml file (convert trian to vtk mesh)
    --HG--
    branch : fw4spl_0.7.3
    extra : rebase_source : 92a743a9aa7d8ede59964a137f5558d342f89de0

---
+++ Apps/TutoTrianConverterCtrl/include/tutoTrianConverterCtrl/Namespace.hpp
@@ -0,0 +1,21 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef TUTOTRIANCONVERTERCTRLNAMESPACE_HPP_
+#define TUTOTRIANCONVERTERCTRLNAMESPACE_HPP_
+
+/**
+ * @brief      The namespace tutoTrianConverterCtrl.
+ * @namespace  tutoTrianConverterCtrl
+ * @author     IRCAD (Research and Development Team).
+ * @date       2009-2010.
+ *
+ */
+namespace tutoTrianConverterCtrl
+{
+
+}
+#endif /* TUTOTRIANCONVERTERCTRLNAMESPACE_HPP_ */
