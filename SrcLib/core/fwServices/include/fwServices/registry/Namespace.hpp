d81a1be56a1dda9dddeb055b247f25f1c3c344e7
Pascal Monnier <plmonnier@gmail.com>
2011-03-03 08:57:22 +0100

    Added appXml bundle on several build.options.
    --HG--
    branch : fw4spl_0.7.3

---
+++ SrcLib/core/fwServices/include/fwServices/registry/Namespace.hpp
@@ -0,0 +1,24 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef FWSERVICESREGISTRYNAMESPACE_HPP_
+#define FWSERVICESREGISTRYNAMESPACE_HPP_
+
+namespace fwServices
+{
+/**
+ * @brief       The namespace fwServices::registry contains class to manage services, object and config.
+ * @namespace   fwServices::registry
+ * @author      IRCAD (Research and Development Team).
+ * @date        2009-2010.
+ *
+ */
+namespace registry
+{
+
+}
+}
+#endif /* FWSERVICESREGISTRYNAMESPACE_HPP_ */
