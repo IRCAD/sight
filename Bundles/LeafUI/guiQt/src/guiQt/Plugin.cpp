157da8b3ae9f970e07d49c062b4debe56bfee859
Nicolas Philipps <nicolasph@gmail.com>
2011-04-27 12:43:16 +0200

    Cleaned default qt library paths : removed qt build path
    --HG--
    branch : fw4spl_0.7.5

--- Bundles/LeafUI/guiQt/src/guiQt/Plugin.cpp
+++ Bundles/LeafUI/guiQt/src/guiQt/Plugin.cpp
@@ -4,6 +4,8 @@
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
 
+#include <QStringList>
+
 #include <boost/bind.hpp>
 
 #include <fwCore/base.hpp>
@@ -44,6 +46,10 @@ void Plugin::start() throw(::fwRuntime::RuntimeException)
     ::fwGuiQt::App *app;
     app = new ::fwGuiQt::App( m_argc, argv );
     m_app = app;
+
+    QStringList libraryPaths = app->libraryPaths();
+    libraryPaths.removeFirst();
+    app->setLibraryPaths(libraryPaths);
 
     ::fwRuntime::profile::getCurrentProfile()->setRunCallback(::boost::bind(&Plugin::run, this));
 }
