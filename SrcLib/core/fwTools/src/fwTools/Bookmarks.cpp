13a49ffebc6ad8cfd63bb255df7906f281d6da4f
Vincent Agnus <vincent.agnus@gmail.com>
2011-05-04 18:35:33 +0200

    see #1614 : bookmark system on object
    --HG--
    branch : fw4spl_0.7.5
    extra : rebase_source : a8ee1c7ba6bdf1d0984139d97699fee1b0d0450a

---
+++ SrcLib/core/fwTools/src/fwTools/Bookmarks.cpp
@@ -0,0 +1,82 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2010.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#include <assert.h>
+#include <boost/lexical_cast.hpp>
+
+#include <fwCore/Demangler.hpp>
+
+#include "fwTools/Object.hpp"
+#include "fwTools/Bookmarks.hpp"
+#include "fwTools/Failed.hpp"
+
+namespace fwTools
+{
+Bookmarks::Dictionary Bookmarks::m_dictionary;
+
+//-----------------------------------------------------------------------------
+Bookmarks::~Bookmarks()
+{
+}
+
+
+//-----------------------------------------------------------------------------
+bool Bookmarks::exist( Bookmarks::BookmarkName _id)
+{
+    return m_dictionary.find( _id ) != m_dictionary.end();
+}
+
+
+
+//-----------------------------------------------------------------------------
+void Bookmarks::add( Bookmarks::BookmarkName _bookmark,  ::fwTools::Object::sptr obj )
+{
+    OSLM_FATAL_IF("Try to set a  Bookmarks on a null object ", !obj );
+    m_dictionary[ _bookmark ] = obj;
+}
+
+//-----------------------------------------------------------------------------
+void Bookmarks::remove( Bookmarks::BookmarkName _bookmark  )
+{
+    m_dictionary.erase( m_dictionary.find( _bookmark ) );
+}
+
+
+//-----------------------------------------------------------------------------
+::fwTools::Object::sptr Bookmarks::getObject( Bookmarks::BookmarkName _bookmark )
+{
+
+    Dictionary::iterator iter = m_dictionary.find( _bookmark );
+    if ( iter != m_dictionary.end() )
+    {
+        if ( iter->second.expired() == false )
+        {
+            return iter->second.lock();
+        }
+    }
+    return  ::fwTools::Object::NewSptr();
+}
+
+
+
+std::list<Bookmarks::BookmarkName> Bookmarks::getBookmarks( ::fwTools::Object::sptr obj )
+{
+    std::list<Bookmarks::BookmarkName> result;
+    Dictionary::iterator iter = m_dictionary.begin();
+    for ( ; iter != m_dictionary.end(); ++iter )
+    {
+        if ( iter->second.expired() == false && iter->second.lock() == obj )
+        {
+            result.push_back(  iter->first );
+        }
+    }
+    return  result;
+}
+
+
+
+
+}
