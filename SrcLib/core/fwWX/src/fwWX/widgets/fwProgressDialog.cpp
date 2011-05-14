5a627a5012b320805c3edc308013f2169267fa9f
Julien Waechter <julien.waechter@gmail.com>
2011-05-14 17:57:38 +0200

    Fixed fwWX compilation: GetParentForModalDialog required windows style parameter see http://trac.wxwidgets.org/changeset/64019
    --HG--
    branch : fw4spl_0.7.5

--- SrcLib/core/fwWX/src/fwWX/widgets/fwProgressDialog.cpp
+++ SrcLib/core/fwWX/src/fwWX/widgets/fwProgressDialog.cpp
@@ -85,7 +85,7 @@ fwProgressDialog::fwProgressDialog(const wxString& title,
                                    int maximum,
                                    wxWindow *parent,
                                    int style)
-                : wxDialog(GetParentForModalDialog(parent), wxID_ANY, title),
+                : wxDialog(GetParentForModalDialog(parent, style), wxID_ANY, title),
                   m_skip(false),
                   m_delay(3),
                   m_hasAbortButton(false),
