4a7573f984093acb228e42d4eba1bac40fd18fbc
Nicolas Philipps <nicolasph@gmail.com>
2011-08-29 14:15:54 +0200

    Updated TriangularMeshReader :  * Using now a boost::spirit parser  * The new code is a little bit slower in debug mode, but much faster in release mode.
    --HG--
    branch : fw4spl_0.7.6

--- SrcLib/core/fwDataIO/src/fwDataIO/reader/TriangularMeshReader.cpp
+++ SrcLib/core/fwDataIO/src/fwDataIO/reader/TriangularMeshReader.cpp
@@ -9,6 +9,12 @@
 
 #include <boost/cstdint.hpp>
 
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/home/phoenix/statement/sequence.hpp>
+#include <boost/spirit/home/phoenix/container.hpp>
+
 #include <fwTools/ClassRegistrar.hpp>
 
 #include <fwData/Object.hpp>
@@ -26,6 +32,59 @@ namespace fwDataIO
 
 namespace reader
 {
+
+
+template <typename Iterator>
+bool parseTrian(Iterator first, Iterator last, ::fwData::TriangularMesh::PointContainer &points, ::fwData::TriangularMesh::CellContainer &cells)
+{
+    using boost::spirit::qi::ulong_long;
+    using boost::spirit::qi::int_;
+    using boost::spirit::qi::float_;
+    using boost::spirit::qi::phrase_parse;
+    using boost::spirit::qi::_1;
+    using boost::spirit::qi::_2;
+    using boost::spirit::qi::_3;
+    using boost::spirit::qi::repeat;
+    using boost::spirit::ascii::space;
+    using boost::phoenix::push_back;
+    using boost::phoenix::ref;
+    namespace phx = boost::phoenix;
+
+    unsigned long long int nbPoints;
+    unsigned long long int nbCells;
+
+    ::fwData::TriangularMesh::PointContainer::value_type point(3);
+    float &pa = point[0];
+    float &pb = point[1];
+    float &pc = point[2];
+    ::fwData::TriangularMesh::CellContainer::value_type cell(3);
+    int &ca = cell[0];
+    int &cb = cell[1];
+    int &cc = cell[2];
+
+    bool r = phrase_parse(first, last,
+
+        //  Begin grammar
+        (
+            ulong_long[ phx::ref(nbPoints) = _1, phx::reserve(phx::ref(points), phx::ref(nbPoints)) ] >>
+            repeat(phx::ref(nbPoints))[ (float_ >> float_ >> float_)[phx::ref(pa) = _1, phx::ref(pb) = _2, phx::ref(pc) = _3, phx::push_back(phx::ref(points),phx::ref(point))] ] >>
+            ulong_long[ phx::ref(nbCells) = _1, phx::reserve(phx::ref(cells), phx::ref(nbCells)) ] >>
+            repeat(phx::ref(nbCells))[ (int_ >> int_ >> int_ >> "-1 -1 -1")[phx::ref(ca) = _1, phx::ref(cb) = _2, phx::ref(cc) = _3, phx::push_back(phx::ref(cells),phx::ref(cell))]  ]
+            // An alternative for >> "-1 -1 -1" is: >> int_  >> int_  >> int_
+            // but this is a little bit less efficient, since every file should
+            // currently contain -1 -1 -1 as normal's values
+        ),
+        //  End grammar
+
+        space
+        );
+
+    if (first != last) // fail if we didn't get a full match
+        return false;
+    return r;
+
+}
+
 
 //------------------------------------------------------------------------------
 
@@ -48,59 +107,55 @@ void TriangularMeshReader::read()
     OSLM_INFO( "[TriangularMeshReader::read] Trian file: " << path.string());
     SLM_ASSERT("Empty path for TriangularMesh file", !path.empty() );
 
-    ::fwData::TriangularMesh::sptr triMesh = getConcreteObject();
 
-    std::fstream file;
-    file.open(path.string().c_str(), std::fstream::in);
+    size_t length;
+    //char *buffer;
+    std::string buf;
+    std::ifstream file;
+    file.open(path.string().c_str(), std::ios::binary );
+
     if (!file.is_open())
     {
         OSLM_ERROR( "Trian file loading error for " << path.string());
         throw std::ios_base::failure("Unable to open " + path.string());
     }
 
-    // Clear the container cells and set its capacity to 0
-    triMesh->clearCells();
-    // Clear the container points and set its capacity to 0
-    triMesh->clearPoints();
 
+    file.seekg (0, std::ios::end);
+    length = file.tellg();
+    file.seekg (0, std::ios::beg);
+
+    //buffer = new char [length];
+    buf.resize(length);
+    char *buffer = &buf[0];
+
+    file.read (buffer, length);
+    file.close();
+
+
+    ::fwData::TriangularMesh::sptr triMesh = getConcreteObject();
+
+    ::fwData::TriangularMesh::PointContainer newPoints;
+    ::fwData::TriangularMesh::CellContainer  newCells;
     ::fwData::TriangularMesh::PointContainer &points = triMesh->points();
     ::fwData::TriangularMesh::CellContainer  &cells  = triMesh->cells();
 
-    std::vector< float > point(3);
-    float &pa = point[0];
-    float &pb = point[1];
-    float &pc = point[2];
-    std::vector< int > cell(3);
-    int &ca = cell[0];
-    int &cb = cell[1];
-    int &cc = cell[2];
+    //Free TriangularMesh memory
+    triMesh->clearCells();
+    triMesh->clearPoints();
 
-    /// Read content and update mesh data structure
-    if(!file.eof())
+    if (!parseTrian(buffer, buffer+length, newPoints, newCells))
     {
-        unsigned int nbPts=0, nbCells=0, i=0;
-        file>>nbPts;
-        points.reserve(nbPts);
-        i = nbPts + 1;
-        while (--i)
-        {
-            file >> pa >> pb >> pc;
-            points.push_back( point ) ;
-        }
-        points.resize(nbPts);
-
-        file>>nbCells;
-        cells.reserve(nbCells);
-        i = nbCells + 1;
-        while (--i)
-        {
-            file>>ca >> cb >> cc;
-            file.ignore(20, '\n');
-            cells.push_back( cell ) ;
-        }
-        cells.resize(nbCells);
+        OSLM_ERROR( "Bad file format : " << path.string());
+        throw std::ios_base::failure("Unable to open " + path.string() + " : Bad file format.");
     }
-    file.close();
+
+    points.swap(newPoints);
+    cells.swap(newCells);
+
+
+    //delete buffer;
+
 }
 
 //------------------------------------------------------------------------------
