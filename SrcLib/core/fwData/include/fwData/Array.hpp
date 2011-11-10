b20e0bf736d54409210e65455113a9c523cda8ab
Nicolas Philipps <nicolasph@gmail.com>
2011-11-10 12:34:07 +0100

    Added Array and Mesh header
    --HG--
    branch : fw4spl_0.8.0

---
+++ SrcLib/core/fwData/include/fwData/Array.hpp
@@ -0,0 +1,154 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2011.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef _FWDATA_ARRAY_HPP_
+#define _FWDATA_ARRAY_HPP_
+
+#include <fwTools/DynamicType.hpp>
+
+#include "fwData/Exception.hpp"
+
+#include "fwData/config.hpp"
+#include "fwData/Object.hpp"
+
+
+namespace fwData
+{
+
+/**
+ * @class   Array
+ * @brief   Provides a way to manage a multidimentionnal array.  If the
+ * array own his buffer, it will support the allocation, reallocation,
+ * destruction of the buffer. Else, this class will behave like a "view" on the
+ * buffer
+ * @author  IRCAD (Research and Development Team).
+ * @date    2011.
+ */
+class FWDATA_CLASS_API Array : public ::fwData::Object
+{
+public :
+
+    fwCoreClassDefinitionsWithFactoryMacro( (Array)(::fwData::Object), (()), ::fwTools::Factory::New< Array >) ;
+
+    fwDataObjectMacro();
+
+    typedef std::vector<size_t> SizeType;
+    typedef std::vector<size_t> OffsetType;
+    typedef OffsetType IndexType;
+
+
+    /*
+     * public API
+     */
+    /// Defines shallow copy
+    FWDATA_API void shallowCopy( Array::csptr _source );
+
+    /// Defines deep copy
+    FWDATA_API void deepCopy( Array::csptr _source );
+
+
+    //Allocates or resizes allocated buffer. If the new combination of type, size
+    //and components do not match anymore the previously allocated size, a
+    //reallocation is required. If the reallocation is needed and the
+    //reallocate parameter is true, the reallocation is performed. Otherwise,
+    //an exception is thrown.
+
+    FWDATA_API virtual size_t allocate(const ::fwTools::DynamicType &type, const SizeType &size, const size_t nbOfComponents) throw(::fwData::Exception);
+    FWDATA_API virtual size_t allocate(const std::string &type, const SizeType &size, const size_t nbOfComponents) throw(::fwData::Exception);
+
+    FWDATA_API virtual size_t resize(const ::fwTools::DynamicType &type, const SizeType &size, const size_t nbOfComponents, const bool reallocate = false) throw(::fwData::Exception);
+    FWDATA_API virtual size_t resize(const std::string &type, const SizeType &size, const size_t nbOfComponents, const bool reallocate = false) throw(::fwData::Exception);
+    FWDATA_API virtual size_t resize(const SizeType &size, const size_t nbOfComponents, const bool reallocate = false) throw(::fwData::Exception);
+    FWDATA_API virtual size_t resize(const SizeType &size, const bool reallocate = false) throw(::fwData::Exception);
+
+    FWDATA_API virtual void clear();
+
+
+    ///<value> must be a valid buffer of elements of type <m_type> with a length equal to <m_nbOfComponents>
+    FWDATA_API virtual void setItem(const IndexType &id, const void *value);
+    ///<value> must be a valid pointer to an element of type <m_type>
+    FWDATA_API virtual void setItem(const IndexType &id, const size_t component, const void *value);
+
+    ///Returns the pointer to the data in the buffer
+    FWDATA_API virtual void* getItem(const IndexType &id) const;
+    FWDATA_API virtual void* getItem(const IndexType &id, const size_t component) const;
+
+    ///Copies the data to the buffer pointed by <value>
+    FWDATA_API virtual void  getItem(const IndexType &id, void *value) const;
+    FWDATA_API virtual void  getItem(const IndexType &id, const size_t component, void *value) const;
+
+
+
+    FWDATA_API virtual void *getBuffer() const;
+
+    /// Returns the begining/end of the buffer, casted to T
+    template< typename T > T* begin() const;
+    template< typename T > T* end() const;
+    template< typename T > const T* cbegin() const;
+    template< typename T > const T* cend() const;
+
+
+
+    FWDATA_API virtual size_t getSizeInBytes() const;
+
+    FWDATA_API virtual SizeType   getSize() const;
+    FWDATA_API virtual OffsetType getStrides() const;
+    FWDATA_API virtual size_t getNumberOfComponents() const;
+    FWDATA_API virtual size_t getNumberOfDimensions() const;
+
+    FWDATA_API virtual void setIsBufferOwner(const bool own);
+    FWDATA_API virtual bool getIsBufferOwner() const;
+
+    FWDATA_API virtual ::fwTools::DynamicType getType() const;
+
+protected:
+    FWDATA_API Array();
+    FWDATA_API virtual ~Array();
+
+    /// Not implemented
+    Array( const Array& );
+    const Array & operator= ( const Array& );
+
+    //-----------------------------------------------------------------------------
+
+    OffsetType m_strides; // Liste de valeurs représentant le pas dans chaque dimension pour parcourir le tableau
+
+
+    //=============================================================================
+    ::fwTools::DynamicType m_type;
+    void * m_buffer;
+    SizeType  m_size;
+    size_t m_nbOfComponents;
+    bool   m_isBufferOwner;
+
+};
+
+template< typename T > T*       Array::begin() const
+{
+    return static_cast<T*>(m_buffer);
+}
+
+template< typename T > T*       Array::end() const
+{
+    return reinterpret_cast<T*> (static_cast<char*>(m_buffer) + this->getSizeInBytes());
+}
+
+template< typename T > const T* Array::cbegin() const
+{
+    return static_cast<const T*>(m_buffer);
+}
+
+template< typename T > const T* Array::cend() const
+{
+    return reinterpret_cast<const T*> (static_cast<char*>(m_buffer) + this->getSizeInBytes());
+}
+
+
+
+
+} // namespace fwData
+
+#endif // _FWDATA_ARRAY_HPP_
---
+++ SrcLib/core/fwData/include/fwData/Mesh.hpp
@@ -0,0 +1,168 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2011.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef _FWDATA_MESH_HPP_
+#define _FWDATA_MESH_HPP_
+
+#include <boost/multi_array.hpp>
+#include "fwData/Array.hpp"
+#include "fwData/Exception.hpp"
+#include "fwData/Mesh.hpp"
+
+#include "fwData/config.hpp"
+
+
+namespace fwData
+{
+
+/**
+ * @class   Mesh
+ * @brief   
+ * @author  IRCAD (Research and Development Team).
+ * @date    2011.
+ */
+class FWDATA_CLASS_API Mesh : public ::fwData::Object
+{
+
+public:
+
+    fwCoreClassDefinitionsWithFactoryMacro( (Mesh)(::fwData::Object), (()), ::fwTools::Factory::New< Mesh >) ;
+
+    fwDataObjectMacro();
+
+    typedef std::map< std::string, ::fwData::Array::sptr > ArrayMapType;
+    typedef uint64_t Id;
+
+    typedef enum {
+        NONE = 0,
+        POINT,
+        EDGE,
+        TRIANGLE,
+        QUAD,
+        POLY
+    } CellTypes;
+
+    typedef float PointValueType;
+    typedef float ColorValueType;
+    typedef float NormalValueType;
+    typedef Id    CellValueType;
+
+    typedef boost::multi_array_ref<PointValueType, 2>  PointMultiArrayType;
+    typedef boost::multi_array_ref<CellTypes, 1>       CellTypesMultiArrayType;
+    typedef boost::multi_array_ref<CellValueType, 1>   CellDataMultiArrayType;
+    typedef boost::multi_array_ref<Id, 1>              CellDataOffsetsMultiArrayType;
+    typedef boost::multi_array_ref<ColorValueType, 2>  PointColorsMultiArrayType;
+    typedef boost::multi_array_ref<ColorValueType, 2>  CellColorsMultiArrayType;
+    typedef boost::multi_array_ref<NormalValueType, 2> PointNormalsMultiArrayType;
+    typedef boost::multi_array_ref<NormalValueType, 2> CellNormalsMultiArrayType;
+
+    /// Defines shallow copy
+    FWDATA_API void shallowCopy( Mesh::csptr _source );
+
+    /// Defines deep copy
+    FWDATA_API void deepCopy( Mesh::csptr _source );
+
+
+    FWDATA_API size_t allocate(size_t nbPts, size_t nbCells, size_t nbCellsData = 0);
+
+    FWDATA_API size_t allocatePointNormals();
+    FWDATA_API size_t allocatePointColors();
+    FWDATA_API size_t allocateCellNormals();
+    FWDATA_API size_t allocateCellColors();
+
+    FWDATA_API void stripAllocatedMemory();
+
+    FWDATA_API Id insertNextPoint(PointValueType x, PointValueType y, PointValueType z);
+    FWDATA_API void setPoint(Id id, PointValueType x, PointValueType y, PointValueType z);
+
+    FWDATA_API Id insertNextCell(CellValueType p);
+    FWDATA_API Id insertNextCell(CellValueType p1, CellValueType p2);
+    FWDATA_API Id insertNextCell(CellValueType p1, CellValueType p2, CellValueType p3);
+    FWDATA_API Id insertNextCell(CellValueType p1, CellValueType p2, CellValueType p3, CellValueType p4);
+
+    template <typename T> Id insertNextCell(CellTypes type, const T &pointsBegin, const T &pointsEnd);
+    template <typename T> Id insertNextCell(CellTypes type, const T &pointsBegin, size_t nb);
+
+    FWDATA_API void cleanCells();
+
+
+    FWDATA_API PointMultiArrayType           getPoints() const;
+    FWDATA_API CellTypesMultiArrayType       getCellTypes() const;
+    FWDATA_API CellDataMultiArrayType        getCellData() const;
+    FWDATA_API CellDataOffsetsMultiArrayType getCellDataOffsets() const;
+    FWDATA_API PointColorsMultiArrayType     getPointColors() const;
+    FWDATA_API CellColorsMultiArrayType      getCellColors() const;
+    FWDATA_API PointNormalsMultiArrayType    getPointNormalsType() const;
+    FWDATA_API CellNormalsMultiArrayType     getCellNormals() const;
+
+    FWDATA_API void setPointArray            (::fwData::Array::sptr array);
+    FWDATA_API void setCellTypesArray        (::fwData::Array::sptr array);
+    FWDATA_API void setCellDataArray         (::fwData::Array::sptr array);
+    FWDATA_API void setCellDataOffsetsArray  (::fwData::Array::sptr array);
+    FWDATA_API void setPointColorsArray      (::fwData::Array::sptr array);
+    FWDATA_API void setCellColorsArray       (::fwData::Array::sptr array);
+    FWDATA_API void setPointNormalsTypeArray (::fwData::Array::sptr array);
+    FWDATA_API void setCellNormalsArray      (::fwData::Array::sptr array);
+
+    FWDATA_API ::fwData::Array::sptr getPointsArray           () const;
+    FWDATA_API ::fwData::Array::sptr getCellTypesArray        () const;
+    FWDATA_API ::fwData::Array::sptr getCellDataArray         () const;
+    FWDATA_API ::fwData::Array::sptr getCellDataOffsetsArray  () const;
+    FWDATA_API ::fwData::Array::sptr getPointColorsArray      () const;
+    FWDATA_API ::fwData::Array::sptr getCellColorsArray       () const;
+    FWDATA_API ::fwData::Array::sptr getPointNormalsTypeArray () const;
+    FWDATA_API ::fwData::Array::sptr getCellNormalsArray      () const;
+
+
+
+    FWDATA_API void clearPoints();
+    FWDATA_API void clearCells();
+    FWDATA_API void clear();
+
+    FWDATA_API void clearPointNormals();
+    FWDATA_API void clearPointColors();
+    FWDATA_API void clearCellNormals();
+    FWDATA_API void clearCellColors();
+
+
+    FWDATA_API size_t getNumberOfPoints() const;
+    FWDATA_API size_t getNumberOfCells() const;
+
+    FWDATA_API size_t getDataSizeInBytes() const;
+    FWDATA_API size_t getAllocatedSizeInBytes() const;
+
+    FWDATA_API void addDataArray(const std::string &name, ::fwData::Array::sptr array);
+    FWDATA_API ::fwData::Array::sptr getDataArray(const std::string &name) const;
+    FWDATA_API void removeDataArray(const std::string &name);
+
+
+protected:
+
+    FWDATA_API Mesh();
+    FWDATA_API virtual ~Mesh() ;
+
+
+    size_t m_nbPoints;
+    size_t m_nbCells;
+    size_t m_nbCellsData;
+
+    ::fwData::Array::sptr m_points;
+    ::fwData::Array::sptr m_cellTypes;
+    ::fwData::Array::sptr m_cellData;
+    ::fwData::Array::sptr m_cellDataOffsets;
+    ::fwData::Array::sptr m_pointColors;
+    ::fwData::Array::sptr m_cellColors;
+    ::fwData::Array::sptr m_pointNormals;
+    ::fwData::Array::sptr m_cellNormals;
+
+    ArrayMapType m_arrayMap;
+
+
+};
+
+} // namespace fwData
+
+#endif // _FWDATA_MESH_HPP_
