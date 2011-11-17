d13516e951010171577c1ae5e8750399095ed7c6
Nicolas Philipps <nicolasph@gmail.com>
2011-11-17 15:48:29 +0100

    refs #1840 : Updated fwData::Mesh trian export
    --HG--
    branch : fw4spl_0.8.0
    extra : rebase_source : 279ff51d089a9bca0e891185b4aeb852b0063823

--- SrcLib/core/fwDataIO/src/fwDataIO/writer/MeshWriter.cpp
+++ SrcLib/core/fwDataIO/src/fwDataIO/writer/MeshWriter.cpp
@@ -42,8 +42,8 @@ void MeshWriter::write()
     OSLM_INFO( "[MeshReader::read] Trian file: " << getFile());
     assert( getFile().empty() ==  false );
 
-    ::fwData::Mesh::sptr triMesh = this->getConcreteObject();
-    FW_RAISE_IF("Can't convert this Mesh to TriangularMesh", !::fwDataTools::MeshGenerator::hasUniqueCellType(triMesh, ::fwData::Mesh::TRIANGLE));
+    ::fwData::Mesh::sptr mesh = this->getConcreteObject();
+    FW_RAISE_IF("Can't convert this Mesh to TriangularMesh", !::fwDataTools::MeshGenerator::hasUniqueCellType(mesh, ::fwData::Mesh::TRIANGLE));
 
     std::fstream file;
     file.open(getFile().string().c_str(), std::fstream::out);
@@ -56,41 +56,49 @@ void MeshWriter::write()
     }
 
     size_t i, nbPts, nbCells;
-    nbPts = triMesh->getNumberOfPoints();
-    ::fwData::Mesh::PointsMultiArrayType points = triMesh->getPoints();
+    nbPts = mesh->getNumberOfPoints();
+    ::fwData::Mesh::PointsMultiArrayType points = mesh->getPoints();
     file<<nbPts<<std::endl;
     for( i=0 ; i<nbPts ; ++i )
     {
-        file<<points[i][0]<<" "<<points[i][1]<<" "<<points[i][2]<<std::endl;
+        file << points[i][0] << " " << points[i][1] << " " << points[i][2] << std::endl;
     }
 
-    nbCells = triMesh->getNumberOfCells();
-    ::fwData::Mesh::CellDataMultiArrayType cells = triMesh->getCellData();
-    size_t cellsSize = nbCells*3;
-    SLM_ASSERT("Wrong CellDataMultiArray size", cells.size() == cellsSize);
-    file<<nbCells<<std::endl;
-    typedef ::fwData::Mesh::CellDataMultiArrayType::index CellTypesIndex;
-    ::fwData::Array::sptr normals;// = triMesh->getCellNormalsArray();
-    if(normals)
+    nbCells = mesh->getNumberOfCells();
+    ::fwData::Array::sptr cells = mesh->getCellDataArray();
+
+    FW_RAISE_IF("Not able to write " << cells->getType().string() << " cell type in trian file.",
+            cells->getType() != ::fwTools::Type::create< ::boost::uint64_t >());
+
+    ::boost::uint64_t *cellBuf = cells->begin< ::boost::uint64_t >();
+    ::boost::uint64_t *cellBufEnd = cellBuf + 3*nbCells;
+
+    SLM_ASSERT("Wrong CellDataMultiArray size", cells->getNumberOfElements() >= nbCells*3);
+    file << nbCells << std::endl;
+
+    ::fwData::Array::sptr normals = mesh->getCellNormalsArray();
+    if(normals
+            && normals->getBuffer() != NULL
+            && normals->getType() == ::fwTools::Type::create<float>()
+            && normals->getNumberOfComponents() == 3
+            && normals->getNumberOfDimensions() == 1
+            && nbCells == normals->getSize().at(0)
+            )
     {
-//        ::fwData::Array::sptr cellTypes = triMesh->getCellTypesArray();
-//        SLM_ASSERT("", normals->getSize().size() == 1);
-//        SLM_ASSERT("", cellTypes->getSize().size() == 1);
-//        SLM_ASSERT("", cellTypes->getSize()[0] == normals->getSize()[0]);
-//
-//        ::fwData::Mesh::CellNormalsMultiArrayType cellNormals = triMesh->getCellNormals();
-//        for (CellTypesIndex i = 0; i < cellsSize; i+=3)
-//        {
-//            file<<cells[i]<<" "<<cells[i+1]<<" "<<cells[i+2]<<" "
-//                    <<cellNormals[i/3][0]<<" "<<cellNormals[i/3][1]<<" "<<cellNormals[i/3][2]<<std::endl;
-//        }
+        float *normalBuf = normals->begin< float >();
+
+        while (cellBuf != cellBufEnd)
+        {
+
+            file << (*cellBuf++) << " " << (*cellBuf++) << " " << (*cellBuf++) << " " 
+                 << (*normalBuf++) << " "  << (*normalBuf++) << " "  << (*normalBuf++) << std::endl;
+        }
     }
     else
     {
-        for (CellTypesIndex i = 0; i < cellsSize; i+=3)
+        while (cellBuf != cellBufEnd)
         {
-            file<<cells[i]<<" "<<cells[i+1]<<" "<<cells[i+2]<<" "
-                    <<-1<<" "<<-1<<" "<<-1<<std::endl;
+            file << (*cellBuf++) << " " << (*cellBuf++) << " " << (*cellBuf++) << " -1 -1 -1" << std::endl;
         }
     }
     file.close();
