7a33253c47a8991354c00d8229fccf2f852c8419
Nicolas Philipps <nicolasph@gmail.com>
2012-01-26 15:43:23 +0100

    Added test sample for guiQt (needs sconspiracy >= a752c8c27534)
    --HG--
    branch : fw4spl_0.8.1

---
+++ Bundles/LeafUI/guiQt/test/tu/include/GuiQtTest.hpp
@@ -0,0 +1,34 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2011.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef _FWDATATOOLS_TEST_TU_MESHGENERATORTEST_HPP_
+#define _FWDATATOOLS_TEST_TU_MESHGENERATORTEST_HPP_
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <fwData/Mesh.hpp>
+#include <fwData/TriangularMesh.hpp>
+#include <fwData/Array.hpp>
+
+/**
+ * @brief Test many methods to create mesh.
+ */
+class GuiQtTest : public CPPUNIT_NS::TestFixture
+{
+    CPPUNIT_TEST_SUITE( GuiQtTest );
+    CPPUNIT_TEST( testDefaultFrame );
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    // interface
+    void setUp();
+    void tearDown();
+
+    void testDefaultFrame();
+
+};
+
+#endif //_FWDATATOOLS_TEST_TU_MESHGENERATORTEST_HPP_
---
+++ Bundles/LeafUI/guiQt/test/tu/src/GuiQtTest.cpp
@@ -0,0 +1,84 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2011.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#include <QApplication>
+#include <QMainWindow>
+
+#include <fwData/Object.hpp>
+
+#include <fwRuntime/EConfigurationElement.hpp>
+
+#include <fwServices/Base.hpp>
+#include <fwGuiQt/App.hpp>
+
+
+#include "GuiQtTest.hpp"
+
+// Registers the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION( GuiQtTest );
+
+//------------------------------------------------------------------------------
+
+void GuiQtTest::setUp()
+{
+    // Set up context before running a test.
+}
+
+//------------------------------------------------------------------------------
+
+void GuiQtTest::tearDown()
+{
+    // Clean up after the test run.
+}
+
+//------------------------------------------------------------------------------
+
+void GuiQtTest::testDefaultFrame()
+{
+    ::fwData::Object::NewSptr object;
+
+
+    ::fwRuntime::EConfigurationElement::NewSptr frameCfg("service");
+    ::fwRuntime::EConfigurationElement::NewSptr guiCfg("gui");
+    ::fwRuntime::EConfigurationElement::NewSptr guiFrameCfg("frame");
+
+    ::fwRuntime::EConfigurationElement::NewSptr guiFrameNameCfg("name");
+    guiFrameNameCfg->setValue("guiQtUnitTest");
+    ::fwRuntime::EConfigurationElement::NewSptr guiFrameMinSizeCfg("minSize");
+    guiFrameMinSizeCfg->setAttributeValue("width", "800");
+    guiFrameMinSizeCfg->setAttributeValue("height", "600");
+
+    guiFrameCfg->addConfigurationElement(guiFrameNameCfg);
+    guiFrameCfg->addConfigurationElement(guiFrameMinSizeCfg);
+    guiCfg->addConfigurationElement(guiFrameCfg);
+
+    frameCfg->addConfigurationElement(guiCfg);
+
+
+    ::fwServices::IService::sptr srv = ::fwServices::registry::ServiceFactory::getDefault()->create(  "::fwGui::IFrameSrv", "::gui::frame::DefaultFrame" );
+    CPPUNIT_ASSERT(srv);
+
+    ::fwServices::OSR::registerService( object , srv );
+
+    srv->setConfiguration( frameCfg ) ;
+    srv->configure();
+    srv->start();
+
+    QMainWindow *window = qobject_cast<QMainWindow *>(qApp->activeWindow());
+
+    CPPUNIT_ASSERT(qApp);
+    CPPUNIT_ASSERT(qApp->activeWindow());
+    CPPUNIT_ASSERT(window);
+    CPPUNIT_ASSERT_EQUAL(std::string("guiQtUnitTest"), window->windowTitle().toStdString());
+
+    // srv->stop();
+    // ::gui::frame::DefaultFrame
+
+}
+
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
