3532251a0967d5d970dc73502502d69ceb06ad32
Emilie Harquel <emilie.har@gmail.com>
2012-02-16 15:29:33 +0100

    see #1969 Fixed FocusLandmark slice index poisition
    --HG--
    branch : fw4spl_0.8.2

--- Bundles/LeafUI/uiMeasurement/src/uiMeasurement/action/FocusLandmark.cpp
+++ Bundles/LeafUI/uiMeasurement/src/uiMeasurement/action/FocusLandmark.cpp
@@ -142,11 +142,11 @@ void FocusLandmark::updating() throw(::fwTools::Failed)
                 ::fwData::Point::sptr selectedPoint = name2Point[ selection ];
                 SLM_ASSERT("selectedPoint not instanced", selectedPoint);
                 ::fwData::Integer::NewSptr paramA;
-                paramA->value() = (selectedPoint->getRefCoord()[2] - pImage->getOrigin()[2])/  pImage->getSpacing()[2];
+                paramA->value() = static_cast<int>((selectedPoint->getRefCoord()[2] - pImage->getOrigin()[2] )/  pImage->getSpacing()[2] +0.5);
                 ::fwData::Integer::NewSptr paramF;
-                paramF->value() = (selectedPoint->getRefCoord()[1] -  pImage->getOrigin()[1])/  pImage->getSpacing()[1];
+                paramF->value() = static_cast<int>((selectedPoint->getRefCoord()[1] -  pImage->getOrigin()[1])/  pImage->getSpacing()[1] +0.5);
                 ::fwData::Integer::NewSptr paramS;
-                paramS->value() = (selectedPoint->getRefCoord()[0] -  pImage->getOrigin()[0])/  pImage->getSpacing()[0];
+                paramS->value() = static_cast<int>((selectedPoint->getRefCoord()[0] -  pImage->getOrigin()[0])/  pImage->getSpacing()[0] +0.5);
                 if( paramS->value() >= 0 &&
                         paramF->value() >= 0 &&
                         paramA->value() >= 0 &&
