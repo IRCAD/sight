1d542a33d396f0a9e0df63387c62f9ffbeecfef7
Nicolas Philipps <nicolasph@gmail.com>
2012-03-09 11:34:22 +0100

    see#2025: Added getter/setter macros for fwData
    --HG--
    branch : fw4spl_0.8.3

--- SrcLib/core/fwData/include/fwData/macros.hpp
+++ SrcLib/core/fwData/include/fwData/macros.hpp
@@ -4,6 +4,12 @@
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
 
+#ifndef __FWDATA_MACROS_HPP__
+#define __FWDATA_MACROS_HPP__
+
+#include <boost/preprocessor/cat.hpp>
+
+#include <fwCore/macros.hpp>
 /**
  * @file fwData/macros.hpp
  * @brief fwData specific macros.
@@ -22,7 +28,97 @@
  * text : comment used in doxygen
  */
 
-#include <fwCore/macros.hpp>
+
+
+//-----------------------------------
+// Getter/Setter macros
+//-----------------------------------
+#define __FWDATA_CLASS_ATTRIBUTE_PREFIX m_
+#define __FWDATA_GENERATED_ATTRIBUTE_PREFIX attr
+
+//-----------------------------------
+
+#define fwDataGetMacro(attrName, attrType)                                        \
+attrType get##attrName () const                                                   \
+{                                                                                 \
+    return BOOST_PP_CAT(                                                          \
+                       __FWDATA_CLASS_ATTRIBUTE_PREFIX,                           \
+                       BOOST_PP_CAT(__FWDATA_GENERATED_ATTRIBUTE_PREFIX,attrName) \
+                       );                                                         \
+}
+
+#define fwDataSetMacro(attrName, attrType)                                 \
+void set##attrName (attrType val)                                          \
+{                                                                          \
+    BOOST_PP_CAT(                                                          \
+                __FWDATA_CLASS_ATTRIBUTE_PREFIX,                           \
+                BOOST_PP_CAT(__FWDATA_GENERATED_ATTRIBUTE_PREFIX,attrName) \
+                ) = val;                                                   \
+}
+
+#define fwDataGetSptrMacro(attrName, attrType)                                       \
+attrType get##attrName () const                                                      \
+{                                                                                    \
+    return BOOST_PP_CAT(                                                             \
+                        __FWDATA_CLASS_ATTRIBUTE_PREFIX,                             \
+                        BOOST_PP_CAT( __FWDATA_GENERATED_ATTRIBUTE_PREFIX, attrName) \
+                        );                                                           \
+}
+
+#define fwDataSetSptrMacro(attrName, attrType)                               \
+void set##attrName (attrType val)                                            \
+{                                                                            \
+    BOOST_PP_CAT(                                                            \
+                __FWDATA_CLASS_ATTRIBUTE_PREFIX,                             \
+                BOOST_PP_CAT( __FWDATA_GENERATED_ATTRIBUTE_PREFIX, attrName) \
+                ) = val;                                                     \
+}
+
+//-----------------------------------
+
+#define fwDataGetCRefMacro(attrName, attrType)                                      \
+const attrType &get##attrName () const                                              \
+{                                                                                   \
+    return BOOST_PP_CAT(                                                            \
+                        __FWDATA_CLASS_ATTRIBUTE_PREFIX,                            \
+                        BOOST_PP_CAT(__FWDATA_GENERATED_ATTRIBUTE_PREFIX, attrName) \
+                        );                                                          \
+}
+
+#define fwDataSetCRefMacro(attrName, attrType)                              \
+void set##attrName (const attrType &val)                                    \
+{                                                                           \
+    BOOST_PP_CAT(                                                           \
+                __FWDATA_CLASS_ATTRIBUTE_PREFIX,                            \
+                BOOST_PP_CAT(__FWDATA_GENERATED_ATTRIBUTE_PREFIX, attrName) \
+                ) = val;                                                    \
+}
+
+//-----------------------------------
+
+#define fwDataGetSetMacro(attrName, attrType) \
+    /** @{ */                                 \
+    fwDataGetMacro(attrName, attrType)        \
+    fwDataSetMacro(attrName, attrType)        \
+    /** @}  */
+
+#define fwDataGetSetSptrMacro(attrName, attrType) \
+    /** @{ */                                     \
+    fwDataGetSptrMacro(attrName, attrType)        \
+    fwDataSetSptrMacro(attrName, attrType)        \
+    /** @}  */
+
+#define fwDataGetSetCRefMacro(attrName, attrType) \
+    /** @{ */                                     \
+    fwDataGetCRefMacro(attrName, attrType)        \
+    fwDataSetCRefMacro(attrName, attrType)        \
+    /** @}  */
+
+
+
+//-----------------------------------
+// Old Getter/Setter macros
+//-----------------------------------
 
 #define fwGettersSettersMacro(funcName, var, type) \
     /** @name funcName accessor      */            \
@@ -43,24 +139,24 @@
 //-----------------------------------
 // Setter
 //-----------------------------------
-#define fwSettersMacro(funcName, var, type)     \
-    fwSetMacro(funcName, var, type);            \
+#define fwSettersMacro(funcName, var, type) \
+    fwSetMacro(funcName, var, type);        \
     fwSetCRefMacro(funcName, var, type);
 
 //-----------------------------------
 //
-#define fwSetMacro(funcName, var, type)               \
-    void set##funcName (const type _##var)            \
-    {                                                 \
-        this->m_##var = _##var;                       \
+#define fwSetMacro(funcName, var, type)    \
+    void set##funcName (const type _##var) \
+    {                                      \
+        this->m_##var = _##var;            \
     }
 
 //-----------------------------------
 //
-#define fwSetCRefMacro(funcName, var, type)                 \
-    void setCRef##funcName (const type & _##var)            \
-    {                                                       \
-        this->m_##var = _##var;                             \
+#define fwSetCRefMacro(funcName, var, type)      \
+    void setCRef##funcName (const type & _##var) \
+    {                                            \
+        this->m_##var = _##var;                  \
     }
 
 //-----------------------------------
@@ -69,61 +165,62 @@
 //-----------------------------------
 // Getter
 //-----------------------------------
-#define fwGettersMacro(funcName, var, type)      \
-    fwGetMacro(funcName, var, type);             \
-    fwGetRefMacro(funcName, var, type);          \
+#define fwGettersMacro(funcName, var, type) \
+    fwGetMacro(funcName, var, type);        \
+    fwGetRefMacro(funcName, var, type);     \
     fwGetCRefMacro(funcName, var, type);
 
 
 //-----------------------------------
 //
-#define fwGetMacro(funcName, var, type)          \
-    const type get##funcName () const            \
-    {                                            \
-        return m_##var;                          \
+#define fwGetMacro(funcName, var, type) \
+    const type get##funcName () const   \
+    {                                   \
+        return m_##var;                 \
     }
 
 //-----------------------------------
 //
-#define fwGetRefMacro(funcName, var, type)     \
-    type & getRef##funcName ()                 \
-    {                                          \
-        return this->m_##var;                  \
+#define fwGetRefMacro(funcName, var, type) \
+    type & getRef##funcName ()             \
+    {                                      \
+        return this->m_##var;              \
     }
 
 //-----------------------------------
 //
-#define fwGetCRefMacro(funcName, var, type)            \
-    const type & getCRef##funcName () const            \
-    {                                                  \
-        return this->m_##var;                          \
+#define fwGetCRefMacro(funcName, var, type) \
+    const type & getCRef##funcName () const \
+    {                                       \
+        return this->m_##var;               \
     }
+
 
 
 //-----------------------------------
 // Deep/Shallow copy
 //-----------------------------------
 
-#define fwDataShallowCopyMacro()                                                    \
-    void shallowCopy( ::fwData::Object::csptr _source )                            \
-    {                                                                               \
-        ::fwData::Object::shallowCopy< __FWCORE_TYPEDEF_SELF_NAME >( _source );    \
-    };                                                                              \
-    void shallowCopy( __FWCORE_TYPEDEF_SELF_NAME::sptr _source )                    \
-    {                                                                               \
-        ::fwData::Object::shallowCopy< __FWCORE_TYPEDEF_SELF_NAME >( _source );    \
+#define fwDataShallowCopyMacro()                                                \
+    void shallowCopy( ::fwData::Object::csptr _source )                         \
+    {                                                                           \
+        ::fwData::Object::shallowCopy< __FWCORE_TYPEDEF_SELF_NAME >( _source ); \
+    };                                                                          \
+    void shallowCopy( __FWCORE_TYPEDEF_SELF_NAME::sptr _source )                \
+    {                                                                           \
+        ::fwData::Object::shallowCopy< __FWCORE_TYPEDEF_SELF_NAME >( _source ); \
     };
 
 //-----------------------------------
 
-#define fwDataDeepCopyMacro()                                                       \
-    void deepCopy( ::fwData::Object::csptr _source )                               \
-    {                                                                               \
-        ::fwData::Object::deepCopy< __FWCORE_TYPEDEF_SELF_NAME >( _source );       \
-    };                                                                              \
-    void deepCopy( __FWCORE_TYPEDEF_SELF_NAME::sptr _source )                       \
-    {                                                                               \
-        ::fwData::Object::deepCopy< __FWCORE_TYPEDEF_SELF_NAME >( _source );       \
+#define fwDataDeepCopyMacro()                                                \
+    void deepCopy( ::fwData::Object::csptr _source )                         \
+    {                                                                        \
+        ::fwData::Object::deepCopy< __FWCORE_TYPEDEF_SELF_NAME >( _source ); \
+    };                                                                       \
+    void deepCopy( __FWCORE_TYPEDEF_SELF_NAME::sptr _source )                \
+    {                                                                        \
+        ::fwData::Object::deepCopy< __FWCORE_TYPEDEF_SELF_NAME >( _source ); \
     };
 
 
@@ -136,3 +233,5 @@
     fwDataShallowCopyMacro();                   \
     fwDataDeepCopyMacro();                      \
     /**@}                                    */
+
+#endif //__FWDATA_MACROS_HPP__
