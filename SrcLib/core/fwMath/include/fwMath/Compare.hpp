3a81b82202b1bee61efa68ca0473213c7eb7488a
Julien Waechter <julien.waechter@gmail.com>
2012-04-16 12:48:22 +0200

    see#2083 : Added helper to compare float/double value and container (basic version)
    --HG--
    branch : fw4spl_0.8.3
    extra : rebase_source : ff494b7a7a50c22ae93c891d7aacf76a68348ea9

---
+++ SrcLib/core/fwMath/include/fwMath/Compare.hpp
@@ -0,0 +1,57 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2011-2012.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef __FWMATH_COMPARE_HPP__
+#define __FWMATH_COMPARE_HPP__
+
+namespace fwMath
+{
+
+/**
+ * @brief Returns true iff container a and b are equal with 'epsilon' error margin
+ * @param containerA container to compare
+ * @param containerB container to compare
+ * @param epsilon : error margin
+ * @return true iff container a and b are equal with 'epsilon' error margin
+ */
+template < typename CONTAINER >
+bool isContainerEqual(CONTAINER&  containerA, CONTAINER& containerB, const float epsilon = 0.00001)
+{
+    bool isEqual = true;
+    if(containerA.size() != containerB.size())
+    {
+        return false;
+    }
+
+    CONTAINER::const_iterator iterA = containerA.begin();
+    CONTAINER::const_iterator iterB = containerB.begin();
+
+    while(isEqual && iterA != containerA.end())
+    {
+        isEqual = ::fwMath::isEqual(*iterA, *iterB, epsilon);
+        ++iterA;
+        ++iterB;
+    }
+
+    return isEqual;
+}
+
+/**
+ * @brief Returns true iff a and b are equal with 'epsilon' error margin
+ * @param a value to compare
+ * @param b value to compare
+ * @param epsilon : error margin
+ * @return true iff a and b are equal with 'epsilon' error margin
+ */
+template < typename TYPE >
+bool isEqual(TYPE a, TYPE b, const float epsilon = 0.00001)
+{
+    return fabs(a - b) < epsilon;
+}
+
+} // namespace fwMath
+
+#endif /* __FWMATH_COMPARE_HPP__ */
