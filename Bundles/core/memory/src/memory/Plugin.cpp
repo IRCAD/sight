6c1acda5c3b35917524b38d9012daee0bee76a5e
Nicolas Philipps <nicolasph@gmail.com>
2012-04-18 12:53:19 +0200

    refs #2087 Removed old memory dump services. Added setup of fwMemory buffer manager.
    --HG--
    branch : fw4spl_0.8.3
    extra : rebase_source : 1753c8c8b1d2866f62fa31645c80efd83928f73d

--- Bundles/core/memory/src/memory/Plugin.cpp
+++ Bundles/core/memory/src/memory/Plugin.cpp
@@ -12,9 +12,7 @@
 #include <fwTools/ClassRegistrar.hpp>
 #include <fwTools/System.hpp>
 
-#include <fwMemory/Initializer.hpp>
-#include <fwMemory/MemoryMonitor.hpp>
-#include <fwMemory/IDumpPolicy.hpp>
+#include <fwMemory/BufferManager.hpp>
 
 #include "memory/Plugin.hpp"
 
@@ -30,33 +28,14 @@ Plugin::~Plugin() throw()
 
 void Plugin::start() throw(::fwRuntime::RuntimeException)
 {
-    ::fwTools::Factory::addInitializer( ::fwMemory::Initializer::New() ) ;
+    ::fwTools::IBufferManager::setCurrent( ::fwMemory::BufferManager::New() );
 
-    if( this->getBundle()->hasParameter("Policy") )
-    {
-        SLM_INFO("Memory policy specified by monitor component") ;
-        std::string policyName = this->getBundle()->getParameterValue("Policy") ;
-        typedef std::map< std::string , ::boost::shared_ptr< ::fwRuntime::ConfigurationElement > > MapType ;
-        MapType ids = ::fwRuntime::getAllIdAndConfigurationElementsForPoint( "::memory::DumpPolicy" ) ;
-        if( ids.find(policyName) != ids.end() )
-        {
-            ::boost::shared_ptr< ::fwMemory::IDumpPolicy > memPolicy = ::fwTools::ClassFactoryRegistry::create< ::fwMemory::IDumpPolicy , std::string >( policyName ) ;
-            SLM_INFO("Affecting Memory policy to the memory monitor") ;
-            ::fwMemory::MemoryMonitor::setPolicy( memPolicy ) ;
-            memPolicy->configure( ids[policyName] );
-        }
-        else
-        {
-            SLM_WARN("Bad memory policy identifier") ;
-        }
-    }
 }
 
 //------------------------------------------------------------------------------
 
 void Plugin::stop() throw()
 {
-    ::fwTools::System::getDefault()->eraseDumpFolderOfZombies();
 }
 
 //------------------------------------------------------------------------------
