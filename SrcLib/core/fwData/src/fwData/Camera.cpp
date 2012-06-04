b3f91bbe4a22fc8cf31db246a8b8677581e09a45
Matthieu Hauglustaine <matt.hauglustaine@gmail.com>
2012-06-04 15:07:43 +0200

    Modified the fwData::Camera class adding the skew and distortion coefficients.
    --HG--
    branch : fw4spl_0.8.3

--- SrcLib/core/fwData/src/fwData/Camera.cpp
+++ SrcLib/core/fwData/src/fwData/Camera.cpp
@@ -5,62 +5,49 @@
  * ****** END LICENSE BLOCK ****** */
 
 #include <fwCore/base.hpp>
+
 #include "fwData/registry/macros.hpp"
-
 #include "fwData/Video.hpp"
-
 #include "fwData/Camera.hpp"
 
-fwDataRegisterMacro( ::fwData::Camera );
+fwDataRegisterMacro(::fwData::Camera);
+
 namespace fwData
 {
-//------------------------------------------------------------------------------
+    Camera::Camera()
+        : m_attrSkew(0.0), m_dataAvailable(false)
+    {
+        SLM_TRACE_FUNC();
 
-Camera::Camera()
-{
-    SLM_TRACE_FUNC();
-    m_extrinsicMatrix = ::fwData::TransformationMatrix3D::New();
-    m_intrinsicMatrix = ::fwData::TransformationMatrix3D::New();
-    uint32HL def;
-    def.Low = 0;
-    def.High = 0;
-    m_cameraGUID = std::make_pair(false, def);
-    m_dataAvailable = false ;
-}
+        m_extrinsicMatrix = ::fwData::TransformationMatrix3D::New();
+        m_intrinsicMatrix = ::fwData::TransformationMatrix3D::New();
 
-//------------------------------------------------------------------------------
+        uint32HL def = { 0, 0 };
+        m_cameraGUID = std::make_pair(false, def);
+    }
 
-Camera::~Camera() throw()
-{
-    SLM_TRACE_FUNC();
-}
+    // -------------------------------------------------------------------------
 
-//------------------------------------------------------------------------------
+    Camera::~Camera() throw()
+    { SLM_TRACE_FUNC(); }
 
-bool &Camera::dataAvailable()
-{
-    return m_dataAvailable;
-}
+    // -------------------------------------------------------------------------
 
-//------------------------------------------------------------------------------
+    void Camera::setCameraGUID(uint32HL cameraGUID)
+    {
+        m_cameraGUID.first = true;
+        m_cameraGUID.second = cameraGUID;
+    }
 
-void Camera::setExtrinsicCameraTransformation( ::fwData::TransformationMatrix3D::sptr  transMat)
-{
-    m_extrinsicMatrix = transMat;
-}
+    // -------------------------------------------------------------------------
 
-//------------------------------------------------------------------------------
-
-void Camera::setIntrinsicCameraTransformation( ::fwData::TransformationMatrix3D::sptr  transMat)
-{
-    m_intrinsicMatrix = transMat;
-}
-
-//------------------------------------------------------------------------------
-
-void Camera::setCameraGUID(uint32HL _cameraGUID){
-    m_cameraGUID.first = true;
-    m_cameraGUID.second = _cameraGUID;
-}
-
+    void Camera::setDistortionCoefficient(double k1, double k2,
+                                          double p1, double p2, double k3)
+    {
+        m_attrDistortionCoefficient[0] = k1;
+        m_attrDistortionCoefficient[1] = k2;
+        m_attrDistortionCoefficient[2] = p1;
+        m_attrDistortionCoefficient[3] = p2;
+        m_attrDistortionCoefficient[4] = k3;
+    }
 } // namespace fwData
