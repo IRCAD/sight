40e439e943248328e9267bfb8bd353e766ae6661
emilie.harquel, pmonnier <none@none>
2012-07-23 14:57:04 +0200

    see #2229 Updated fwComEd message unit test : check message creation using factory
    --HG--
    branch : fw4spl_0.9.0
    extra : rebase_source : e267d4132149932820ea66980a7ce06dcec8d60e

--- SrcLib/core/fwComEd/test/tu/include/MessagesTest.hpp
+++ SrcLib/core/fwComEd/test/tu/include/MessagesTest.hpp
@@ -25,6 +25,7 @@ class MessagesTest : public CPPUNIT_NS::TestFixture
     CPPUNIT_TEST( testFloatMsg );
     CPPUNIT_TEST( testImageMsg );
     CPPUNIT_TEST( testPatientDBMsg );
+    CPPUNIT_TEST( messageCreationTest );
     CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -42,6 +43,8 @@ public:
 
     /// test PatientDBMsg
     void testPatientDBMsg();
+
+    void messageCreationTest();
 };
 
 } //namespace ut
