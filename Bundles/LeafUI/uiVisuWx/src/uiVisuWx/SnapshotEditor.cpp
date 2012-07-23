de95ce3bf611710d4ff1996ace50c1fbe425c9eb
arnaud.charnoz@gmail.com <arnaud.charnoz@gmail.com>
2012-07-23 09:56:42 +0200

    see #2230 Fixed compil, cannot create a fwData::Object
    --HG--
    branch : fw4spl_0.9.0
    extra : rebase_source : 515135709e6ecdb92a920ac5da603673f6eb439a

--- Bundles/LeafUI/uiVisuWx/src/uiVisuWx/SnapshotEditor.cpp
+++ Bundles/LeafUI/uiVisuWx/src/uiVisuWx/SnapshotEditor.cpp
@@ -18,6 +18,7 @@
 
 #include <fwTools/Object.hpp>
 
+#include <fwData/Object.hpp>
 #include <fwData/String.hpp>
 #include <fwData/Composite.hpp>
 #include <fwData/location/SingleFile.hpp>
@@ -166,7 +167,7 @@ void SnapshotEditor::onSnapButton( wxCommandEvent& event )
             ::fwData::Composite::sptr composite = service->getObject< ::fwData::Composite >();
             SLM_ASSERT("SnapshotEditor sceneUID " << m_scenesUID.at(i) <<" isn't a GenericScene?" , composite);
 
-            ::fwData::Object::NewSptr dataInfo;
+            ::fwData::String::NewSptr dataInfo;
 
             ::fwData::String::NewSptr sceneID;
             sceneID->value() = m_scenesUID.at(i);
