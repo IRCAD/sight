26cafd66f4bb891f73d5e6c6c2d7f65bbc71808c
Arnaud Charnoz <arnaud.charnoz@gmail.com>
2012-09-06 16:52:48 +0200

    Updated fwDataCamp
    --HG--
    branch : fw4spl_0.9.0

--- SrcLib/core/fwDataCamp/src/fwDataCamp/Object.cpp
+++ SrcLib/core/fwDataCamp/src/fwDataCamp/Object.cpp
@@ -6,5 +6,6 @@ fwCampImplementDataMacro((fwData)(Object))
 {
     builder
         .property("fields", &::fwData::Object::m_fields)
-    ;
+        .function("classname", (std::string (fwData::Object::*)() const) &::fwData::Object::className)
+        ;
 }
