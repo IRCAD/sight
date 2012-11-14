2ced58725448a3482594bd41f00d218b5b163962
Emilie Harquel <emilie.har@gmail.com>
2012-11-14 17:17:06 +0100

    see #2291 : Added fwTest unit test for Worker
    --HG--
    branch : fw4spl_0.9.1

---
+++ SrcLib/core/fwThread/test/tu/include/WorkerTest.hpp
@@ -0,0 +1,34 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2012.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef __FWTHREAD_UT_WORKERTEST_HPP__
+#define __FWTHREAD_UT_WORKERTEST_HPP__
+
+#include <cppunit/extensions/HelperMacros.h>
+
+namespace fwThread
+{
+namespace ut
+{
+
+class WorkerTest : public CPPUNIT_NS::TestFixture
+{
+    CPPUNIT_TEST_SUITE( WorkerTest );
+    CPPUNIT_TEST( basicTest );
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    // interface
+    void setUp();
+    void tearDown();
+
+    void basicTest();
+
+};
+
+} //namespace ut
+} //namespace fwThread
+#endif //__FWTHREAD_UT_WORKERTEST_HPP__
---
+++ SrcLib/core/fwThread/test/tu/src/WorkerTest.cpp
@@ -0,0 +1,86 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2012.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#include <iostream>
+#include <exception>
+
+#include <fwCore/spyLog.hpp>
+
+#include <fwTest/Exception.hpp>
+
+#include <fwThread/Worker.hpp>
+
+#include "WorkerTest.hpp"
+
+
+// Registers the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION( ::fwThread::ut::WorkerTest );
+
+namespace fwThread
+{
+namespace ut
+{
+
+static ::fwTest::Exception e(""); // force link with fwTest
+
+void WorkerTest::setUp()
+{
+    // Set up context before running a test.
+}
+void WorkerTest::tearDown()
+{
+    // Clean up after the test run.
+}
+
+
+//-----------------------------------------------------------------------------
+
+struct TestHandler
+{
+    TestHandler() : m_step(0), m_threadCheckOk(true)
+    {
+        m_constructorThreadId = ::fwThread::getCurrentThreadId();
+    }
+
+    void nextStep()
+    {
+        m_threadCheckOk &= (m_constructorThreadId != ::fwThread::getCurrentThreadId());
+        m_threadCheckOk &= (m_workerThreadId == ::fwThread::getCurrentThreadId());
+        ++m_step;
+    }
+
+    void setWorkerId(::fwThread::ThreadIdType id)
+    {
+        m_workerThreadId = id;
+    }
+
+    int m_step;
+    bool m_threadCheckOk;
+    ::fwThread::ThreadIdType m_constructorThreadId;
+    ::fwThread::ThreadIdType m_workerThreadId;
+};
+
+//-----------------------------------------------------------------------------
+
+void WorkerTest::basicTest()
+{
+    ::fwThread::Worker::sptr worker = ::fwThread::Worker::New();
+
+    TestHandler handler;
+    handler.setWorkerId(worker->getThreadId());
+    worker->post( ::boost::bind( &TestHandler::nextStep, &handler) );
+    worker->post( ::boost::bind( &TestHandler::nextStep, &handler) );
+    worker->post( ::boost::bind( &TestHandler::nextStep, &handler) );
+
+    worker->stop();
+    CPPUNIT_ASSERT_EQUAL(3, handler.m_step);
+    CPPUNIT_ASSERT_EQUAL(true, handler.m_threadCheckOk);
+}
+
+//-----------------------------------------------------------------------------
+
+} //namespace ut
+} //namespace fwThread
