9c0f8444dc28774d595c33d05135529d8371953e
arnaud.charnoz@gmail.com <arnaud.charnoz@gmail.com>
2012-12-20 15:16:53 +0100

    Fix debug log in DictionaryReader
    --HG--
    branch : fw4spl_0.9.1

--- SrcLib/core/fwDataIO/src/fwDataIO/reader/DictionaryReader.cpp
+++ SrcLib/core/fwDataIO/src/fwDataIO/reader/DictionaryReader.cpp
@@ -4,15 +4,18 @@
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
 
-#ifdef DEBUG
-  #define BOOST_SPIRIT_DEBUG
-#endif
-
 #define FUSION_MAX_VECTOR_SIZE 20
 
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
+
+#ifdef DEBUG
+  static std::stringstream spiritDebugStream;
+  #define BOOST_SPIRIT_DEBUG_OUT spiritDebugStream
+  #define BOOST_SPIRIT_DEBUG
+#endif
 
 #include <boost/cstdint.hpp>
 #include <boost/algorithm/string.hpp>
@@ -195,6 +198,8 @@ struct line_parser : qi::grammar<Iterator, std::vector <line>() >
       BOOST_SPIRIT_DEBUG_NODE(dbl);
       BOOST_SPIRIT_DEBUG_NODE(line);
       BOOST_SPIRIT_DEBUG_NODE(lines);
+      SLM_DEBUG(spiritDebugStream.str());
+      spiritDebugStream.str( std::string() );
     #endif
 
       qi::on_error< qi::fail>
