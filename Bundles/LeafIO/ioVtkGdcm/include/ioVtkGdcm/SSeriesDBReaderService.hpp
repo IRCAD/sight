d5fa9c26bd1dc0c5023485b3640afc400b8bb922
Nicolas Philipps <nicolasph@gmail.com>
2013-02-19 11:33:31 +0100

    refs #2358 : Added vtk-gdcm SeriesDB Reader service
    --HG--
    branch : fw4spl_0.9.2
    extra : rebase_source : ee0cc7439959e6ccfc84772e4d86976ab2dff2a3

---
+++ Bundles/LeafIO/ioVtkGdcm/include/ioVtkGdcm/SSeriesDBReaderService.hpp
@@ -0,0 +1,85 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2013.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef __IOVTKGDCM_SSERIESDBREADERSERVICE_HPP__
+#define __IOVTKGDCM_SSERIESDBREADERSERVICE_HPP__
+
+#include <string>
+#include <boost/filesystem/path.hpp>
+
+#include <io/IReader.hpp>
+
+#include "ioVtkGdcm/config.hpp"
+
+namespace fwMedData
+{
+    class SeriesDB;
+    class Patient;
+}
+
+
+namespace ioVtkGdcm
+{
+
+class IOVTKGDCM_CLASS_API SSeriesDBReaderService : public ::io::IReader
+{
+
+public :
+    typedef std::string                  ExtensionType;
+    typedef std::vector< ExtensionType > ExtensionsType;
+
+    fwCoreServiceClassDefinitionsMacro ( (SSeriesDBReaderService)( ::io::IReader) ) ;
+    /**
+     * @brief   constructor
+     *
+     */
+    IOVTKGDCM_API SSeriesDBReaderService() throw();
+
+    /**
+     * @brief   destructor
+     */
+    IOVTKGDCM_API virtual ~SSeriesDBReaderService() throw();
+
+protected:
+
+    /// Override
+    IOVTKGDCM_API virtual void starting() throw(::fwTools::Failed);
+
+    /// Override
+    IOVTKGDCM_API virtual void stopping() throw(::fwTools::Failed);
+
+    /// Override
+    IOVTKGDCM_API void updating() throw(::fwTools::Failed);
+
+    /// Override
+    virtual void receiving( CSPTR(::fwServices::ObjectMsg) _msg ) throw(::fwTools::Failed){};
+
+    /// Override
+    IOVTKGDCM_API void info(std::ostream &_sstream ) ;
+
+    /// Override
+    IOVTKGDCM_API virtual ExtensionsType getSupportedExtensions() ;
+
+    /// Override
+    IOVTKGDCM_API virtual std::string getSelectorDialogTitle();
+
+    /// Override
+    IOVTKGDCM_API virtual void configureWithIHM();
+
+    /// Return path type managed by the service, here FOLDER
+    IOVTKGDCM_API ::io::IOPathType getIOPathType() const;
+
+private :
+
+    void notificationOfDBUpdate();
+
+    SPTR(::fwMedData::SeriesDB) createSeriesDB(const ::boost::filesystem::path& dicomDir);
+
+};
+
+} // namespace ioDicom
+
+#endif //__IOVTKGDCM_SSERIESDBREADERSERVICE_HPP__
---
+++ Bundles/LeafIO/ioVtkGdcm/src/ioVtkGdcm/SSeriesDBReaderService.cpp
@@ -0,0 +1,191 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2013.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#include <boost/bind.hpp>
+
+#include <fwCore/base.hpp>
+
+#include <fwServices/Base.hpp>
+#include <fwServices/macros.hpp>
+#include <fwServices/registry/ObjectService.hpp>
+#include <fwServices/IEditionService.hpp>
+//#include <fwComEd/PatientDBMsg.hpp>
+
+#include <fwTools/ProgressToLogger.hpp>
+
+#include <fwGui/dialog/ProgressDialog.hpp>
+#include <fwGui/dialog/MessageDialog.hpp>
+#include <fwGui/dialog/LocationDialog.hpp>
+#include <fwGui/Cursor.hpp>
+
+#include <io/IReader.hpp>
+
+#include <fwMedData/SeriesDB.hpp>
+
+#include <vtkGdcmIO/SeriesDBReader.hpp>
+
+#include "ioVtkGdcm/SSeriesDBReaderService.hpp"
+
+
+namespace ioVtkGdcm
+{
+
+fwServicesRegisterMacro( ::io::IReader , ::ioVtkGdcm::SSeriesDBReaderService , ::fwMedData::SeriesDB ) ;
+
+//------------------------------------------------------------------------------
+
+SSeriesDBReaderService::SSeriesDBReaderService() throw()
+{}
+
+//------------------------------------------------------------------------------
+
+SSeriesDBReaderService::~SSeriesDBReaderService() throw()
+{}
+
+//------------------------------------------------------------------------------
+
+void SSeriesDBReaderService::configureWithIHM()
+{
+    static ::boost::filesystem::path _sDefaultPath;
+
+    ::fwGui::dialog::LocationDialog dialogFile;
+    dialogFile.setTitle(this->getSelectorDialogTitle());
+    dialogFile.setDefaultLocation( ::fwData::location::Folder::New(_sDefaultPath) );
+    dialogFile.setOption(::fwGui::dialog::ILocationDialog::READ);
+    dialogFile.setType(::fwGui::dialog::LocationDialog::FOLDER);
+
+    ::fwData::location::Folder::sptr  result;
+    result= ::fwData::location::Folder::dynamicCast( dialogFile.show() );
+    if (result)
+    {
+        _sDefaultPath = result->getFolder();
+        this->setFolder( result->getFolder() );
+        dialogFile.saveDefaultLocation( ::fwData::location::Folder::New(_sDefaultPath) );
+    }
+}
+
+//------------------------------------------------------------------------------
+
+void SSeriesDBReaderService::starting() throw(::fwTools::Failed)
+{
+    SLM_TRACE_FUNC();
+}
+
+//------------------------------------------------------------------------------
+
+void SSeriesDBReaderService::stopping() throw(::fwTools::Failed)
+{
+    SLM_TRACE_FUNC();
+}
+
+//------------------------------------------------------------------------------
+
+void SSeriesDBReaderService::info(std::ostream &_sstream )
+{
+    _sstream << "SSeriesDBReaderService::info" ;
+}
+
+//------------------------------------------------------------------------------
+
+SSeriesDBReaderService::ExtensionsType SSeriesDBReaderService::getSupportedExtensions()
+{
+    ExtensionsType extensions ;
+    return extensions ;
+}
+
+//------------------------------------------------------------------------------
+
+std::string SSeriesDBReaderService::getSelectorDialogTitle()
+{
+    return "Choose a directory with DICOM images";
+}
+
+
+//------------------------------------------------------------------------------
+
+::fwMedData::SeriesDB::sptr SSeriesDBReaderService::createSeriesDB(const ::boost::filesystem::path& dicomDir)
+{
+    SLM_TRACE_FUNC();
+    ::vtkGdcmIO::SeriesDBReader::NewSptr myLoader;
+    ::fwMedData::SeriesDB::NewSptr dummy;
+    myLoader->setObject(dummy);
+    myLoader->setFolder(dicomDir);
+
+    try
+    {
+        ::fwGui::dialog::ProgressDialog progressMeterGUI("Loading Dicom Image");
+        myLoader->addHandler( progressMeterGUI );
+        myLoader->read();
+    }
+    catch (const std::exception & e)
+    {
+        std::stringstream ss;
+        ss << "Warning during loading : " << e.what();
+        ::fwGui::dialog::MessageDialog::showMessageDialog(
+                "Warning", ss.str(), ::fwGui::dialog::IMessageDialog::WARNING);
+    }
+    catch( ... )
+    {
+        ::fwGui::dialog::MessageDialog::showMessageDialog(
+                "Warning", "Warning during loading", ::fwGui::dialog::IMessageDialog::WARNING);
+    }
+
+    return myLoader->getConcreteObject();
+}
+
+//------------------------------------------------------------------------------
+
+void SSeriesDBReaderService::updating() throw(::fwTools::Failed)
+{
+    SLM_TRACE_FUNC();
+    if( this->hasLocationDefined() )
+    {
+        ::fwMedData::SeriesDB::sptr seriesDB = createSeriesDB( this->getFolder() );
+
+        if( seriesDB->size() > 0 )
+        {
+            // Retrieve dataStruct associated with this service
+            ::fwMedData::SeriesDB::sptr associatedSeriesDB = this->getObject< ::fwMedData::SeriesDB >();
+            SLM_ASSERT("associated SeriesDB not instanced", associatedSeriesDB);
+            associatedSeriesDB->shallowCopy( seriesDB ) ;
+
+            ::fwGui::Cursor cursor;
+            cursor.setCursor(::fwGui::ICursor::BUSY);
+            this->notificationOfDBUpdate();
+            cursor.setDefaultCursor();
+        }
+        else
+        {
+            ::fwGui::dialog::MessageDialog::showMessageDialog(
+                    "Image Reader","This file can not be read. Retry with another file reader.", ::fwGui::dialog::IMessageDialog::WARNING);
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+
+void SSeriesDBReaderService::notificationOfDBUpdate()
+{
+    SLM_TRACE_FUNC();
+    ::fwMedData::SeriesDB::sptr pDPDB = this->getObject< ::fwMedData::SeriesDB >() ;
+    SLM_ASSERT("pDPDB not instanced", pDPDB);
+
+    //::fwComEd::PatientDBMsg::NewSptr msg;
+    //msg->addEvent( ::fwComEd::PatientDBMsg::NEW_PATIENT );
+
+    //::fwServices::IEditionService::notify(this->getSptr(),  pDPDB, msg);
+}
+
+//-----------------------------------------------------------------------------
+
+::io::IOPathType SSeriesDBReaderService::getIOPathType() const
+{
+    return ::io::FOLDER;
+}
+
+//------------------------------------------------------------------------------
+
+} // namespace ioInr
