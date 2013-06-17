72de617cff79e962e168a4b2a6ee6c298e34ee91
Emilie Harquel <emilie.har@gmail.com>
2013-06-17 09:39:08 +0200

    Added patchMedicalData bundle. It is associated to fwStructuralPatch and fwMDSemanticPatch library.
    --HG--
    branch : fw4spl_0.9.2

---
+++ Bundles/LeafPatch/patchMedicalData/include/patchMedicalData/namespace.hpp
@@ -0,0 +1,18 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2013.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef __PATCHMEDICALDATA_NAMESPACE_HPP__
+#define __PATCHMEDICALDATA_NAMESPACE_HPP__
+
+/**
+ * @brief       The bundle patchMedicalData allows to load patches from fwStructuralPatch and fwMDSemanticPatch library.
+ * @namespace   patchMedicalData
+ */
+namespace patchMedicalData
+{
+
+}
+#endif /* __PATCHMEDICALDATA_NAMESPACE_HPP__ */
