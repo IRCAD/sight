207e28c47f27098009294b46900619be3bbfbe7e
arnaud.charnoz@gmail.com <arnaud.charnoz@gmail.com>
2013-10-23 16:30:50 +0200

    Create a new filterVRRender bundle to manage filtering in VRRender
    --HG--
    branch : fw4spl_0.9.1
    extra : transplant_source : %22%07%C7%AA%9F%CE%9DN9%A0I%5D%88/7T%04%9DqF

---
+++ Bundles/LeafAtomFilter/filterVRRender/test/tu/include/MedicalDataV1Test.hpp
@@ -0,0 +1,46 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2013.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef __FILTERVRRENDER_UT_MEDICALDATAV1TEST_HPP__
+#define __FILTERVRRENDER_UT_MEDICALDATAV1TEST_HPP__
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <fwRuntime/EConfigurationElement.hpp>
+
+#include <fwData/Patient.hpp>
+
+namespace filterVRRender
+{
+namespace ut
+{
+
+/**
+ * @brief Test ::fwAtomsFilter::IFilter::apply method.
+ */
+class MedicalDataV1Test : public CPPUNIT_NS::TestFixture
+{
+    CPPUNIT_TEST_SUITE( MedicalDataV1Test );
+    CPPUNIT_TEST( applyTest );
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+
+    /**
+     * @name Interface
+     * @{ */
+    void setUp();
+    void tearDown();
+    /**  @} */
+
+    void applyTest();
+
+};
+
+} // namespace ut
+} // namespace filterVRRender
+
+#endif //__FILTERVRRENDER_UT_MEDICALDATAV1TEST_HPP__
