fc61deef3469de76b4fd13e6546cde509160fe18
Emilie Harquel <emilie.har@gmail.com>
2014-02-24 12:33:32 +0100

    Fixed wrong warning in PushObjectSrv.
    --HG--
    branch : fw4spl_0.9.2
    extra : source : f82fc3e70dfb9ff3c7c25e420c47fadfa8c18728

--- Bundles/core/gui/src/gui/action/PushObjectSrv.cpp
+++ Bundles/core/gui/src/gui/action/PushObjectSrv.cpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2012.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2014.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -122,7 +122,7 @@ void PushObjectSrv::updating() throw(::fwTools::Failed)
 
         ::fwData::Composite::const_iterator iter = composite_src->find(src_key);
 
-        OSLM_WARN_IF("'" << src_key << "' not found in composite '" << src_uid << "'" ,iter != composite_src->end());
+        OSLM_WARN_IF("'" << src_key << "' not found in composite '" << src_uid << "'" ,iter == composite_src->end());
         if (iter != composite_src->end())
         {
             ::fwData::Object::sptr obj = composite_src->getContainer()[src_key];
