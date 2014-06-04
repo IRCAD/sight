fe456bfe5bbdbefc62c70f8635f75c3fdeb689d7
Valentin Martinet <martinetvalentin@gmail.com>
2014-06-04 11:18:58 +0200

    Fixed UT working directory
    --HG--
    branch : fw4spl_0.9.2

--- Bundles/LeafIO/ioAtoms/test/Properties.cmake
+++ Bundles/LeafIO/ioAtoms/test/Properties.cmake
@@ -2,7 +2,7 @@
 set( NAME ioAtomsTest )
 set( VERSION  )
 set( TYPE TEST )
-set( DEPENDENCIES fwCore fwTest fwTools fwData fwDataTools fwDataCamp fwAtomConversion )
-set( REQUIREMENTS ioAtoms )
+set( DEPENDENCIES fwCore fwTest fwTools fwData fwDataTools fwDataCamp fwAtomConversion)
+set( REQUIREMENTS ioAtoms)
 
-set(CPPUNITTEST_OPTIONS BUNDLE ioAtoms)
+set(CPPUNITTEST_OPTIONS BUNDLE ioAtoms WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
--- Bundles/LeafUI/guiQt/test/Properties.cmake
+++ Bundles/LeafUI/guiQt/test/Properties.cmake
@@ -5,4 +5,4 @@ set( TYPE TEST )
 set( DEPENDENCIES fwTest fwCore fwTools fwData fwGuiQt )
 set( REQUIREMENTS dataReg servicesReg gui guiQt io ioVTK visu visuVTKQt vtkSimpleNegato Tuto01Basic Tuto02DataServiceBasic )
 
-set(CPPUNITTEST_OPTIONS BUNDLE guiQt)
+set(CPPUNITTEST_OPTIONS BUNDLE guiQt WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
