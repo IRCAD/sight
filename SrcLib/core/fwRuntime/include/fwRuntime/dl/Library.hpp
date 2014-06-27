6cd421f27c4c74591d9e17b29212ff4b0ce2c0a1
Valentin Martinet <martinetvalentin@gmail.com>
2014-06-27 16:48:56 +0200

    Fixed fwRuntime compilation on Linux
    --HG--
    branch : fw4spl_0.9.2

--- SrcLib/core/fwRuntime/include/fwRuntime/dl/Library.hpp
+++ SrcLib/core/fwRuntime/include/fwRuntime/dl/Library.hpp
@@ -149,14 +149,14 @@ private:
 };
 
 
-#ifdef linux
+#if defined(linux) || defined(__linux)
     typedef struct LibraryBridge< Posix > Library;
 #else
 #   ifdef WIN32
     typedef struct LibraryBridge< Win32 > Library;
 #   else
 #       ifdef __MACOSX__
-            typedef struct LibraryBridge< Darwin > Library;
+    typedef struct LibraryBridge< Darwin > Library;
 #       endif
 #   endif
 #endif
