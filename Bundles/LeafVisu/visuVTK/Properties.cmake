5bc63c6082271ef62fbecd3c253fa840fe516010
Flavien Bridault <f.bridault@gmail.com>
2014-09-11 15:10:31 +0200

    Removed useless dependencies in CMakeLists.txt, especially against Boost.
    --HG--
    branch : fw4spl_0.10.0

--- Bundles/LeafVisu/visuVTK/Properties.cmake
+++ Bundles/LeafVisu/visuVTK/Properties.cmake
@@ -2,6 +2,6 @@
 set( NAME visuVTK )
 set( VERSION 0.1 )
 set( TYPE BUNDLE )
-set( DEPENDENCIES fwData fwRender fwRenderVTK fwRuntime fwServices )
+set( DEPENDENCIES fwRender fwRenderVTK fwRuntime fwServices )
 set( REQUIREMENTS visu )
 
