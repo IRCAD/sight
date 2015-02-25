a357f6ae940c31833ba386985574c2f447699fdc
Marc Schweitzer <marc.schw@gmail.com>
2015-02-25 17:20:26 +0100

    Added SrcLib for the support of OpenIGTLink protocol
    --HG--
    branch : fw4spl_0.10.1

---
+++ SrcLib/igtlProtocol/include/igtlProtocol/namespace.hpp
@@ -0,0 +1,43 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009-2014.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+/**
+ * @brief for support of OpenIGTLink protocol
+ **/
+namespace igtlProtocol
+{
+/**
+ * @brief converter  namespace for IConverter derived classes
+ **/
+namespace converter
+{
+
+} // namespace converter
+
+/**
+ * @brief archiver namespace for IReadArchive/IWriteArchiver (in-memory) implementation
+ */
+namespace archiver
+{
+
+} // namespace archiver
+
+/**
+ * @brief contain utilities
+ */
+namespace helper
+{
+/**
+ * @brief some detail to hide to the user of ScalarToBytes utility class
+ */
+namespace detail
+{
+
+} // namespace detail
+
+} // namespace helper
+
+} // namespace igtlProtocol
