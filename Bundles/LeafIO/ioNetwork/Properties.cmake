19da2dc081be2c70052e3b121eb89ec44d0c93f9
Marc Schweitzer <marc.schw@gmail.com>
2015-02-25 17:20:45 +0100

    Added interfaces for network listener/sender
    --HG--
    branch : fw4spl_0.10.1

---
+++ Bundles/LeafIO/ioNetwork/Properties.cmake
@@ -0,0 +1,13 @@
+set( NAME ioNetwork )
+set( VERSION 0.1 )
+set( TYPE BUNDLE )
+set( DEPENDENCIES
+    fwCore
+    fwTools
+    fwData
+    fwServices
+    fwCom
+    fwComEd
+    fwRuntime
+)
+set( REQUIREMENTS  )
