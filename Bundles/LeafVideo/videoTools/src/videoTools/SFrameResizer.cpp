cca8cf2e27707017fbe990eddf18a39b0a982f04
fbridault@ircad.fr <fbridault@ircad.fr>
2015-05-26 09:34:33 +0200

    Added the SFrameResizer service that resize frames from a timeline.
    --HG--
    branch : fw4spl_0.10.2

---
+++ Bundles/LeafVideo/videoTools/src/videoTools/SFrameResizer.cpp
@@ -0,0 +1,154 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2015.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#include "videoTools/SFrameResizer.hpp"
+
+#include <fwCom/Signal.hxx>
+#include <fwData/Composite.hpp>
+#include <fwServices/Base.hpp>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+namespace videoTools
+{
+
+fwServicesRegisterMacro( ::arServices::IOperator, ::videoTools::SFrameResizer, ::fwData::Composite);
+
+//------------------------------------------------------------------------------
+SFrameResizer::SFrameResizer() throw()
+{
+    m_connections = ::fwServices::helper::SigSlotConnection::New();
+}
+
+//------------------------------------------------------------------------------
+
+SFrameResizer::~SFrameResizer() throw()
+{
+}
+
+//------------------------------------------------------------------------------
+
+void SFrameResizer::configuring() throw(::fwTools::Failed)
+{
+    ::fwRuntime::ConfigurationElement::sptr config;
+
+    config = m_configuration->findConfigurationElement("input");
+    SLM_ASSERT("Missing input timeline in SFrameResizer configuration", config);
+    m_inputTLKey = config->getValue();
+
+    config = m_configuration->findConfigurationElement("output");
+    SLM_ASSERT("Missing output timeline in SFrameResizer configuration", config);
+    m_outputTLKey = config->getValue();
+
+    config = m_configuration->findConfigurationElement("ratio");
+    SLM_ASSERT("Missing ratio in SFrameResizer configuration", config);
+    const std::string x = config->getAttributeValue("x");
+    const std::string y = config->getAttributeValue("y");
+
+    m_ratio[0] = std::stof(x) / 100.f;
+    m_ratio[1] = std::stof(y) / 100.f;
+
+    config = m_configuration->findConfigurationElement("interpolation");
+
+    if(config)
+    {
+        m_interpolationMethod = ::cv::INTER_NEAREST;
+
+        const std::string method = config->getValue();
+        if(method == "linear")
+        {
+            m_interpolationMethod = ::cv::INTER_LINEAR;
+        }
+        else if(method == "area")
+        {
+            m_interpolationMethod = ::cv::INTER_AREA;
+        }
+        else if(method == "cubic")
+        {
+            m_interpolationMethod = ::cv::INTER_CUBIC;
+        }
+        else if(method == "lanczos4")
+        {
+            m_interpolationMethod = ::cv::INTER_LANCZOS4;
+        }
+        else if(method != "nearest")
+        {
+            OSLM_ERROR("Unknown '" << method << "' interpolation method. Defaulting to nearest filter." );
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+
+void SFrameResizer::starting() throw(::fwTools::Failed)
+{
+    ::fwData::Composite::sptr composite = this->getObject< ::fwData::Composite >();
+
+    m_inputTL = ::extData::FrameTL::dynamicCast((*composite)[m_inputTLKey]);
+    OSLM_ASSERT("The timeline \"" << m_inputTL << "\" is not valid.", m_inputTL);
+
+    m_outputTL = ::extData::FrameTL::dynamicCast((*composite)[m_outputTLKey]);
+    OSLM_ASSERT("The timeline \"" << m_outputTL << "\" is not valid.", m_outputTL);
+
+    m_connections->connect(m_inputTL, ::extData::TimeLine::s_OBJECT_PUSHED_SIG, this->getSptr(), s_UPDATE_SLOT);
+}
+
+//------------------------------------------------------------------------------
+
+void SFrameResizer::stopping() throw(::fwTools::Failed)
+{
+    m_connections->disconnect();
+}
+
+//------------------------------------------------------------------------------
+
+void SFrameResizer::updating() throw(::fwTools::Failed)
+{
+    // Retrieves the last timestamp
+    ::fwCore::HiResClock::HiResClockType timestamp = m_inputTL->getNewerTimestamp();
+
+    // Get the buffer of the copied timeline
+    CSPTR(::extData::FrameTL::BufferType) bufferFrameIn = m_inputTL->getClosestBuffer(timestamp);
+    const ::boost::uint8_t* frameBuffIn = &bufferFrameIn->getElement(0);
+
+    int width  = static_cast<int>( m_inputTL->getWidth() );
+    int height = static_cast<int>( m_inputTL->getHeight() );
+
+    // The cv image that will be processed
+    ::cv::Mat imgIn(::cv::Size(width, height), CV_8UC4, (void*)frameBuffIn, ::cv::Mat::AUTO_STEP);
+
+    size_t outWidth  = static_cast<size_t>( m_inputTL->getWidth() * m_ratio[0] );
+    size_t outHeight = static_cast<size_t>( m_inputTL->getHeight() * m_ratio[1] );
+
+    if((outWidth != m_outputTL->getWidth() || outHeight != m_outputTL->getHeight()))
+    {
+        m_outputTL->clearTimeline();
+        m_outputTL->initPoolSize(outWidth, outHeight, ::fwTools::Type::s_UINT8, 4);
+    }
+
+    // Get the buffer of the timeline to fill
+    SPTR(::extData::FrameTL::BufferType) bufferOut = m_outputTL->createBuffer(timestamp);
+    ::boost::uint8_t* frameBuffOut                 = bufferOut->addElement(0);
+
+    // Create an openCV mat that aliases the buffer created from the output timeline
+    ::cv::Size size(static_cast<int>(outWidth), static_cast<int>(outHeight));
+    ::cv::Mat imgOut(size, CV_8UC4, (void*)frameBuffOut, ::cv::Mat::AUTO_STEP);
+
+    ::cv::resize(imgIn, imgOut, size, 0, 0, m_interpolationMethod );
+
+    m_outputTL->pushObject(bufferOut);
+
+    auto sig =
+        m_outputTL->signal< ::extData::TimeLine::ObjectPushedSignalType >(::extData::TimeLine::s_OBJECT_PUSHED_SIG);
+    sig->asyncEmit(timestamp);
+}
+
+
+//------------------------------------------------------------------------------
+
+} // namespace videoTools
