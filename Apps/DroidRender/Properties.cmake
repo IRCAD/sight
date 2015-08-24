214841e3e4ffc1439d1488325aa8d9bf582ec30f
fchamp@IRCAD.FR <fchamp@IRCAD.FR>
2015-08-24 09:28:30 +0200

    Added Android samples: DroidSimpleGUI: same as PoC09Android (fw4spl-ext) but with a simple GUI DroidRender: Patient visualization with buttons to set view among axis
    --HG--
    branch : ARMobile03

---
+++ Apps/DroidRender/Properties.cmake
@@ -0,0 +1,28 @@
+
+set( NAME DroidRender )
+set( VERSION 0.1 )
+set( TYPE APP )
+set( PLATFORM DROID)
+set( DEPENDENCIES  )
+set( REQUIREMENTS
+    appXml
+    servicesReg
+    dataReg
+    launcher
+    visu
+    visuVTK
+    visuVTKAndroid
+    visuVTKAdaptor
+    guiDroid
+    io
+    ioVTK
+    ioAtoms
+    ctrlCamp
+    gui
+)
+set( JAVA_PACKAGES 
+    ndkgui
+)
+
+bundle_param(appXml PARAM_LIST config PARAM_VALUES DroidRenderConfig)
+
