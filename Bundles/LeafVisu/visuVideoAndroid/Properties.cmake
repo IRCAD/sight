2c6a6046e0c72559fd870bc0ce95c3f7b2e8c789
Emilie Harquel <emilie.har@gmail.com>
2015-09-04 12:10:57 +0200

    Removed Android files (moved to fw4spl-droid)

--- Bundles/LeafVisu/visuVideoAndroid/Properties.cmake
+++
@@ -1,8 +0,0 @@
-
-set( NAME visuVideoAndroid )
-set( VERSION 0.1 )
-set( TYPE BUNDLE )
-set( DEPENDENCIES fwActivities fwComEd fwCom fwCore fwData fwDataCamp fwRuntime fwServices fwTools arAndroidTools extData extDataReg)
-set( REQUIREMENTS )
-set( PLATFORM DROID )
-
