6bd132c635dab30f47be4ac770df8e6d397cb493
Marc Schweitzer <marc.schw@gmail.com>
2015-09-16 17:38:31 +0200

    New Poc with IGTL that send/receive the camera transform
    --HG--
    branch : fw4spl_0.10.2

---
+++ Apps/PoC08Igtl/Properties.cmake
@@ -0,0 +1,27 @@
+
+set( NAME PoC08Igtl )
+set( VERSION 0.1 )
+set( TYPE BUNDLE )
+set( DEPENDENCIES  )
+set( REQUIREMENTS
+    dataReg
+    extDataReg
+    gui
+    guiQt
+    servicesReg
+    visu
+    launcher
+    appXml
+    visuVTK
+    visuVTKQt
+    visuVTKAdaptor
+    media
+    mediaExt
+    io
+    ioVTK
+    ioNetwork
+    ioIGTL
+    ioData
+    uiNetwork
+    uiIGTL
+    )
