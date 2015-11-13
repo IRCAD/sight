d8ba77d07af45204be9c5c5b09e4adc598d8238a
clement.troesch <clement.troesch@visiblepatient.com>
2015-11-13 14:34:00 +0100

    Added fwDcmtkIO and fwPacsIO libraries
    --HG--
    branch : fw4spl_0.10.2

---
+++ SrcLib/fwPacsIO/Properties.cmake
@@ -0,0 +1,15 @@
+
+set( NAME fwPacsIO )
+set( VERSION 0.1 )
+set( TYPE LIBRARY )
+set( DEPENDENCIES
+    fwCore
+    fwTools
+    fwThread
+    fwData
+    fwMedData
+    fwCom
+    fwDicomData
+    fwDcmtkTools
+    )
+set( REQUIREMENTS )
