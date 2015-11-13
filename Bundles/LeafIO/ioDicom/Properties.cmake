2e8a08838ac5af14e26f8e8eeed67b7accaf0da8
clement.troesch <clement.troesch@visiblepatient.com>
2015-11-13 14:34:10 +0100

    Added ioDcmtk, ioDicom, ioGdcm and ioPacs bundles
    --HG--
    branch : fw4spl_0.10.2

---
+++ Bundles/LeafIO/ioDicom/Properties.cmake
@@ -0,0 +1,29 @@
+
+set( NAME ioDicom )
+set( VERSION 0.1 )
+set( TYPE BUNDLE )
+set( DEPENDENCIES
+    fwCom
+    fwComEd
+    fwCore
+    fwDicomIOFilter
+    fwDicomIOFilterQt
+    fwGui
+    fwGuiExt
+    fwGuiQt
+    fwJobs
+    fwLog
+    fwRuntime
+    fwServices
+    fwDicomData
+    fwTools
+    gui
+    io
+    fwData
+    fwMedData
+    fwThread
+    )
+set( REQUIREMENTS
+    dataReg
+    servicesReg
+    )
