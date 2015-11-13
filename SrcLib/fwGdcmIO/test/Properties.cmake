3d28285f57c3a05df53dcf330d26da2287808dc8
clement.troesch <clement.troesch@visiblepatient.com>
2015-11-13 14:34:05 +0100

    Added fwGdcmIO library
    --HG--
    branch : fw4spl_0.10.2

---
+++ SrcLib/fwGdcmIO/test/Properties.cmake
@@ -0,0 +1,21 @@
+
+set( NAME fwGdcmIOTest )
+set( VERSION  )
+set( TYPE TEST )
+set( DEPENDENCIES
+    fwGdcmIO
+    fwTools
+    fwCore
+    fwData
+    fwMedData
+    fwTest
+    fwDataTools
+    fwMemory
+    fwComEd
+    fwThread
+    fwZip
+    fwLog
+    fwDataIO
+    fwDicomData
+    )
+set( REQUIREMENTS )
