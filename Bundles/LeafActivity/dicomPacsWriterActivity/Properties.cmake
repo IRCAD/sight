1e4337af9cd578e33e6d691895eca79cca68068c
clement.troesch <clement.troesch@visiblepatient.com>
2015-11-13 14:34:13 +0100

    Added DicomPacsWriter activity
    --HG--
    branch : fw4spl_0.10.2

---
+++ Bundles/LeafActivity/dicomPacsWriterActivity/Properties.cmake
@@ -0,0 +1,25 @@
+
+set( NAME dicomPacsWriterActivity )
+set( VERSION 0.1 )
+set( TYPE BUNDLE )
+set( DEPENDENCIES)
+set( REQUIREMENTS
+    activities
+    dicomAppConfig
+    ctrlMemory
+    ctrlSelection
+    dataReg
+    dicomPacsReaderActivity
+    gui
+    guiQt
+    ioGdcm
+    ioPacs
+    media
+    mediaExt
+    servicesReg
+    visu
+    visuVTK
+    visuVTKAdaptor
+    visuVTKQt
+    )
+
