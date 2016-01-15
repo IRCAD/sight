1c69443060a803931072d194d538e606bf3ac1e8
Flavien Bridault <fbridault@ircad.fr>
2016-01-15 17:20:18 +0100

    Removed useless SceneManager in SRender.
    --HG--
    branch : fw4spl_0.10.2

--- SrcLib/visu/fwRenderOgre/include/fwRenderOgre/SRender.hpp
+++ SrcLib/visu/fwRenderOgre/include/fwRenderOgre/SRender.hpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2014-2015.
+ * FW4SPL - Copyright (C) IRCAD, 2014-2016.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -281,9 +281,6 @@ private:
 
     /// Ogre root
     ::Ogre::Root* m_ogreRoot;
-
-    /// Ogre SceneManager
-    ::Ogre::SceneManager* m_sceneManager;
 
     /// Show or not ogre overlay
     bool m_showOverlay;
