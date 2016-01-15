a46b6d9fcd2bd5aba22b47067b4dee374b9465e7
Flavien Bridault <fbridault@ircad.fr>
2016-01-15 17:19:45 +0100

    Fixed broken background when a compositor is set in XML.
    --HG--
    branch : fw4spl_0.10.2

--- SrcLib/visu/fwRenderOgre/src/fwRenderOgre/compositor/CompositorChainManager.cpp
+++ SrcLib/visu/fwRenderOgre/src/fwRenderOgre/compositor/CompositorChainManager.cpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2014-2015.
+ * FW4SPL - Copyright (C) IRCAD, 2014-2016.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -91,6 +91,8 @@ void CompositorChainManager::updateCompositorState(CompositorIdType compositorNa
 
 void CompositorChainManager::setCompositorChain(std::vector<CompositorIdType> compositors)
 {
+    this->clearCompositorChain();
+
     ::Ogre::CompositorManager* compositorManager = this->getCompositorManager();
 
     for(CompositorIdType compositorName : compositors)
