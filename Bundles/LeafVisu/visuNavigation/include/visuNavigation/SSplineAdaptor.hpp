097037239f6f93010b5718a65b5a6a650336cc65
Emilie Harquel <emilie.har@gmail.com>
2016-01-26 09:15:55 +0100

    - renamed VtkRenderService to SRender - added doConfigure() methods in adaptors
    --HG--
    branch : fw4spl_0.10.2

--- Bundles/LeafVisu/visuNavigation/include/visuNavigation/SSplineAdaptor.hpp
+++ Bundles/LeafVisu/visuNavigation/include/visuNavigation/SSplineAdaptor.hpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2015.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2016.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -64,7 +64,7 @@ protected:
     {
     }
     VISUNAVIGATION_API void doUpdate() throw(fwTools::Failed);
-    VISUNAVIGATION_API void configuring() throw(fwTools::Failed);
+    VISUNAVIGATION_API void doConfigure() throw(fwTools::Failed);
     /**  @} */
 
 private:
--- Bundles/LeafVisu/visuNavigation/src/visuNavigation/SSplineAdaptor.cpp
+++ Bundles/LeafVisu/visuNavigation/src/visuNavigation/SSplineAdaptor.cpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2015.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2016.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -76,11 +76,8 @@ void SSplineAdaptor::doStop() throw(fwTools::Failed)
 
 //-----------------------------------------------------------------------------------
 
-void SSplineAdaptor::configuring() throw(fwTools::Failed)
+void SSplineAdaptor::doConfigure() throw(fwTools::Failed)
 {
-    SLM_TRACE_FUNC();
-    OSLM_ASSERT("Wrong configuration tag name", m_configuration->getName() == "config");
-    this->setRenderId(m_configuration->getAttributeValue("renderer"));
 }
 
 //----------------------------------------------------------------------------------------------------------------
