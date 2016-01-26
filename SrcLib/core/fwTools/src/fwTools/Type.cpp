cecdb8ee250801942ed0d0b588403a7cb710f070
marc.schweitzer <marc.schweitzer@ircad.u-strasbg.fr>
2016-01-26 09:10:39 +0100

    Fixed the "#if defined" for linux
    --HG--
    branch : fw4spl_0.10.2

--- SrcLib/core/fwTools/src/fwTools/Type.cpp
+++ SrcLib/core/fwTools/src/fwTools/Type.cpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2015.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2016.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -244,7 +244,7 @@ void Type::setType<char>()
 
 //-----------------------------------------------------------------------------
 
-#ifdef linux
+#if (defined(linux) || defined(__linux)) && !defined(ANDROID)
 
 template <>
 void Type::setType<boost::int64_t>()
