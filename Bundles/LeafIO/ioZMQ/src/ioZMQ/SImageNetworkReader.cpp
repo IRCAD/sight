a03ee6dd95c68f9fcb9337155713e465117ecfc6
tschmitt <none@none>
2016-03-02 17:17:27 +0100

    Fixed SImageNetworkReader in ioZMQ. Missing includes for signals/slots.
    --HG--
    branch : fw4spl_0.11.0

--- Bundles/LeafIO/ioZMQ/src/ioZMQ/SImageNetworkReader.cpp
+++ Bundles/LeafIO/ioZMQ/src/ioZMQ/SImageNetworkReader.cpp
@@ -1,10 +1,14 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2015.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2016.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
 
 #include "ioZMQ/SImageNetworkReader.hpp"
+
+#include <fwCom/Signal.hxx>
+#include <fwCom/Slots.hpp>
+#include <fwCom/Slots.hxx>
 
 #include <fwData/Image.hpp>
 #include <fwData/String.hpp>
