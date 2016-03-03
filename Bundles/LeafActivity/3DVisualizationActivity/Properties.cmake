185a091415c2cbbfd87e71a8d1671780c3dd18d2
Flavien Bridault <fbridault@ircad.fr>
2016-03-03 10:58:34 +0100

    Added missing 2DVisualizationActivity dependency in 3DVisualizationActivity, since it is required in the plugin.xml.
    --HG--
    branch : fw4spl_0.11.0

--- Bundles/LeafActivity/3DVisualizationActivity/Properties.cmake
+++ Bundles/LeafActivity/3DVisualizationActivity/Properties.cmake
@@ -3,5 +3,17 @@ set( NAME 3DVisualizationActivity )
 set( VERSION 0.1 )
 set( TYPE BUNDLE )
 set( DEPENDENCIES  )
-set( REQUIREMENTS gui guiQt ctrlSelection uiVisu uiVisuQt visuVTKAdaptor uiImageQt uiMeasurement uiMeasurementQt uiReconstructionQt media )
-
+set( REQUIREMENTS
+    gui
+    guiQt
+    ctrlSelection
+    uiVisu
+    uiVisuQt
+    visuVTKAdaptor
+    uiImageQt
+    uiMeasurement
+    uiMeasurementQt
+    uiReconstructionQt
+    media
+    2DVisualizationActivity
+)
