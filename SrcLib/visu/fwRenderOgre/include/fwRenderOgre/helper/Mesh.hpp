27587232ea10facce19eb064dd0ed01cdf0806ca
Flavien Bridault <fbridault@ircad.fr>
2016-04-27 09:33:58 +0200

    Fixed compiling on Windows.
    --HG--
    branch : fw4spl_0.11.0

--- SrcLib/visu/fwRenderOgre/include/fwRenderOgre/helper/Mesh.hpp
+++ SrcLib/visu/fwRenderOgre/include/fwRenderOgre/helper/Mesh.hpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2014-2015.
+ * FW4SPL - Copyright (C) IRCAD, 2014-2016.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -10,6 +10,8 @@
 #include "fwRenderOgre/config.hpp"
 
 #include <OGRE/OgreColourValue.h>
+
+#include <cstdint>
 
 namespace fwRenderOgre
 {
