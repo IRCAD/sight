429c9a943c79cce329a3dc54b2b16736fce9d1cd
Flavien Bridault <fbridault@ircad.fr>
2016-04-29 12:10:51 +0200

    Removed fwUseForwardInclude in CMakeLists.txt.
    --HG--
    branch : fw4spl_0.11.0

--- Bundles/LeafRegistration/basicRegistration/Properties.cmake
+++ Bundles/LeafRegistration/basicRegistration/Properties.cmake
@@ -3,17 +3,11 @@ set( NAME basicRegistration )
 set( VERSION 0.1 )
 set( TYPE BUNDLE )
 set( DEPENDENCIES
-    fwData
-    fwServices
     fwComEd
-    fwCom
     fwDataTools
-    fwGui
     fwGuiQt
-    fwTools
-    fwRuntime
-    fwCore
-    
+    fwItkIO
+
     gui
 )
 set( REQUIREMENTS  )
