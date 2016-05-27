c8dd5285fe74305145e2c7044e3713176f9d0f66
fnuttens <florent.nuttens@ircad.u-strasbg.fr>
2016-05-27 12:13:55 +0200

    Added degree support for ellipse orientation and fixed the X axis (inverted). Also fixed bad indentation in Ellipse compositor
    --HG--
    branch : compositor_mask

--- Bundles/material/rc/glsl/postFX/EllipseMask_FP.glsl
+++ Bundles/material/rc/glsl/postFX/EllipseMask_FP.glsl
@@ -13,24 +13,26 @@ uniform float u_theta;
 
 out vec4 FragColor;
 
+#define PI 3.1415926535897932384626433832795
+
 vec2 ellipseCoord()
 {
-    float cos_theta = cos(u_theta);
-    float sin_theta = sin(u_theta);
+    float thetaRad = u_theta * (PI / 180);
+    float cos_theta = cos(thetaRad);
+    float sin_theta = sin(thetaRad);
 
     vec2 result = uv;
-    vec2 origin = vec2(u_x,u_y);
+    vec2 origin = vec2(-u_x, u_y);
 
-    //translation
+    // Translation
     result += origin;
 
-    //rotation
+    // Rotation
     result -= 0.5f;
     result = result * mat2(cos_theta, -sin_theta, sin_theta, cos_theta);
 
-    //Scale in percentage
-    result =  result / vec2(u_a / 100, u_b / 100);
-
+    // Scale (in percentage)
+    result = result / vec2(u_a / 100, u_b / 100);
     result += 0.5f;
 
     return result;
