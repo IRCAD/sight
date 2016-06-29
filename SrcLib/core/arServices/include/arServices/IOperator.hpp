ab7bd40a78ec98006ee73a1b8b713f892dccc05d
Flavien Bridault <fbridault@ircad.fr>
2016-06-29 23:55:36 +0200

    Added signals to receive parameters in ::arServices::IOperator interface.
    --HG--
    branch : appxml2

--- SrcLib/core/arServices/include/arServices/IOperator.hpp
+++ SrcLib/core/arServices/include/arServices/IOperator.hpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2014-2015.
+ * FW4SPL - Copyright (C) IRCAD, 2014-2016.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -19,10 +19,11 @@ namespace arServices
 
 /**
  * @brief   This interface defines operator service API.
- *
- * @class   IOperator
- *
- * @date    2015
+
+ * @section Slots Slots
+ * - \b setBoolParameter() : Receive the value of a boolean parameter along with its identifier.
+ * - \b setDoubleParameter() : Receive the value of a double parameter along with its identifier.
+ * - \b setIntParameter() : Receive the value of a integer parameter along with its identifier.
  */
 class ARSERVICES_CLASS_API IOperator : public ::fwServices::IService
 {
@@ -34,6 +35,10 @@ public:
     ARSERVICES_API static const ::fwCom::Signals::SignalKeyType s_COMPUTED_SIG;
     typedef ::fwCom::Signal<void ()> ComputedSignalType;
 
+    ARSERVICES_API static const ::fwCom::Slots::SlotKeyType s_SET_BOOL_PARAMETER_SLOT;
+    ARSERVICES_API static const ::fwCom::Slots::SlotKeyType s_SET_DOUBLE_PARAMETER_SLOT;
+    ARSERVICES_API static const ::fwCom::Slots::SlotKeyType s_SET_INT_PARAMETER_SLOT;
+
 protected:
 
     /// IOperator constructor. Do nothing.
@@ -42,8 +47,16 @@ protected:
     /// IOperator destructor. Do nothing.
     ARSERVICES_API virtual ~IOperator();
 
-    ComputedSignalType::sptr m_sigComputed; ///< Signal emitted when operator is computed.
+    /// Slot: This method is used to set an boolean parameter.
+    virtual void setBoolParameter(std::string key, bool val);
 
+    /// Slot: This method is used to set a double parameter.
+    virtual void setDoubleParameter(std::string key, double val);
+
+    /// Slot: This method is used to set an integer parameter.
+    virtual void setIntParameter(std::string key, int val);
+
+    ComputedSignalType::sptr m_sigComputed; ///< Signal emitted when operator is computed.
 };
 
 }
--- SrcLib/core/arServices/src/arServices/IOperator.cpp
+++ SrcLib/core/arServices/src/arServices/IOperator.cpp
@@ -1,14 +1,13 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2014-2015.
+ * FW4SPL - Copyright (C) IRCAD, 2014-2016.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
 
 #include "arServices/IOperator.hpp"
 
-#include <fwCom/Signal.hpp>
 #include <fwCom/Signal.hxx>
-#include <fwCom/Signals.hpp>
+#include <fwCom/Slots.hxx>
 
 namespace arServices
 {
@@ -17,20 +16,46 @@ namespace arServices
 
 const ::fwCom::Signals::SignalKeyType IOperator::s_COMPUTED_SIG = "computed";
 
+const ::fwCom::Slots::SlotKeyType IOperator::s_SET_BOOL_PARAMETER_SLOT   = "setBoolParameter";
+const ::fwCom::Slots::SlotKeyType IOperator::s_SET_DOUBLE_PARAMETER_SLOT = "setDoubleParameter";
+const ::fwCom::Slots::SlotKeyType IOperator::s_SET_INT_PARAMETER_SLOT    = "setIntParameter";
+
 //-----------------------------------------------------------------------------
 
-IOperator::IOperator() :
-    m_sigComputed(ComputedSignalType::New())
+IOperator::IOperator()
 {
-    ::fwCom::HasSignals::m_signals(s_COMPUTED_SIG, m_sigComputed);
+    m_sigComputed = newSignal<ComputedSignalType>(s_COMPUTED_SIG);
 
-
+    newSlot(s_SET_BOOL_PARAMETER_SLOT, &IOperator::setBoolParameter, this);
+    newSlot(s_SET_DOUBLE_PARAMETER_SLOT, &IOperator::setDoubleParameter, this);
+    newSlot(s_SET_INT_PARAMETER_SLOT, &IOperator::setIntParameter, this);
 }
 
 //-----------------------------------------------------------------------------
 
 IOperator::~IOperator()
 {
+}
+
+//-----------------------------------------------------------------------------
+
+void IOperator::setBoolParameter(std::string key, bool val)
+{
+
+}
+
+//-----------------------------------------------------------------------------
+
+void IOperator::setDoubleParameter(std::string key, double val)
+{
+
+}
+
+//-----------------------------------------------------------------------------
+
+void IOperator::setIntParameter(std::string key, int val)
+{
+
 }
 
 //-----------------------------------------------------------------------------
