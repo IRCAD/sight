e63bb94f4a9938b00c421af6cfa52d658065526a
Flavien Bridault <fbridault@ircad.fr>
2016-06-30 16:39:45 +0200

    Removed most of set(START ON) in requirements.
    --HG--
    branch : appxml2

--- Bundles/LeafVisu/visuOgre/Properties.cmake
+++ Bundles/LeafVisu/visuOgre/Properties.cmake
@@ -2,7 +2,6 @@
 set( NAME visuOgre )
 set( VERSION 0.1 )
 set( TYPE BUNDLE )
-set( START ON )
 set( DEPENDENCIES
     fwCore
     fwRender
--- Bundles/material/Properties.cmake
+++ Bundles/material/Properties.cmake
@@ -2,7 +2,6 @@
 set( NAME material )
 set( VERSION 0.1 )
 set( TYPE BUNDLE )
-set( START ON )
 set( DEPENDENCIES
     fwCore
     fwRuntime
