eb354af47fb43ff8f60542fb127e5c94584c4be4
Julien Waechter <julien.waechter@gmail.com>
2016-08-18 10:59:33 +0200

    Moved TutoPython to fw4spl-ext repository
    --HG--
    branch : fw4spl_0.11.0

--- Bundles/LeafUI/uiPython/Properties.cmake
+++
@@ -1,7 +0,0 @@
-
-set( NAME uiPython )
-set( VERSION 0.1 )
-set( TYPE BUNDLE )
-set( DEPENDENCIES fwCore fwData fwGui fwPython fwRuntime fwServices fwTools)
-set( REQUIREMENTS binding )
-
--- Bundles/core/binding/Properties.cmake
+++
@@ -1,7 +0,0 @@
-
-set( NAME binding )
-set( VERSION 0.1 )
-set( TYPE BUNDLE )
-set( DEPENDENCIES fwCore fwPython fwRuntime )
-set( REQUIREMENTS  )
-
