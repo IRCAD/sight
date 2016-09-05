e8cee9ed29247e1585c9fba9f4b40a9c1ba8dab1
Flavien Bridault <fbridault@ircad.fr>
2016-09-05 09:38:26 +0200

    Fixed video preferences setting. Removed "VIDEORENDER_PREF" composite.
    --HG--
    branch : fw4spl_0.11.0

--- SrcLib/core/arPreferences/src/arPreferences/preferences.cpp
+++ SrcLib/core/arPreferences/src/arPreferences/preferences.cpp
@@ -14,8 +14,7 @@
 namespace arPreferences
 {
 
-const std::string s_VIDEORENDER_PREF = "VIDEORENDER_PREF";
-const std::string s_VIDEO_DIR_PREF   = "VIDEO_DIR_PREF";
+const std::string s_VIDEO_DIR_PREF = "VIDEO_DIR_PREF";
 
 //-----------------------------------------------------------------------------
 
@@ -26,22 +25,16 @@ std::string getVideoDir()
     if(!preferencesServicesList.empty())
     {
         ::fwServices::IService::sptr prefService = *preferencesServicesList.begin();
-        ::fwData::Composite::sptr prefs          = prefService->getObject< ::fwData::Composite >();
+        ::fwData::Composite::sptr prefComposite  = prefService->getObject< ::fwData::Composite >();
 
-        ::fwData::Composite::IteratorType iterPref = prefs->find( s_VIDEORENDER_PREF );
-        if ( iterPref != prefs->end() )
+        ::fwData::Composite::IteratorType iterVideoDir = prefComposite->find( s_VIDEO_DIR_PREF );
+        const bool videoFound = (iterVideoDir  != prefComposite->end());
+        if (videoFound)
         {
-            ::fwData::Composite::sptr prefComposite = ::fwData::Composite::dynamicCast(iterPref->second);
-
-            ::fwData::Composite::IteratorType iterVideoDir = prefComposite->find( s_VIDEO_DIR_PREF );
-            const bool videoFound = (iterVideoDir  != prefComposite->end());
-            if (videoFound)
-            {
-                ::fwData::String::sptr videoDir = ::fwData::String::dynamicCast(iterVideoDir->second);
-                SLM_ERROR_IF("Wrong type of preference : '" + s_VIDEO_DIR_PREF + "' parameter must be a string",
-                             !videoDir);
-                videoDirectory = videoDir->value();
-            }
+            ::fwData::String::sptr videoDir = ::fwData::String::dynamicCast(iterVideoDir->second);
+            SLM_ERROR_IF("Wrong type of preference : '" + s_VIDEO_DIR_PREF + "' parameter must be a string",
+                         !videoDir);
+            videoDirectory = videoDir->value();
         }
     }
     return videoDirectory;
