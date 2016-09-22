5e81026db6cb5e22f94a79358528f6370154a760
Flavien Bridault <fbridault@ircad.fr>
2016-09-22 16:28:16 +0200

    Added new VRViewer application dedicated to Volume Rendering.
    --HG--
    branch : volume_rendering

---
+++ Apps/VRViewer/Properties.cmake
@@ -0,0 +1,42 @@
+
+set( NAME VRViewer )
+set( VERSION 0.1 )
+set( TYPE APP )
+set( DEPENDENCIES  )
+set( REQUIREMENTS
+    dataReg
+    ctrlSelection
+    ctrlCamp
+    gui
+    guiQt
+    ioData
+    ioAtoms
+    io
+    ioVTK
+    servicesReg
+    uiVisuQt
+    uiImageQt
+    uiReconstructionQt
+    uiMedDataQt
+    uiIO
+    uiTF
+    visuVTK
+    visuVTKQt
+    visuVTKAdaptor
+    fwlauncher
+    appXml2
+    arMedia
+    arDataReg
+    videoQt
+    videoTools
+    visuOgre
+    visuOgreQt
+    visuOgreAdaptor
+    uiVisuOgre
+    scene2D
+    material
+    media
+    preferences
+    )
+
+bundleParam(appXml2 PARAM_LIST config PARAM_VALUES VRViewer_Extension)
