398ae31bd00ae8bc70e8af3b2c37f542de670cab
Emilie Harquel <emilie.har@gmail.com>
2016-10-03 13:16:20 +0200

    Added bundle videoOpenCV. It contains two writer for FrameTL: - SFrameWriter: save all image frames in a forlder - SVideoWriter: save the video
    --HG--
    branch : fw4spl_0.11.0

---
+++ Bundles/LeafVideo/videoOpenCV/Properties.cmake
@@ -0,0 +1,20 @@
+
+set( NAME videoOpenCV )
+set( VERSION 0.1 )
+set( TYPE BUNDLE )
+set( DEPENDENCIES
+        fwComEd
+        fwCore
+        fwData
+        fwRuntime
+        fwServices
+        fwTools
+        arData
+        fwCom
+        fwGui
+        arServices
+        arPreferences
+
+        io
+)
+set( REQUIREMENTS  )
