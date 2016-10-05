4145e6d72f729dba5ff05f94b6cf8da3b4be72bb
julien.waechter <julien.waechter@gmail.com>
2016-10-05 10:18:06 +0200

    Removed useless exports in midasIO
    --HG--
    branch : fw4spl_0.11.0

--- SrcLib/io/midasIO/include/midasIO/IResponseHandler.hpp
+++ SrcLib/io/midasIO/include/midasIO/IResponseHandler.hpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2015.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2016.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -21,7 +21,6 @@ namespace midasIO
 
 /**
  * @brief Base class for Midas client response handlers.
- * @class IResponseHandler
  */
 class MIDASIO_CLASS_API IResponseHandler : public ::fwCore::BaseObject
 {
@@ -61,7 +60,7 @@ public:
      * @name Constructor/Destructor.
      * @{ */
     MIDASIO_API IResponseHandler();
-    MIDASIO_API virtual ~IResponseHandler()
+    virtual ~IResponseHandler()
     {
     }
     /**  @} */
