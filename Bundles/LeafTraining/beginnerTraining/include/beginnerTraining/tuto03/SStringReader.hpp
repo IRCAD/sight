51e7aa18a7fed5f6c820a2a292ca9c44c9d364cd
Flavien Bridault <fbridault@ircad.fr>
2016-11-07 12:53:38 +0100

    Converted beginnerTraining samples to appXml2.
    --HG--
    branch : fw4spl_0.11.0

--- Bundles/LeafTraining/beginnerTraining/include/beginnerTraining/tuto03/SStringReader.hpp
+++ Bundles/LeafTraining/beginnerTraining/include/beginnerTraining/tuto03/SStringReader.hpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2015.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2016.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -7,28 +7,38 @@
 #ifndef __BEGINNERTRAINING_TUTO03_SSTRINGREADER_HPP__
 #define __BEGINNERTRAINING_TUTO03_SSTRINGREADER_HPP__
 
-#include <io/IReader.hpp>
-
 #include "beginnerTraining/config.hpp"
+
+#include <io/IReader.hpp>
 
 namespace beginnerTraining
 {
 namespace tuto03
 {
 
+/**
+ * @brief   Reader that pushes the content of a text file in a ::fwData::String.
+
+ * @section XML XML Configuration
+ *
+ * @code{.xml}
+        <service type="::beginnerTraining::tuto03::SStringReader">
+            <inout key="targetString" uid="..." />
+            <file>...</file>
+       </service>
+   @endcode
+ * @subsection In-Out In-Out:
+ * - \b string [::fwData::String]: output string.
+ * @subsection Configuration Configuration:
+ * - \b file: input file path.
+ */
 class BEGINNERTRAINING_CLASS_API SStringReader : public ::io::IReader
 {
-
 public:
-
 
     fwCoreServiceClassDefinitionsMacro ( (SStringReader)(::io::IReader) );
 
-
-    /// Constructor
     BEGINNERTRAINING_API SStringReader();
-
-    /// Destructor
     BEGINNERTRAINING_API virtual ~SStringReader() throw();
 
 protected:
@@ -42,9 +52,6 @@ protected:
     /// Overrides
     BEGINNERTRAINING_API virtual void updating() throw ( ::fwTools::Failed );
 
-    /// Overrides
-    BEGINNERTRAINING_API virtual void swapping() throw ( ::fwTools::Failed );
-
     /// Overrides io method
     BEGINNERTRAINING_API virtual void configureWithIHM();
 
@@ -53,10 +60,7 @@ protected:
 
     /// Notify modification of associated data
     BEGINNERTRAINING_API void notifyMessage();
-
 };
-
-
 
 } // namespace tuto03
 } // namespace beginnerTraining
