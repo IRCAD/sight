37e80982db2bb917cf87f5284851641e2a44ce41
emilie.harquel <emilie.har@gmail.com>
2016-12-01 11:49:12 +0100

    Added namespace.hpp for preferences.

---
+++ Bundles/core/preferences/include/preferences/namespace.hpp
@@ -0,0 +1,23 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2016-2017.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef __PREFERENCES_NAMESPACE_HPP__
+#define __PREFERENCES_NAMESPACE_HPP__
+
+/**
+ * @ingroup apprequirement
+ * @brief  The namespace preferences contains the service to manage the application's preferences (window size/position,
+ *         default read/write directory, ...).
+ *
+ * This bundle must be added in the \p \<requirement\> section of your application if you want to save the
+ * application's preferences.
+ */
+namespace preferences
+{
+
+} // namespace preferences
+
+#endif //__PREFERENCES_NAMESPACE_HPP__
