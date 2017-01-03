d9d67627f86ae88a7f1c8365679170b2b620a2bb
julien.waechter <julien.waechter@gmail.com>
2017-01-03 10:24:48 +0100

    Updated license

--- SrcLib/io/fwCsvIO/include/fwCsvIO/Namespace.hpp
+++ SrcLib/io/fwCsvIO/include/fwCsvIO/Namespace.hpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2016.
+ * FW4SPL - Copyright (C) IRCAD, 2016-2017.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -8,7 +8,7 @@
 #define __FWCSVIO_NAMESPACE_HPP__
 
 /**
- * @brief       This namespace fwCsvIO contains reader for csv and txt files.
+ * @brief       This namespace fwCsvIO contains reader for csv file.
  */
 namespace fwCsvIO
 {
