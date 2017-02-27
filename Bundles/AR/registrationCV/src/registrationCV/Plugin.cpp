b150a344e4e481ff21b94f26f1a40ff2e6772f36
Marc Schweitzer <marc.schw@gmail.com>
2017-02-27 11:35:34 +0100

    feat(registrationCV): add SPoseFrom2d using solvePnP from OpenCv.

---
+++ Bundles/AR/registrationCV/src/registrationCV/Plugin.cpp
@@ -0,0 +1,34 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2017.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#include "registrationCV/Plugin.hpp"
+
+#include <fwRuntime/utils/GenericExecutableFactoryRegistrar.hpp>
+
+#include <fwServices/macros.hpp>
+
+namespace registrationCV
+{
+
+static ::fwRuntime::utils::GenericExecutableFactoryRegistrar<Plugin> registrar("::registrationCV::Plugin");
+
+Plugin::~Plugin() throw()
+{
+}
+
+//------------------------------------------------------------------------------
+
+void Plugin::start() throw(::fwRuntime::RuntimeException)
+{
+}
+
+//------------------------------------------------------------------------------
+
+void Plugin::stop() throw()
+{
+}
+
+} // namespace registrationCV
