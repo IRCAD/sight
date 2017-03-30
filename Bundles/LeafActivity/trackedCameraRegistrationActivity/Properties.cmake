fa91a5085ace36feb4c73a73df74a027131b236a
Flavien Bridault <fbridault@ircad.fr>
2017-03-30 16:39:53 +0200

    fix(trackedCameraRegistration): clean dependencies.

--- Bundles/LeafActivity/trackedCameraRegistrationActivity/Properties.cmake
+++ Bundles/LeafActivity/trackedCameraRegistrationActivity/Properties.cmake
@@ -4,36 +4,20 @@ set( VERSION 0.1 )
 set( TYPE BUNDLE )
 set( DEPENDENCIES)
 set( REQUIREMENTS
-    3DVisualizationActivity2
     activities
     arMedia
     ctrlCamp
-    ctrlSelection
-    doseSimulation
     gui
     guiQt
-    ioIGTL
-    ioNetwork
-    ioVTK
-    kinfu
     maths
     media
-    opMesh
-    opMeshExt
-    opRegistration
-    rdMedia
-    rdTools
-    uiDosimetry
-    uiImageQt
-    uiSelector
-    videoOpenCV
+    tracker
+    trackerAruco
+    uiIO
     videoOpenni
     videoQt
-    videoVLC
-    videoRealSense
     videoTools
     visuVTKAdaptor
     visuVTKARAdaptor
     visuVTKQt
-    visuVTKRDAdaptor
 )
