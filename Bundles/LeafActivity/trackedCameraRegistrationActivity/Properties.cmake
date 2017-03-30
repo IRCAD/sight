02c29f5d4e47b1a85104be3faaa64d3f78e1aff4
ctrompf <caroline.trompf@ihu-strasbourg.eu>
2017-03-30 15:29:42 +0200

    feat(trackedCameraRegistration): Add new activity to ARCalibration
    Activity to compute the matrix between a tag and a camera

---
+++ Bundles/LeafActivity/trackedCameraRegistrationActivity/Properties.cmake
@@ -0,0 +1,39 @@
+
+set( NAME trackedCameraRegistrationActivity )
+set( VERSION 0.1 )
+set( TYPE BUNDLE )
+set( DEPENDENCIES)
+set( REQUIREMENTS
+    3DVisualizationActivity2
+    activities
+    arMedia
+    ctrlCamp
+    ctrlSelection
+    doseSimulation
+    gui
+    guiQt
+    ioIGTL
+    ioNetwork
+    ioVTK
+    kinfu
+    maths
+    media
+    opMesh
+    opMeshExt
+    opRegistration
+    rdMedia
+    rdTools
+    uiDosimetry
+    uiImageQt
+    uiSelector
+    videoOpenCV
+    videoOpenni
+    videoQt
+    videoVLC
+    videoRealSense
+    videoTools
+    visuVTKAdaptor
+    visuVTKARAdaptor
+    visuVTKQt
+    visuVTKRDAdaptor
+)
