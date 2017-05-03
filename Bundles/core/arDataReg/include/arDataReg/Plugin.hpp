47e15a77e9a6b25a52874ea61b9aec5564fee437
Salima Laamrani <s.laamrani@gmail.com>
2017-05-03 09:09:24 +0200

    fix(dataReg): force link with fwDataCamp and fwMedDataCamp.
    fwDataCamp and fwMedDataCamp bundles were not linked in release with GCC >= 5.4

--- Bundles/core/arDataReg/include/arDataReg/Plugin.hpp
+++ Bundles/core/arDataReg/include/arDataReg/Plugin.hpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2014-2015.
+ * FW4SPL - Copyright (C) IRCAD, 2014-2017.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -7,19 +7,19 @@
 #ifndef __ARDATAREG_PLUGIN_HPP__
 #define __ARDATAREG_PLUGIN_HPP__
 
-#include <fwRuntime/Plugin.hpp>
-
 #include "arDataReg/config.hpp"
+
+#include <fwRuntime/Plugin.hpp>
 
 namespace arDataReg
 {
 
 /**
  * @brief   This class is started when the bundles is loaded.
- * @class   Plugin
  */
-struct ARDATAREG_CLASS_API Plugin : public ::fwRuntime::Plugin
+class ARDATAREG_CLASS_API Plugin : public ::fwRuntime::Plugin
 {
+public:
     /// Destructor. Do nothing.
     ARDATAREG_API ~Plugin() throw();
 
@@ -32,6 +32,12 @@ struct ARDATAREG_CLASS_API Plugin : public ::fwRuntime::Plugin
     /// Overrides stop method. Do nothing
     ARDATAREG_API void stop() throw();
 
+private:
+    /// Dummy variable members used to force link with fwDataCamp, fwMedDataCamp and arDataCamp bundles
+    /// Recent GCC versions (>= 5.4) strip local variables with -O2
+    int m_fwDataVersion;
+    int m_fwMedDataVersion;
+    int m_arDataVersion;
 };
 
 } // namespace arDataReg
