5911aac1c07874631b591ee748d9c7436300f695
Marc Schweitzer <marc.schweitzer@ircad.fr>
2017-06-12 10:02:41 +0200

    feat(Ex06ManualAR): Add new sample to do manual registration.
    - Load xml/yml calibration
    - Load a Mesh
    - Load a .trf transform
    - Play Video

---
+++ Samples/Ex06ManualAR/Properties.cmake
@@ -0,0 +1,39 @@
+
+set( NAME Ex06ManualAR )
+set( VERSION 0.1 )
+set( TYPE APP )
+set( DEPENDENCIES  )
+set( REQUIREMENTS
+
+    activities
+    appXml2
+    arDataReg
+    arMedia
+    ctrlCamp
+    dataReg
+    fwlauncher
+    gui
+    guiQt
+    ioCalibration
+    uiPreferences
+    uiTools
+    maths
+    preferences
+    servicesReg
+    trackerAruco
+    registrationARL
+    videoQt
+    videoTools
+    videoCalibration
+    visuVTK
+    visuVTKAdaptor
+    visuVTKARAdaptor
+    visuVTKQt
+    fwVtkIO
+    ioData
+    uiIO
+    ioVTK
+
+    )
+
+bundleParam(appXml2 PARAM_LIST config PARAM_VALUES Ex06ManualARConfig)
