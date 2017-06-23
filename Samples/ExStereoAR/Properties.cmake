a549e19a8b3c3a98c1a8c91a2057eb37042f5d2b
Marc Schweitzer <marc.schweitzer@ircad.fr>
2017-06-23 12:11:51 +0200

    feat(ExStereoAR): create sample to do stereoAR.

---
+++ Samples/ExStereoAR/Properties.cmake
@@ -0,0 +1,35 @@
+
+set( NAME ExStereoAR )
+set( VERSION 0.1 )
+set( TYPE APP )
+set( DEPENDENCIES  )
+set( REQUIREMENTS
+
+    activities
+    appXml2
+    arDataReg
+    arMedia
+    ctrlCamp
+    dataReg
+    fwlauncher
+    gui
+    guiQt
+    ioCalibration
+    uiPreferences
+    uiTools
+    maths
+    preferences
+    servicesReg
+    trackerAruco
+    registrationARL
+    videoQt
+    videoTools
+    videoCalibration
+    visuVTK
+    visuVTKAdaptor
+    visuVTKARAdaptor
+    visuVTKQt
+
+    )
+
+bundleParam(appXml2 PARAM_LIST config PARAM_VALUES ExStereoARConfig)
