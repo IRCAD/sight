67749e8a01725913b516cbc4fc03fa49dda833ef
Flavien Bridault <fbridault@ircad.fr>
2017-06-27 17:20:38 +0200

    chore(RayEntryPoints): document shader.

--- Bundles/material/rc/glsl/RayEntryPoints_FP.glsl
+++ Bundles/material/rc/glsl/RayEntryPoints_FP.glsl
@@ -1,30 +1,38 @@
 #version 330
 
+// The blend function is set to "minimum"
+// We try to find the entry depth in x and exit depth in y
 out vec2 fragColour;
+
+#define FLT_MAX 1e9f
 
 void main()
 {
-#if FRONT_FACES == 1
-
-    fragColour = vec2(gl_FragCoord.z, 10000000.0);
-
-#endif // FRONT_FACES
-
 #if BACK_FACES_MAX == 1
 
-    fragColour = vec2(100000000.0, -gl_FragCoord.z);
+    // FIRST STEP - Back Faces max: find the exit points of the geometry.
+    fragColour = vec2(FLT_MAX, -gl_FragCoord.z);
 
 #endif // BACK_FACES
 
 #if BACK_FACES == 1
 
-    fragColour = vec2(gl_FragCoord.z, 10000000.0);
+    // SECOND STEP - Back Faces: find the closest back faces and initialize the entry points with their depths.
+    fragColour = vec2(gl_FragCoord.z, FLT_MAX);
 
 #endif // BACK_FACES
 
+#if FRONT_FACES == 1
+
+    // THIRD STEP - Front Faces: update the entry points depths with the closest front faces occluding closest back faces
+    fragColour = vec2(gl_FragCoord.z, FLT_MAX);
+
+#endif // FRONT_FACES
+
 #if NEAR_PLANE == 1
 
-    fragColour = vec2(0., 10000000.0);
+    // FOURTH STEP - Near Plane: update the entry points depths with the near plane depth when when it is inside a brick
+    fragColour = vec2(0., FLT_MAX);
 
 #endif //  NEAR_PLANE
 }
