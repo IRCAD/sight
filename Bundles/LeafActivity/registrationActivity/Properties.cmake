315961f10ac15383e884c0303a7e9fcbeecb2342
Maxime Guinin <maxime.guinin@ihu-strasbourg.eu>
2017-06-29 12:23:48 +0200

    feat(PoCRegistration): Add a PoC registration activity
    - Load two images (source and target)
    - Display the source image (left view) and the target on the source image (center view)
    - Each image has it own editor
    - add transfert function view
    - Add tools to change transfert function of transformed image in reference image.
    - Add inout transformation matrix for computed result.
    - Add Transform editor and resampling service.

---
+++ Bundles/LeafActivity/registrationActivity/Properties.cmake
@@ -0,0 +1,23 @@
+
+set( NAME registrationActivity )
+set( VERSION 0.1 )
+set( TYPE BUNDLE )
+set( DEPENDENCIES  )
+set( REQUIREMENTS
+    gui
+    guiQt
+    ctrlSelection
+    ctrlCamp
+    uiData
+    uiVisu
+    uiVisuQt
+    visuVTKAdaptor
+    uiImageQt
+    scene2D
+    uiTF
+    media
+    visuVTKQt
+    imageConfig
+    opItkRegistration
+    )
+
