5088b2c227ae4b55dd3795d7ce3cfdac627a27e8
Didier Weckmann <didier.weckmann@ircad.fr>
2017-07-03 14:58:00 +0200

    feat(SManageLandmark): add a service to add points into a pointlist

---
+++ Bundles/LeafUI/uiMeasurement/include/uiMeasurement/SManageLandmark.hpp
@@ -0,0 +1,94 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2017.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#ifndef __UIMEASUREMENT_SMANAGELANDMARK_HPP__
+#define __UIMEASUREMENT_SMANAGELANDMARK_HPP__
+
+#include "uiMeasurement/config.hpp"
+
+#include <fwCom/Slot.hpp>
+
+#include <fwCore/base.hpp>
+
+#include <fwDataTools/PickingInfo.hpp>
+
+#include <fwServices/IController.hpp>
+
+namespace uiMeasurement
+{
+/**
+ * @brief   SManageLandmark add a point into a pointlist on ctrl-left click.
+ *
+ * @section Slots Slots
+ * - \b createLandmark(::fwDataTools::PickingInfo): add a point into a pointlist on ctrl-left click using PickingInfo.
+ * - \b clearPointlist(): Clear all points from pointlist
+ *
+ * @section XML XML Configuration
+ *
+ * @code{.xml}
+       <service uid="..." type="::uiMeasurement::SManageLandmark" >
+       <inout key="pointlist" uid="..." />
+       </service>
+   @endcode
+ *
+ * @subsection In-Out In-Out
+ * - \b PointList [::fwData::PointList]: the pointlist containing the added points.
+ */
+class UIMEASUREMENT_CLASS_API SManageLandmark : public ::fwServices::IController
+{
+public:
+
+    fwCoreServiceClassDefinitionsMacro((SManageLandmark)(::fwServices::IController));
+
+    /**
+     * @brief Constructor.
+     */
+    UIMEASUREMENT_API SManageLandmark() throw ();
+
+    /**
+     * @brief Destructor.
+     */
+    virtual ~SManageLandmark() throw ()
+    {
+    }
+
+    /**
+     * @name Slot API
+     * @{
+     */
+    UIMEASUREMENT_API static const ::fwCom::Slots::SlotKeyType s_CREATE_LANDMARK_SLOT;
+    UIMEASUREMENT_API static const ::fwCom::Slots::SlotKeyType s_CLEAR_SLOT;
+    ///@}
+
+protected:
+
+    /**
+     * @brief This method is used to configure the service.
+     */
+    UIMEASUREMENT_API void configuring() throw (fwTools::Failed);
+
+    /// This method is used to initialize the service.
+    UIMEASUREMENT_API void starting() throw (fwTools::Failed);
+
+    /// Does nothing.
+    UIMEASUREMENT_API void stopping() throw (fwTools::Failed);
+
+    /// Does nothing.
+    UIMEASUREMENT_API void updating() throw (fwTools::Failed);
+
+private:
+
+    /// Each time a point is created, adds an ID label to the point.
+    void createLandmark(::fwDataTools::PickingInfo info);
+    /// Clear the pointlist
+    void clearPointlist();
+    size_t m_counter; ///< Counter used as point ID.
+
+};
+
+} //namespace uiMeasurement
+
+#endif  // __UIMEASUREMENT_SMANAGELANDMARK_HPP__
