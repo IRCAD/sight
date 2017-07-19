f3fc74077f3c521637f504e8e7dbed69bab645c0
Kevin Gaudet <kevin.gaudet@ihu-strasbourg.eu>
2017-07-19 09:43:24 +0200

    chore(tfEditor): configs moved from uiTF to imageConfig.
    Renamed ''TransferFunctionEditor' 'to 'TransferFunctionWithNegatoEditor'.
    Don't extract landmarks in the config but pass them as a parameter.

--- Bundles/LeafUI/uiTF/Properties.cmake
+++ Bundles/LeafUI/uiTF/Properties.cmake
@@ -3,5 +3,5 @@ set( NAME uiTF )
 set( VERSION 0.1 )
 set( TYPE BUNDLE )
 set( DEPENDENCIES fwTools fwCore fwData fwGui fwGuiQt fwRuntime fwServices gui io)
-set( REQUIREMENTS gui guiQt ctrlSelection uiVisu uiVisuQt visuVTKAdaptor uiImageQt scene2D media)
+set( REQUIREMENTS )
 
