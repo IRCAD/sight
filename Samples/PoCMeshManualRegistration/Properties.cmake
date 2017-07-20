41049cc312347667072bc947768555c9187c9617
Flavien Bridault <fbridault@ircad.fr>
2017-07-20 08:56:53 +0200

    chore(Apps): clean obsolete PoC and rename existing ones.
    - remove TutoDevForum
    - remove PoC01SofaMonoOrgan
    - remove PoC02SofaMultiOrgans
    - remove PoC04SofaInteractive
    - remove PoC05Kinect

---
+++ Samples/PoCMeshManualRegistration/Properties.cmake
@@ -0,0 +1,31 @@
+
+set( NAME PoCMeshManualRegistration)
+set( VERSION 0.1 )
+set( TYPE APP )
+set( UNIQUE TRUE)
+set( DEPENDENCIES  )
+set( REQUIREMENTS
+    servicesReg
+    dataReg
+    gui
+    guiQt
+    io
+    ioITK
+    fwlauncher
+    uiIO
+    visuVTK
+    visuVTKQt
+    visuVTKAdaptor
+    ioVTK
+    ioData
+    uiImageQt
+    uiVisuQt
+    ctrlSelection
+    uiReconstructionQt
+    uiMedDataQt
+    appXml
+    opVTKMesh
+    media
+)
+
+bundleParam(appXml PARAM_LIST config PARAM_VALUES PoCMeshManualRegistration)
