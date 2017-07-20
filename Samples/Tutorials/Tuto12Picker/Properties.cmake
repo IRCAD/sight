cd641edbf03fbde7c908ff78a470011e9b16ee42
bfahrer <benedicte.fahrer@ircad.fr>
2017-07-20 15:35:44 +0200

    refactor(Tuto12Picker): port to appXml2

--- Samples/Tutorials/Tuto12Picker/Properties.cmake
+++ Samples/Tutorials/Tuto12Picker/Properties.cmake
@@ -20,7 +20,7 @@ set( REQUIREMENTS
     visuVTKQt
     visuVTKAdaptor
     fwlauncher
-    appXml
+    appXml2
 )
 
-bundleParam(appXml PARAM_LIST config PARAM_VALUES Tuto12Picker)
+bundleParam(appXml2 PARAM_LIST config PARAM_VALUES Tuto12Picker)
