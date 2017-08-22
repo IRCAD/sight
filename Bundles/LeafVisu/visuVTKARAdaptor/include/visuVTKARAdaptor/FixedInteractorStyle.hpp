7e7b940fcbd39dfbefa9c99e5f25562d3d66001c
Kevin Gaudet <kevin.gaudet@ihu-strasbourg.eu>
2017-08-22 17:25:19 +0200

    chore(FixedInteractorStyle): update documentation.

--- Bundles/LeafVisu/visuVTKARAdaptor/include/visuVTKARAdaptor/FixedInteractorStyle.hpp
+++ Bundles/LeafVisu/visuVTKARAdaptor/include/visuVTKARAdaptor/FixedInteractorStyle.hpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2014-2016.
+ * FW4SPL - Copyright (C) IRCAD, 2014-2017.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -12,48 +12,54 @@
 #include <vtkInteractorStyleUser.h>
 
 /**
- *  @class FixedInteractorStyle
- *  @brief This class disable all user's interactions in a vtk view.
- *  @note This interactor style must be set to the visuVTKAdaptor::InteractorStyle.
+ *  @brief Interactor class disabling all user interactions in a VTK scene.
+ *
+ *  @note Interactor styles can be set in ::visuVTKAdaptor::SInteractorStyle.
+ *
+ *  @see ::visuVTKAdaptor::SInteractorStyle.
+ *
  *  @code{.xml}
-    <adaptor id="interactorStyle" class="::visuVTKAdaptor::InteractorStyle" objectId="self">
+    <service type="::visuVTKAdaptor::InteractorStyle" >
         <config renderer="default" style="FixedInteractorStyle" />
-    </adaptor>
+    </service>
     @endcode
- *  @note All method are empty in order to disable interactions.
+ *
+ *  @note All overriden methods do absolutely nothing.
  */
 class VISUVTKARADAPTOR_CLASS_API FixedInteractorStyle : public vtkInteractorStyleUser
 {
 
 public:
 
-    ///  New macro
-    VISUVTKARADAPTOR_API static FixedInteractorStyle *New();
+    /// New macro.
+    VISUVTKARADAPTOR_API static FixedInteractorStyle* New();
 
-    /// vtk revision macro
+    /// VTK revision macro.
     vtkTypeMacro(FixedInteractorStyle, vtkInteractorStyle);
 
-    VISUVTKARADAPTOR_API virtual void OnMiddleButtonDown();
-    VISUVTKARADAPTOR_API virtual void OnChar();
-    VISUVTKARADAPTOR_API virtual void OnLeftButtonDown();
-    VISUVTKARADAPTOR_API virtual void OnMouseMove();
-    VISUVTKARADAPTOR_API virtual void OnMouseWheelBackward();
-    VISUVTKARADAPTOR_API virtual void OnMouseWheelForward();
-    VISUVTKARADAPTOR_API virtual void OnRightButtonDown();
+    VISUVTKARADAPTOR_API virtual void OnMiddleButtonDown() override;
+    VISUVTKARADAPTOR_API virtual void OnChar() override;
+    VISUVTKARADAPTOR_API virtual void OnLeftButtonDown() override;
+    VISUVTKARADAPTOR_API virtual void OnMouseMove() override;
+    VISUVTKARADAPTOR_API virtual void OnMouseWheelBackward() override;
+    VISUVTKARADAPTOR_API virtual void OnMouseWheelForward() override;
+    VISUVTKARADAPTOR_API virtual void OnRightButtonDown() override;
 
 protected:
 
-    /// constructor
+    /// Constructor.
     VISUVTKARADAPTOR_API FixedInteractorStyle();
 
-    ///  destructor
+    /// Destructor.
     VISUVTKARADAPTOR_API ~FixedInteractorStyle();
 
 private:
 
-    FixedInteractorStyle(const FixedInteractorStyle&);  // Not implemented.
+    /// Not implemented.
+    FixedInteractorStyle(const FixedInteractorStyle&);
 
-    void operator=(const FixedInteractorStyle&);  // Not implemented.
+    /// Not implemented.
+    void operator=(const FixedInteractorStyle&);
 
 };
 
