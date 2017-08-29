5ec1f8d4c71f8d366597b3c1d675415a3058ae9d
Flavien Bridault <fbridault@ircad.fr>
2017-08-29 17:39:06 +0200

    refactor(SSignal): clean and replace signal parameters by a bool

--- Bundles/core/gui/src/gui/action/SSignal.cpp
+++ Bundles/core/gui/src/gui/action/SSignal.cpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2016.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2017.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -7,48 +7,31 @@
 #include "gui/action/SSignal.hpp"
 
 #include <fwCom/Signal.hxx>
-#include <fwCom/Slots.hxx>
-
-#include <fwCore/base.hpp>
 
 #include <fwGui/dialog/MessageDialog.hpp>
 
-#include <fwRuntime/Extension.hpp>
-#include <fwRuntime/helper.hpp>
-
 #include <fwServices/macros.hpp>
-
-#include <fwTools/fwID.hpp>
 
 namespace gui
 {
 namespace action
 {
 
-fwServicesRegisterMacro( ::fwGui::IActionSrv, ::gui::action::SSignal, ::fwData::Object );
+//-----------------------------------------------------------------------------
 
+fwServicesRegisterMacro( ::fwGui::IActionSrv, ::gui::action::SSignal );
 
-static const ::fwCom::Signals::SignalKeyType TRIGERRED_SIG   = "triggered";
-static const ::fwCom::Signals::SignalKeyType CANCELLED_SIG   = "cancelled";
-static const ::fwCom::Signals::SignalKeyType SET_OBJECT_SLOT = "setObjects";
+static const ::fwCom::Signals::SignalKeyType s_TRIGGERED_SIG = "triggered";
+static const ::fwCom::Signals::SignalKeyType s_CANCELLED_SIG = "cancelled";
 
 //-----------------------------------------------------------------------------
 
 SSignal::SSignal() noexcept :
-    m_sigTriggered(TrigerredSignalType::New()),
-    m_sigCancelled(TrigerredSignalType::New()),
-    m_slotSetObjects(SSignal::SetObjectsSlotType::New(&SSignal::setObjects, this))
+    m_sigTriggered(TriggeredSignalType::New()),
+    m_sigCancelled(TriggeredSignalType::New())
 {
-    ::fwCom::HasSignals::m_signals
-        ( TRIGERRED_SIG, m_sigTriggered )
-        ( CANCELLED_SIG, m_sigCancelled );
-
-    ::fwCom::HasSlots::m_slots
-        ( SET_OBJECT_SLOT, m_slotSetObjects );
-
-
-
-    this->setWorker( m_associatedWorker );
+    m_sigTriggered = newSignal<TriggeredSignalType>(s_TRIGGERED_SIG);
+    m_sigCancelled = newSignal<TriggeredSignalType>(s_CANCELLED_SIG);
 }
 
 //-----------------------------------------------------------------------------
@@ -59,9 +42,15 @@ SSignal::~SSignal() noexcept
 
 //-----------------------------------------------------------------------------
 
+void SSignal::configuring()
+{
+    this->initialize();
+}
+
+//-----------------------------------------------------------------------------
+
 void SSignal::starting()
 {
-    SLM_TRACE_FUNC();
     this->actionServiceStarting();
 }
 
@@ -69,16 +58,12 @@ void SSignal::starting()
 
 void SSignal::stopping()
 {
-    SLM_TRACE_FUNC();
-
-    m_objects.clear();
-
     this->actionServiceStopping();
 }
 
 //-----------------------------------------------------------------------------
 
-void SSignal::info(std::ostream &_sstream )
+void SSignal::info(std::ostream& _sstream )
 {
     _sstream << "Starter Action" << std::endl;
 }
@@ -90,22 +75,16 @@ void SSignal::updating()
     SLM_TRACE_FUNC();
     if (this->confirmAction())
     {
-        m_sigTriggered->asyncEmit(m_objects);
+        m_sigTriggered->asyncEmit(this->getIsActive());
     }
     else
     {
-        m_sigCancelled->asyncEmit(m_objects);
+        m_sigCancelled->asyncEmit(this->getIsActive());
     }
 
 }
 
 //-----------------------------------------------------------------------------
-
-void SSignal::configuring()
-{
-    SLM_TRACE_FUNC();
-    this->initialize();
-}
 
 } // namespace action
 } // namespace gui
