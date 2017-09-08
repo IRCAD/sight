42d48d8f63754da9aad446c9157f3de53586d1ca
Emilie Wernert <emilie.har@gmail.com>
2017-09-08 13:59:45 +0200

    feat(TransferFunctionParser): allows to configure 'isClamped' variable.

--- Bundles/core/dataReg/include/dataReg/parser/TransferFunction.hpp
+++ Bundles/core/dataReg/include/dataReg/parser/TransferFunction.hpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2016.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2017.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -16,7 +16,6 @@
 #include <fwTools/Failed.hpp>
 #include <fwTools/Object.hpp>
 
-
 namespace dataReg
 {
 namespace parser
@@ -31,7 +30,7 @@ class DATAREG_CLASS_API TransferFunction : public ::fwServices::IXMLParser
 
 public:
 
-    fwCoreServiceClassDefinitionsMacro ( (TransferFunction)(::fwServices::IXMLParser) );
+    fwCoreServiceClassDefinitionsMacro( (TransferFunction)(::fwServices::IXMLParser) );
 
     /// Constructor : does nothing.
     TransferFunction()
@@ -46,7 +45,7 @@ public:
     /**
      * @code{.xml}
        <object type="::fwData::TransferFunction">
-           <colors>
+           <colors isClamped="true">
                <step color="#ffffff" value="0" />
                <step color="#0000ff" value="1000" />
                <step color="#00ff00" value="2000" />
@@ -57,6 +56,8 @@ public:
        </object>
        @endcode
      * - \b step : defines a step in the legend given with the arguments color in hex-code and the max value.
+     * - \b isClamped(optional, default: yes) : defines interpolation mode on extremities, if yes then after extremity
+     *       point, the returned TF color is TFColor(0,0,0,0), else it is the color value of the extremity.
      */
     DATAREG_API void createConfig( ::fwTools::Object::sptr _obj );
 protected:
