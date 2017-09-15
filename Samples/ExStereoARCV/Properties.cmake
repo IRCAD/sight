1dad453cb6177e988cf85244338b0335b5746aaa
Marc Schweitzer <marc.schweitzer@ircad.fr>
2017-09-15 11:43:08 +0200

    feat(ExStereoARCV): new example to compute stereo pose with OpenCV/Ceres

---
+++ Samples/ExStereoARCV/Properties.cmake
@@ -0,0 +1,35 @@
+
+set( NAME ExStereoARCV )
+set( VERSION 0.1 )
+set( TYPE APP )
+set( DEPENDENCIES  )
+set( REQUIREMENTS
+
+    activities
+    appXml
+    arDataReg
+    arMedia
+    ctrlCamp
+    dataReg
+    fwlauncher
+    gui
+    guiQt
+    ioCalibration
+    uiPreferences
+    uiTools
+    maths
+    preferences
+    servicesReg
+    trackerAruco
+    registrationCV
+    videoQt
+    videoTools
+    videoCalibration
+    visuVTK
+    visuVTKAdaptor
+    visuVTKARAdaptor
+    visuVTKQt
+
+    )
+
+bundleParam(appXml PARAM_LIST config PARAM_VALUES ExStereoARCVConfig)
