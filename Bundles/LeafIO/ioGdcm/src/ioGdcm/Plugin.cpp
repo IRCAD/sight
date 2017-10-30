da944cc813ce29647995e1575ed00314f36587b5
Alexandre Ancel <alexandre.ancel@ihu-strasbourg.eu>
2017-10-30 11:35:00 +0100

    fix(fwGdcmIO): Key name in ioGdcm/Plugin.cpp
    - Also update @brief comment in ioGdcmSSeriesDBReader.hpp

--- Bundles/LeafIO/ioGdcm/src/ioGdcm/Plugin.cpp
+++ Bundles/LeafIO/ioGdcm/src/ioGdcm/Plugin.cpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2016.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2017.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -7,21 +7,26 @@
 #include "ioGdcm/Plugin.hpp"
 
 #include <fwRuntime/utils/GenericExecutableFactoryRegistrar.hpp>
+
 #include <fwServices/macros.hpp>
 
 namespace ioGdcm
 {
 
 using namespace ::fwRuntime::utils;
-static GenericExecutableFactoryRegistrar<Plugin> registrar("ioGdcm::Plugin");
+static GenericExecutableFactoryRegistrar<Plugin> registrar("::ioGdcm::Plugin");
 
 Plugin::~Plugin() noexcept
 {
 }
 
+//------------------------------------------------------------------------------
+
 void Plugin::start()
 {
 }
+
+//------------------------------------------------------------------------------
 
 void Plugin::stop() noexcept
 {
