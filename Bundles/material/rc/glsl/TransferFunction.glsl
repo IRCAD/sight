1ef75957afca9adc36f68f9a6e6b2ba178266657
Flavien Bridault <fbridault@ircad.fr>
2017-11-09 09:34:58 +0100

    feat(SVideo): add transfer function support

--- Bundles/material/rc/glsl/TransferFunction.glsl
+++ Bundles/material/rc/glsl/TransferFunction.glsl
@@ -15,3 +15,15 @@ vec4 sampleTransferFunction(float intensity)
 
     return texelFetch(u_tfTexture, ivec2(uv), 0);
 }
+
+vec4 sampleTransferFunction_uint16(int intensity)
+{
+    float scaledValue = intensity + 32767;
+
+    // Computes 2D indices from the hounsfield value
+    vec2 uv;
+    uv.y = floor( scaledValue / 256.f );
+    uv.x = floor( scaledValue - uv.y * 256.f );
+
+    return texelFetch(u_tfTexture, ivec2(uv), 0);
+}
--- Bundles/material/rc/glsl/Video_FP.glsl
+++ Bundles/material/rc/glsl/Video_FP.glsl
@@ -1,12 +1,34 @@
-#version 150
+#version 330
+
+#ifdef TF
+
+// Defined in TransferFunction.glsl
+vec4 sampleTransferFunction_uint16(int intensity);
+
+uniform isampler2D u_videoTexture;
+
+#else
 
 uniform sampler2D u_videoTexture;
+
+#endif
 
 in vec2 oTexCoord;
 
 out vec4 fragColor;
 
+//-----------------------------------------------------------------------------
+
 void main(void)
 {
-    fragColor = texture(u_videoTexture, oTexCoord);
+    vec4 color;
+
+#ifdef TF
+    int value = texture(u_videoTexture, oTexCoord).r;
+    color = sampleTransferFunction_uint16(value);
+#else
+    color = texture(u_videoTexture, oTexCoord);
+#endif
+
+    fragColor = color;
 }
