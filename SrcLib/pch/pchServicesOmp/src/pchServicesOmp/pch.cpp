78aa3c8e3259fe6391b957c69e6658306be53739
Flavien Bridault <fbridault@ircad.fr>
2017-11-29 15:26:57 +0100

    feat(pchServicesOmp): add new pch bundle for OpenMP targets

---
+++ SrcLib/pch/pchServicesOmp/src/pchServicesOmp/pch.cpp
@@ -0,0 +1,11 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2017.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#include "pchServicesOmp/pch.hpp"
+
+// Empty source file
+// This is only to get a source file compiled, otherwise CMake would consider this target as an utility target
+// And the pch would not be generated properly
