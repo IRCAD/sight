7d359b25389b718fda4ee833ff556501410aabb1
Davy Sagayaradjou <davy.sagayaradjou@gmail.com>
2018-01-05 16:51:53 +0100

    fix(Shading): remove a warning

--- SrcLib/visu/fwRenderOgre/src/fwRenderOgre/helper/Shading.cpp
+++ SrcLib/visu/fwRenderOgre/src/fwRenderOgre/helper/Shading.cpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2014-2017.
+ * FW4SPL - Copyright (C) IRCAD, 2014-2018.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -499,6 +499,8 @@ Shading::ShaderConstantsType Shading::findShaderConstants(::Ogre::GpuProgramPara
                 "GCT_UNKNOWN"
             };
             OSLM_WARN("Object type "+GpuConstantTypeNames[_type-1]+" not supported yet");
+
+            (void)GpuConstantTypeNames; // Only there to avoid the 'unused' warning.
     }
     return object;
 }
