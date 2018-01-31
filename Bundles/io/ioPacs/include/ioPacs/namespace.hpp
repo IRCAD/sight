e8b5fe2d64b3a702c5d8e4bad5c9bb616ac4c8fa
Julien Waechter <julien.waechter@gmail.com>
2018-01-31 12:10:08 +0100

    doc(ioPacs): update dox with apprequirement tag

--- Bundles/io/ioPacs/include/ioPacs/namespace.hpp
+++ Bundles/io/ioPacs/include/ioPacs/namespace.hpp
@@ -1,16 +1,19 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2016.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2018.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
 
-#ifndef __IOPACS_NAMESPACE_HPP__
-#define __IOPACS_NAMESPACE_HPP__
+#pragma once
 
 /**
+ * @ingroup apprequirement
  * @brief ioPacs contains services use to deal with PACS using DCMTK library.
+ *
+ * It's also used to load fwPacsIO library. So, if you want to use a
+ * ::fwPacsIO::data::PacsConfiguration objects in your application,
+ * you require this bundle: add the requirement in your application plugin.xml.
  */
 namespace ioPacs
 {
 }
-#endif /* __IOPACS_NAMESPACE_HPP__ */
