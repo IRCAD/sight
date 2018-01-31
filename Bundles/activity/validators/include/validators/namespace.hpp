d6bc7346324760cba0c50bf22a0a0b5c3e5d95c7
Julien Waechter <julien.waechter@gmail.com>
2018-01-31 09:36:12 +0100

    doc(validators): add missing apprequirement  doxygen tag

--- Bundles/activity/validators/include/validators/namespace.hpp
+++ Bundles/activity/validators/include/validators/namespace.hpp
@@ -1,19 +1,18 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2016.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2018.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
 
-#ifndef __VALIDATORS_NAMESPACE_HPP__
-#define __VALIDATORS_NAMESPACE_HPP__
-
 /**
- * @brief This namespace contains object validators used to check activity required data.
+ * @ingroup apprequirement
+ * @brief   This namespace contains object validators used to check activity required data.
+ *          It must be set in the application (or activity) \p \<requirement\> in order to use
+ *          a CameraSeries or ModelSeries validator.
  */
+#pragma once
+
 namespace validators
 {
 
 } // namespace validators
-
-
-#endif /* __VALIDATORS_NAMESPACE_HPP__ */
