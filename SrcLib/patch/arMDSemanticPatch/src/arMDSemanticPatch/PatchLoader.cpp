764e207530148d0df06bea5e1bd90a4928f98504
Julien Waechter <julien.waechter@gmail.com>
2018-05-18 15:32:21 +0200

    feat(patch): update fw4spl-ar data version to V13AR
    - Added data version V13AR to manage new Model/ImageSeries with Dicom reference

--- SrcLib/patch/arMDSemanticPatch/src/arMDSemanticPatch/PatchLoader.cpp
+++ SrcLib/patch/arMDSemanticPatch/src/arMDSemanticPatch/PatchLoader.cpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2017.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2018.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -20,5 +20,13 @@ void PatchLoader::loadPatches()
     versionManager->buildLinkTable("./share/arMDSemanticPatch-" ARMDSEMANTICPATCH_VER "/");
 }
 
-} //namespace arMDSemanticPatch
+//------------------------------------------------------------------------------
 
+std::string PatchLoader::getCurrentVersion()
+{
+    return "V13AR";
+}
+
+//------------------------------------------------------------------------------
+
+} //namespace arMDSemanticPatch
