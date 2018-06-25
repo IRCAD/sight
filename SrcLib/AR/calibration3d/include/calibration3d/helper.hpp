a7073b06fbdd19471488c8fef318953583438c98
Marc Schweitzer <marc.schw@gmail.com>
2018-06-25 11:04:36 +0200

    docs(calibration3d): add "@throw" section

--- SrcLib/AR/calibration3d/include/calibration3d/helper.hpp
+++ SrcLib/AR/calibration3d/include/calibration3d/helper.hpp
@@ -99,6 +99,7 @@ CALIBRATION3D_API void calibratePointingTool(
  * @param _height: height of charuco board
  * @param _markerSizeInBits : bits size of marker (can be 4, 5, 6 or 7)
  * @return a ::cv::Ptr of ::cv::aruco::Dictionary.
+ * @throw std::invalid_argument if _markerSizeInBits != [4, 5, 6, 7]
  */
 CALIBRATION3D_API ::cv::Ptr< ::cv::aruco::Dictionary > generateArucoDictionary(const size_t _width,
                                                                                const size_t _height,
