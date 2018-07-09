a2052d9732487455ca13690048e52af9fbffba6f
R.Chavignat <r.chavignat@gmail.com>
2018-07-09 11:31:57 +0200

    fix(Mesh): out of bounds memory read

--- SrcLib/visu/fwRenderOgre/src/fwRenderOgre/helper/Mesh.cpp
+++ SrcLib/visu/fwRenderOgre/src/fwRenderOgre/helper/Mesh.cpp
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2014-2017.
+ * FW4SPL - Copyright (C) IRCAD, 2014-2018.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
@@ -22,7 +22,7 @@ void Mesh::copyColors(::Ogre::RGBA* _dest, const std::uint8_t* _src, size_t _num
 {
     if(_numComponents == 3)
     {
-        for (unsigned int i = 0; i < _numPoints; ++i)
+        for (unsigned int i = 0; i < (_numPoints - 1); ++i)
         {
             // Fastest way to copy tested so far, take 1.0 in alpha as default
             ::Ogre::RGBA argb = 0xFF000000;
@@ -30,6 +30,12 @@ void Mesh::copyColors(::Ogre::RGBA* _dest, const std::uint8_t* _src, size_t _num
             *_dest++          = argb;
             _src             += 3;
         }
+        // The above loop reads 4 bytes at a time and will read one byte out of bounds on the last
+        // vertex, so handle it as a special case.
+        *_dest = 0xFF000000
+                 | _src[0]
+                 | static_cast<unsigned int>(_src[1]) << 8
+                 | static_cast<unsigned int>(_src[2]) << 16;
     }
     else if(_numComponents == 4)
     {
