1940763671576e50782e1876e7b93ad936421cc5
Kevin Gaudet <kevin.gaudet@ihu-strasbourg.eu>
2018-07-09 10:15:06 +0200

    perf(VolumeEntries): simplify the volume entry rendering algorithm
    slightly improves the entry geometry rendering time

--- Bundles/material/rc/glsl/RayEntryPoints_FP.glsl
+++ Bundles/material/rc/glsl/RayEntryPoints_FP.glsl
@@ -2,45 +2,43 @@
 
 // The blend function is set to "minimum"
 // We try to find the entry depth in x and exit depth in y
+#if FRONT_FACES == 1 || BACK_FACES == 1
+out float rayEntry; // Use only the red channel when writing entry points.
+#else
 out vec2 fragColour;
+#endif // FRONT_FACES == 1 || BACK_FACES == 1
 
 #define FLT_MAX 1e9f
 
 void main()
 {
-#if BACK_FACES_MAX == 1
-
-    // FIRST STEP - Back Faces max: find the exit points of the geometry.
-    fragColour = vec2(FLT_MAX, -gl_FragCoord.z);
-
-#endif // BACK_FACES
-
-#if FRONT_FACES_MIN == 1
-
-    // SECOND STEP - Front faces min : find the exit points when a ray hits a surface.
-    fragColour = vec2(-FLT_MAX, -gl_FragCoord.z);
-
-#endif // FRONT_FACES_MIN
 
 #if BACK_FACES == 1
 
-    // THIRD STEP - Back Faces: find the closest back faces and initialize the entry points with their depths.
-    fragColour = vec2(gl_FragCoord.z, FLT_MAX);
+    // FIRST STEP - Back Faces : find the nearest back face depth.
+    // Don't do anything, we're not writing any colours anyway we just want the depth.
 
-#endif // BACK_FACES
+#endif BACK_FACES == 1
 
 #if FRONT_FACES == 1
 
-    // FOURTH STEP - Front Faces: update the entry points depths with the closest front faces occluding closest back faces
-    fragColour = vec2(gl_FragCoord.z, FLT_MAX);
+    // SECOND STEP - Front Faces: update the entry points depths with the closest front faces occluding closest back faces
+    rayEntry = gl_FragCoord.z;
 
 #endif // FRONT_FACES
 
-#if NEAR_PLANE == 1
+#if BACK_FACES_MAX == 1
 
-    // FIFTH STEP - Near Plane: update the entry points depths with the near plane depth when when it is inside a brick
-    fragColour = vec2(0., FLT_MAX);
+    // THIRD STEP - Back Faces max: find the exit points of the geometry.
+    fragColour = vec2(FLT_MAX, -gl_FragCoord.z);
 
-#endif //  NEAR_PLANE
+#endif // BACK_FACES_MAX
+
+#if FRONT_FACES_MIN == 1
+
+    // FOURTH STEP - Front faces min : find the exit points when a ray hits a surface.
+    fragColour = vec2(-FLT_MAX, -gl_FragCoord.z);
+
+#endif // FRONT_FACES_MIN
 
 }
--- Bundles/material/rc/glsl/RayEntryPoints_VP.glsl
+++ Bundles/material/rc/glsl/RayEntryPoints_VP.glsl
@@ -1,25 +1,19 @@
 #version 330
 
-#ifndef NEAR_PLANE
 #ifdef AUTOSTEREO
 uniform mat4 u_worldView;
 uniform mat4 u_proj;
 #else
 uniform mat4 u_worldViewProj;
 #endif // AUTOSTEREO
-#endif // NEAR_PLANE
 
 in vec3 position;
 
 void main()
 {
-#if NEAR_PLANE == 1
-    gl_Position = vec4(position, 1.);
-#else
 #ifdef AUTOSTEREO
     gl_Position = u_proj * u_worldView * vec4(position, 1.);
 #else
     gl_Position = u_worldViewProj * vec4(position, 1.);
 #endif // AUTOSTEREO
-#endif //  NEAR_PLANE
 }
