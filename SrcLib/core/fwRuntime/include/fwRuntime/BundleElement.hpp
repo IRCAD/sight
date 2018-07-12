85f6d91d0414c66698a8a7446c5e02940186ef3e
R.Chavignat <r.chavignat@gmail.com>
2018-07-12 11:48:49 +0200

    fix(fwRuntime): memory leaks
    Break cyclic references between bundle elements with weak_ptrs.

--- SrcLib/core/fwRuntime/include/fwRuntime/BundleElement.hpp
+++ SrcLib/core/fwRuntime/include/fwRuntime/BundleElement.hpp
@@ -1,11 +1,10 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2016.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2018.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
 
-#ifndef __FWRUNTIME_BUNDLEELEMENT_HPP__
-#define __FWRUNTIME_BUNDLEELEMENT_HPP__
+#pragma once
 
 #include "fwRuntime/config.hpp"
 
@@ -16,17 +15,11 @@ namespace fwRuntime
 struct Bundle;
 }
 
-
 namespace fwRuntime
 {
 
-
-
 /**
  * @brief   Implements the base class for all element managed by a bundle.
- * @struct  BundleElement
- * @date    2006-2009
- *
  */
 struct BundleElement
 {
@@ -81,18 +74,11 @@ struct BundleElement
          */
         BundleElement( std::shared_ptr< Bundle > bundle );
 
-
     private:
 
-        std::shared_ptr< Bundle >  m_bundle;///< a shared pointer to the bundle managing the element
+        std::weak_ptr< Bundle >  m_bundle;///< Weak pointer to the bundle managing the element
         bool m_enable;                          ///< to know if bundle element is enabled
 
 };
 
-
-
 } // namespace fwRuntime
-
-
-
-#endif /*__FWRUNTIME_BUNDLEELEMENT_HPP__*/
--- SrcLib/core/fwRuntime/src/BundleElement.cpp
+++ SrcLib/core/fwRuntime/src/BundleElement.cpp
@@ -1,32 +1,29 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2009-2015.
+ * FW4SPL - Copyright (C) IRCAD, 2009-2018.
  * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
  * published by the Free Software Foundation.
  * ****** END LICENSE BLOCK ****** */
 
 #include "fwRuntime/BundleElement.hpp"
+
 #include "fwRuntime/Bundle.hpp"
-
-#include <cassert>
-#include <memory>
-
 
 namespace fwRuntime
 {
 
-//------------------------------------------------------------------------------
-
-BundleElement::BundleElement() :   m_bundle( Bundle::getLoadingBundle() ), m_enable(true)
+BundleElement::BundleElement() :
+    m_bundle( Bundle::getLoadingBundle() ),
+    m_enable(true)
 {
     // Post-condition
-
     SLM_ASSERT("bundle '" << m_bundle->getIdentifier() << "' not initialized", m_bundle != 0 );
 }
 
 //------------------------------------------------------------------------------
 
-BundleElement::BundleElement( std::shared_ptr< Bundle > bundle )
-    :   m_bundle( bundle ), m_enable(true)
+BundleElement::BundleElement( std::shared_ptr< Bundle > bundle ) :
+    m_bundle( bundle ),
+    m_enable(true)
 {
     // Post-condition
     SLM_ASSERT("bundle '" << m_bundle->getIdentifier() << "' not initialized", m_bundle != 0 );
@@ -36,7 +33,7 @@ BundleElement::BundleElement( std::shared_ptr< Bundle > bundle )
 
 std::shared_ptr<Bundle> BundleElement::getBundle() const
 {
-    return m_bundle;
+    return m_bundle.lock();
 }
 
 //------------------------------------------------------------------------------
@@ -45,7 +42,7 @@ bool BundleElement::isEnable() const
 {
     // Pre-condition.
     SLM_ASSERT("bundle '" << m_bundle->getIdentifier() << "' not initialized", m_bundle != 0 );
-    return m_bundle->isEnable() && m_enable;
+    return m_bundle.lock()->isEnable() && m_enable;
 }
 
 //------------------------------------------------------------------------------
@@ -54,7 +51,5 @@ void BundleElement::setEnable(const bool enable)
 {
     m_enable = enable;
 }
-
-//------------------------------------------------------------------------------
 
 } // namespace fwRuntime
