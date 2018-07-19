d7ba211b7c917955d78b83d8d5ac35278f0f6bcd
Julien Waechter <julien.waechter@gmail.com>
2018-07-19 16:04:44 +0200

    feat(conan): add conan cmake script

---
+++ CMake/build/conan.cmake
@@ -0,0 +1,15 @@
+# Conan support
+if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+   message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+   file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/v0.12/conan.cmake"
+                 "${CMAKE_BINARY_DIR}/conan.cmake")
+endif()
+include(${CMAKE_BINARY_DIR}/conan.cmake)
+
+conan_check(VERSION 1.0.0 REQUIRED)
+conan_add_remote(NAME fw4spl-conan INDEX 1
+                 URL http://5.39.78.163:8081/artifactory/api/conan/conan-local)
+                 
+conan_cmake_run(CONANFILE conanfile.txt
+                BASIC_SETUP CMAKE_TARGETS
+                BUILD missing)
