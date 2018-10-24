3dcb4553a8a04e586445b2aa4a9eb74f928a074c
Kevin Gaudet <kevin.gaudet@ihu-strasbourg.eu>
2018-10-24 14:20:54 +0200

    feat(uiVisuOgre): action to toggle stereo rendering in a layer

---
+++ Bundles/ui/uiVisuOgre/include/uiVisuOgre/SStereoToggler.hpp
@@ -0,0 +1,69 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2018.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#pragma once
+
+#include "uiVisuOgre/config.hpp"
+
+#include <fwGui/IActionSrv.hpp>
+
+#include <fwRenderOgre/Layer.hpp>
+
+namespace uiVisuOgre
+{
+
+/**
+ * @brief Action to enable/disable stereo in an ogre scene layer.
+ *
+ * @section XML XML Configuration
+ *
+ * @code{.xml}
+    <service uid="..." type="::uiVisuOgre::SStereoToggler" >
+        <config layer="default" stereoMode="interlaced" />
+    </service>
+ *
+ * @subsection Configuration Configuration
+ * - \b layer : layer in which to activate stereo rendering
+ * - \b stereoMode (values=interlace|AutoStereo5|AutoStereo8): mode to activate.
+ *
+ */
+class UIVISUOGRE_CLASS_API SStereoToggler : public ::fwGui::IActionSrv
+{
+public:
+
+    fwCoreServiceClassDefinitionsMacro( (SStereoToggler)(::fwGui::IActionSrv) );
+
+    /// Constructor.
+    UIVISUOGRE_API SStereoToggler();
+
+    /// Destructor.
+    UIVISUOGRE_API virtual ~SStereoToggler() override;
+
+protected:
+
+    /// Configures the service.
+    UIVISUOGRE_API virtual void configuring() override;
+
+    /// Starts the action service.
+    UIVISUOGRE_API virtual void starting() override;
+
+    /// Enables stereo if active, disables it otherwise.
+    UIVISUOGRE_API virtual void updating() override;
+
+    /// Stops the action service.
+    UIVISUOGRE_API virtual void stopping() override;
+
+private:
+
+    /// Layer in which we enable/disable stereo.
+    std::string m_layerId;
+
+    /// Mode that is toggled.
+    ::fwRenderOgre::Layer::StereoModeType m_stereoMode { ::fwRenderOgre::Layer::StereoModeType::NONE };
+
+};
+
+} // namespace uiVisuOgre
---
+++ Bundles/ui/uiVisuOgre/src/uiVisuOgre/SStereoToggler.cpp
@@ -0,0 +1,102 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2018.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
+ * published by the Free Software Foundation.
+ * ****** END LICENSE BLOCK ****** */
+
+#include "uiVisuOgre/SStereoToggler.hpp"
+
+#include <fwRenderOgre/SRender.hpp>
+
+namespace uiVisuOgre
+{
+
+fwServicesRegisterMacro( ::fwGui::IActionSrv, ::uiVisuOgre::SStereoToggler);
+
+//------------------------------------------------------------------------------
+
+SStereoToggler::SStereoToggler()
+{
+
+}
+
+//------------------------------------------------------------------------------
+
+SStereoToggler::~SStereoToggler()
+{
+
+}
+
+//------------------------------------------------------------------------------
+
+void SStereoToggler::configuring()
+{
+    this->initialize();
+
+    const ConfigType config = this->getConfigTree().get_child("config.<xmlattr>");
+
+    m_layerId = config.get<std::string>("layer");
+    SLM_ASSERT("Empty layer ID.", !m_layerId.empty());
+
+    const std::string stereoMode = config.get<std::string>("stereoMode", "");
+
+    if(stereoMode == "interlaced")
+    {
+        m_stereoMode = ::fwRenderOgre::Layer::StereoModeType::STEREO;
+    }
+    else if (stereoMode == "AutoStereo5")
+    {
+        m_stereoMode = ::fwRenderOgre::Layer::StereoModeType::AUTOSTEREO_5;
+    }
+    else if (stereoMode == "AutoStereo5")
+    {
+        m_stereoMode = ::fwRenderOgre::Layer::StereoModeType::AUTOSTEREO_8;
+    }
+    else
+    {
+        SLM_FATAL("Unknown stereo mode: '" + stereoMode + "'");
+    }
+}
+
+//------------------------------------------------------------------------------
+
+void SStereoToggler::starting()
+{
+    this->actionServiceStarting();
+}
+
+//------------------------------------------------------------------------------
+
+void SStereoToggler::updating()
+{
+    if(this->confirmAction())
+    {
+        ::fwServices::registry::ObjectService::ServiceVectorType renderers =
+            ::fwServices::OSR::getServices("::fwRenderOgre::SRender");
+
+        for(auto srv : renderers)
+        {
+            ::fwRenderOgre::SRender::sptr render           = ::fwRenderOgre::SRender::dynamicCast(srv);
+            ::fwRenderOgre::SRender::LayerMapType layerMap = render->getLayers();
+
+            auto layerIt = layerMap.find(m_layerId);
+            SLM_ASSERT("Layer '" + m_layerId + "' does not exist.", layerIt != layerMap.end());
+
+            const bool enableStereo = this->getIsActive() && this->getIsExecutable();
+            const auto stereoMode   = enableStereo ? m_stereoMode : ::fwRenderOgre::Layer::StereoModeType::NONE;
+
+            layerIt->second->setStereoMode(stereoMode);
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+
+void SStereoToggler::stopping()
+{
+    this->actionServiceStopping();
+}
+
+//------------------------------------------------------------------------------
+
+} // uiVisuOgre
