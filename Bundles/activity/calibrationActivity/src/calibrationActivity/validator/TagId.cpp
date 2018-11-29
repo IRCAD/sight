2773651dc4995babfb0770287e980df546e74f37
Flavien Bridault <fbridault@ircad.fr>
2018-11-29 13:08:48 +0100

    chore(*): update LGPL licence template and add IHU copyright

--- Bundles/activity/calibrationActivity/src/calibrationActivity/validator/TagId.cpp
+++ Bundles/activity/calibrationActivity/src/calibrationActivity/validator/TagId.cpp
@@ -1,8 +1,24 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * FW4SPL - Copyright (C) IRCAD, 2016.
- * Distributed under the terms of the GNU Lesser General Public License (LGPL) as
- * published by the Free Software Foundation.
- * ****** END LICENSE BLOCK ****** */
+/************************************************************************
+ *
+ * Copyright (C) 2016 IRCAD France
+ * Copyright (C) 2016 IHU Strasbourg
+ *
+ * This file is part of Sight.
+ *
+ * Sight is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sight. If not, see <https://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************/
 
 #include "calibrationActivity/validator/TagId.hpp"
 
