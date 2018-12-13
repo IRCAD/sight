3bf03bd821521be5ec834eb1d92c011d7aca364a
Alexandre Ancel <alexandre.ancel@ihu-strasbourg.eu>
2018-12-13 11:01:05 +0100

    chore(BitmapImageWriter): update license blocks

--- Bundles/io/ioVTK/test/tu/include/ImageReaderWriterTest.hpp
+++ Bundles/io/ioVTK/test/tu/include/ImageReaderWriterTest.hpp
@@ -1,7 +1,7 @@
 /************************************************************************
  *
- * Copyright (C) 2009-2015 IRCAD France
- * Copyright (C) 2012-2015 IHU Strasbourg
+ * Copyright (C) 2009-2018 IRCAD France
+ * Copyright (C) 2012-2018 IHU Strasbourg
  *
  * This file is part of Sight.
  *
