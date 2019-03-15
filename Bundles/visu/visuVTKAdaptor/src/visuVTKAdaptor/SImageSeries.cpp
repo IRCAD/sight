1a01c04d332f9b086a3c3d3ed7ca37841f27d909
Ekaterina Olivier <ekaterina.olivier@epitech.eu>
2019-03-15 11:36:33 +0100

    fix(visuVTKAdaptor): register missing object inside SImageSeries
    Register the object imageSeries and tf inside the constructor of ImageSeries to not get an error from visuVTKAdaptor when we add this service in Qml.

--- Bundles/visu/visuVTKAdaptor/src/visuVTKAdaptor/SImageSeries.cpp
+++ Bundles/visu/visuVTKAdaptor/src/visuVTKAdaptor/SImageSeries.cpp
@@ -1,7 +1,7 @@
 /************************************************************************
  *
- * Copyright (C) 2009-2018 IRCAD France
- * Copyright (C) 2012-2018 IHU Strasbourg
+ * Copyright (C) 2009-2019 IRCAD France
+ * Copyright (C) 2012-2019 IHU Strasbourg
  *
  * This file is part of Sight.
  *
@@ -50,6 +50,8 @@ SImageSeries::SImageSeries() noexcept :
     m_sliceMode(SNegatoMPR::THREE_SLICES)
 
 {
+    this->registerObject(s_IMAGE_SERIES_INOUT, AccessType::INOUT, true);
+    this->registerObject(s_TF_INOUT, AccessType::INOUT, false, true);
 }
 
 //------------------------------------------------------------------------------
