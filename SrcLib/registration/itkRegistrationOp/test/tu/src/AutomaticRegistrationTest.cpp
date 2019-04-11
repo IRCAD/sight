5a8cb3e454b0f1a52e5f83d6a90c2bf7bb22a6f7
Marc Schweitzer <marc.schweitzer@ircad.fr>
2019-04-11 12:59:11 +0200

    fix(MedicalImageAdaptor): replace by MedicalImage

--- SrcLib/registration/itkRegistrationOp/test/tu/src/AutomaticRegistrationTest.cpp
+++ SrcLib/registration/itkRegistrationOp/test/tu/src/AutomaticRegistrationTest.cpp
@@ -32,7 +32,7 @@
 
 #include <fwDataTools/fieldHelper/MedicalImageHelpers.hpp>
 #include <fwDataTools/helper/Image.hpp>
-#include <fwDataTools/helper/MedicalImageAdaptor.hpp>
+#include <fwDataTools/helper/MedicalImage.hpp>
 #include <fwDataTools/TransformationMatrix3D.hpp>
 
 #include <fwItkIO/itk.hpp>
