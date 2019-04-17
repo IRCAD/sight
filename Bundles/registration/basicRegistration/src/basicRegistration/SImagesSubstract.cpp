50ce1914aa41c3d84c445c07c80fe456fc5dc1ee
Marc Schweitzer <marc.schweitzer@ircad.fr>
2019-04-17 09:58:45 +0200

    fix(PCH): add missing signal/slot includes when compiling without PCH

--- Bundles/registration/basicRegistration/src/basicRegistration/SImagesSubstract.cpp
+++ Bundles/registration/basicRegistration/src/basicRegistration/SImagesSubstract.cpp
@@ -1,7 +1,7 @@
 /************************************************************************
  *
- * Copyright (C) 2009-2018 IRCAD France
- * Copyright (C) 2012-2018 IHU Strasbourg
+ * Copyright (C) 2009-2019 IRCAD France
+ * Copyright (C) 2012-2019 IHU Strasbourg
  *
  * This file is part of Sight.
  *
@@ -24,6 +24,8 @@
 #include <QPushButton>
 
 #include <fwItkIO/itk.hpp>
+
+#include <fwCom/Signal.hxx>
 
 #include <fwCore/spyLog.hpp>
 
