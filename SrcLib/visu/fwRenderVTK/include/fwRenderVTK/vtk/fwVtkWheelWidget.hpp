182c09273928f5ac35a432f82cab9b3eb808258a
Ekaterina Olivier <ekaterina.olivier@epitech.eu>
2019-05-13 11:58:38 +0200

    chore(VTK): change header
    Removed accidentally the date of creation

--- SrcLib/visu/fwRenderVTK/include/fwRenderVTK/vtk/fwVtkWheelWidget.hpp
+++ SrcLib/visu/fwRenderVTK/include/fwRenderVTK/vtk/fwVtkWheelWidget.hpp
@@ -1,7 +1,7 @@
 /************************************************************************
  *
- * Copyright (C) 2019 IRCAD France
- * Copyright (C) 2019 IHU Strasbourg
+ * Copyright (C) 2017-2019 IRCAD France
+ * Copyright (C) 2017-2019 IHU Strasbourg
  *
  * This file is part of Sight.
  *
