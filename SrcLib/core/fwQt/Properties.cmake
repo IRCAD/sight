562b54c45642a25f623a1020d8cf36bd2fd3063e
Ekaterina Olivier <ekaterina.olivier@epitech.eu>
2019-07-11 15:20:58 +0200

    chore(fwQt): remove useless dependencies
    fwQml does not have to be imported inside fwQt

--- SrcLib/core/fwQt/Properties.cmake
+++ SrcLib/core/fwQt/Properties.cmake
@@ -2,7 +2,7 @@
 set( NAME fwQt )
 set( VERSION 0.1 )
 set( TYPE LIBRARY )
-set( DEPENDENCIES fwCore fwQml fwGui fwRuntime fwServices fwThread fwTools )
+set( DEPENDENCIES fwCore fwGui fwRuntime fwServices fwThread fwTools )
 set( REQUIREMENTS  )
 set( USE_PCH_FROM_TARGET pchServices)
 set( CONAN_DEPS
