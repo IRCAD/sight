ed32937203cc7e5ee0efa670c581cf06ae2cd606
Romain <romain.manciaux@gmail.com>
2019-09-23 14:29:41 +0200

    feat(SVector): add a new adaptor to display a vector

---
+++ Bundles/visu/visuOgreAdaptor/include/visuOgreAdaptor/SVector.hpp
@@ -0,0 +1,126 @@
+/************************************************************************
+ *
+ * Copyright (C) 2017-2019 IRCAD France
+ * Copyright (C) 2017-2019 IHU Strasbourg
+ *
+ * This file is part of Sight.
+ *
+ * Sight is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sight. If not, see <https://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************/
+
+#pragma once
+
+#include "visuOgreAdaptor/config.hpp"
+#include "visuOgreAdaptor/SMaterial.hpp"
+
+#include <fwCom/Slot.hpp>
+
+#include <fwRenderOgre/IAdaptor.hpp>
+#include <fwRenderOgre/ITransformable.hpp>
+
+#include <Ogre.h>
+
+namespace fwData
+{
+class Material;
+}
+
+namespace visuOgreAdaptor
+{
+
+/**
+ * @brief This adaptor shows a simple vector.
+ *
+ * @section Slots Slots
+ * -\b updateVisibility(bool): Sets whether the vector is shown or not.
+ * -\b toggleVisibility(): Toggle whether the vector is shown or not.
+ *
+ * @section XML XML Configuration
+ * @code{.xml}
+    <service uid="..." type="::visuOgreAdaptor::SVector">
+        <config layer="default" transform="transformUID" length="30" color="#FFFFFF" />
+    </service>
+   @endcode
+ * @subsection Configuration Configuration:
+ * - \b layer (mandatory): defines the mesh's layer.
+ * - \b transform (optional): the name of the Ogre transform node where to attach the mesh, as it was specified
+ * in the STransform adaptor.
+ * - \b length (optional): (float) axis length in mm (default 50).
+ * - \b color (optional): (hexadecimal) color of the vector (default #FFFFFF).
+ *
+ */
+class VISUOGREADAPTOR_CLASS_API SVector : public ::fwRenderOgre::IAdaptor,
+                                          public ::fwRenderOgre::ITransformable
+{
+public:
+
+    fwCoreServiceClassDefinitionsMacro((SVector)(::fwRenderOgre::IAdaptor))
+
+    /// Constructor: Sets default parameters and initializes necessary members.
+    VISUOGREADAPTOR_API SVector() noexcept;
+    /// Destructor: Does nothing
+    VISUOGREADAPTOR_API virtual ~SVector() noexcept;
+
+    /// Slot to set visibility
+    VISUOGREADAPTOR_API static const ::fwCom::Slots::SlotKeyType s_UPDATE_VISIBILITY_SLOT;
+    /// Slot to toggle visibility
+    VISUOGREADAPTOR_API static const ::fwCom::Slots::SlotKeyType s_TOGGLE_VISIBILITY_SLOT;
+    /// Slot: update length of the line
+    VISUOGREADAPTOR_API static const ::fwCom::Slots::SlotKeyType s_UPDATE_LENGTH_SLOT;
+
+    /// Sets visibility of the vector
+    VISUOGREADAPTOR_API void updateVisibility(bool _isVisible);
+    /// Toggle visibility of the vector
+    VISUOGREADAPTOR_API void toggleVisibility();
+    /// Returns proposals to connect service slots to associated object signals
+    VISUOGREADAPTOR_API ::fwServices::IService::KeyConnectionsMap getAutoConnections() const override;
+    /// Update the length of the vector.
+    VISUOGREADAPTOR_API void updateLength(float _length);
+
+private:
+
+    /// Configures the adaptor
+    void configuring() override;
+    /// Manually creates a Mesh in the Default Ogre Ressource group
+    void starting() override;
+    /// Deletes the mesh after unregistering the service, and shutting connections.
+    void stopping() override;
+    /// Checks if the fwData::Mesh has changed, and updates it if it has.
+    void updating() override;
+
+    void createVector();
+    void deleteVector();
+
+    /// Pointer to the Material data
+    ::fwData::Material::sptr m_material { nullptr };
+    /// Axis length (in mm)
+    float m_length { 1.f };
+    /// Handles axis visibility.
+    bool m_isVisible { true };
+    /// Color of the vector
+    std::string m_color {"#FFFFFF"};
+
+    /// Line along the x axis
+    ::Ogre::ManualObject* m_line { nullptr };
+    /// Arrow of the x axis
+    ::Ogre::ManualObject* m_cone { nullptr };
+    /// Scene node where all of our manual objects are attached
+    ::Ogre::SceneNode* m_sceneNode { nullptr };
+    /// Material used to draw the vector.
+    ::visuOgreAdaptor::SMaterial::sptr m_materialAdaptor;
+
+};
+
+} //namespace visuOgreAdaptor
---
+++ Bundles/visu/visuOgreAdaptor/src/visuOgreAdaptor/SVector.cpp
@@ -0,0 +1,260 @@
+/************************************************************************
+ *
+ * Copyright (C) 2017-2019 IRCAD France
+ * Copyright (C) 2017-2019 IHU Strasbourg
+ *
+ * This file is part of Sight.
+ *
+ * Sight is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sight. If not, see <https://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************/
+
+#include "visuOgreAdaptor/SVector.hpp"
+
+#include "visuOgreAdaptor/STransform.hpp"
+
+#include <fwCom/Slots.hxx>
+
+#include <fwDataTools/Color.hpp>
+
+#include <fwRenderOgre/helper/Font.hpp>
+#include <fwRenderOgre/helper/ManualObject.hpp>
+#include <fwRenderOgre/helper/Scene.hpp>
+#include <fwRenderOgre/SRender.hpp>
+
+#include <fwServices/macros.hpp>
+
+#include <OgreNode.h>
+
+namespace visuOgreAdaptor
+{
+
+const ::fwCom::Slots::SlotKeyType SVector::s_UPDATE_LENGTH_SLOT     = "updateLength";
+const ::fwCom::Slots::SlotKeyType SVector::s_UPDATE_VISIBILITY_SLOT = "updateVisibility";
+const ::fwCom::Slots::SlotKeyType SVector::s_TOGGLE_VISIBILITY_SLOT = "toggleVisibility";
+
+static const std::string s_COLOR_CONFIG  = "color";
+static const std::string s_LENGTH_CONFIG = "length";
+
+fwServicesRegisterMacro(::fwRenderOgre::IAdaptor, ::visuOgreAdaptor::SVector);
+
+//-----------------------------------------------------------------------------
+
+SVector::SVector() noexcept
+{
+    newSlot(s_UPDATE_VISIBILITY_SLOT, &SVector::updateVisibility, this);
+    newSlot(s_TOGGLE_VISIBILITY_SLOT, &SVector::toggleVisibility, this);
+    newSlot(s_UPDATE_LENGTH_SLOT, &SVector::updateLength, this);
+}
+
+//-----------------------------------------------------------------------------
+
+SVector::~SVector() noexcept
+{
+}
+
+//-----------------------------------------------------------------------------
+
+::fwServices::IService::KeyConnectionsMap visuOgreAdaptor::SVector::getAutoConnections() const
+{
+    ::fwServices::IService::KeyConnectionsMap connections;
+    connections.push( ::visuOgreAdaptor::STransform::s_TRANSFORM_CONFIG, ::fwData::Object::s_MODIFIED_SIG,
+                      s_UPDATE_SLOT );
+    return connections;
+}
+
+//-----------------------------------------------------------------------------
+
+void SVector::configuring()
+{
+    this->configureParams();
+
+    const ConfigType configType = this->getConfigTree();
+    const ConfigType config     = configType.get_child("config.<xmlattr>");
+
+    const std::string transformId = config.get<std::string>(::visuOgreAdaptor::STransform::s_TRANSFORM_CONFIG,
+                                                            this->getID() + "_transform");
+
+    this->setTransformId(transformId);
+    m_length = config.get<float>(s_LENGTH_CONFIG, m_length);
+    m_color  = config.get<std::string>(s_COLOR_CONFIG, m_color);
+    OSLM_ASSERT(
+        "Color string should start with '#' and followed by 6 ou 8 "
+        "hexadecimal digits. Given color : " << m_color,
+            m_color[0] == '#'
+            && ( m_color.length() == 7 || m_color.length() == 9)
+        );
+}
+
+//-----------------------------------------------------------------------------
+
+void SVector::starting()
+{
+    this->initialize();
+
+    this->getRenderService()->makeCurrent();
+
+    ::Ogre::SceneNode* rootSceneNode = this->getSceneManager()->getRootSceneNode();
+    ::Ogre::SceneNode* transformNode = this->getTransformNode(rootSceneNode);
+    m_sceneNode                      = transformNode->createChildSceneNode(this->getID() + "_mainNode");
+
+    // set the material
+    m_material = ::fwData::Material::New();
+
+    m_materialAdaptor = this->registerService< ::visuOgreAdaptor::SMaterial >(
+        "::visuOgreAdaptor::SMaterial");
+    m_materialAdaptor->registerInOut(m_material, ::visuOgreAdaptor::SMaterial::s_MATERIAL_INOUT, true);
+    m_materialAdaptor->setID(this->getID() + m_materialAdaptor->getID());
+    m_materialAdaptor->setMaterialName(this->getID() + m_materialAdaptor->getID());
+    m_materialAdaptor->setRenderService( this->getRenderService() );
+    m_materialAdaptor->setLayerID(m_layerID);
+    m_materialAdaptor->setMaterialTemplateName(::fwRenderOgre::Material::DEFAULT_MATERIAL_TEMPLATE_NAME);
+    m_materialAdaptor->start();
+
+    m_materialAdaptor->getMaterialFw()->setHasVertexColor(true);
+    m_materialAdaptor->update();
+
+    this->createVector();
+
+    this->updateVisibility(m_isVisible);
+
+    this->requestRender();
+}
+
+//-----------------------------------------------------------------------------
+
+void SVector::updating()
+{
+    this->getRenderService()->makeCurrent();
+
+    deleteVector();
+    createVector();
+
+    this->requestRender();
+}
+
+//-----------------------------------------------------------------------------
+
+void SVector::stopping()
+{
+    this->getRenderService()->makeCurrent();
+
+    this->deleteVector();
+
+    ::Ogre::SceneManager* sceneMgr   = this->getSceneManager();
+    ::Ogre::SceneNode* rootSceneNode = sceneMgr->getRootSceneNode();
+    ::Ogre::SceneNode* transformNode = this->getTransformNode(rootSceneNode);
+    transformNode->removeAndDestroyChild(this->getID() + "_mainNode");
+
+    this->unregisterServices();
+    m_material.reset();
+}
+
+//-----------------------------------------------------------------------------
+
+void SVector::createVector()
+{
+    // Size
+    const float cylinderLength = m_length - m_length/10;
+    const float cylinderRadius = m_length/80;
+    const float coneLength     = m_length - cylinderLength;
+    const float coneRadius     = cylinderRadius*2;
+    const unsigned sample      = 64;
+
+    // Color
+    std::uint8_t color[4];
+    ::fwDataTools::Color::hexaStringToRGBA(m_color, color);
+    ::Ogre::ColourValue ogreColor(color[0] / 255.f, color[1] / 255.f, color[2] / 255.f);
+
+    // Draw
+    ::Ogre::SceneManager* sceneMgr = this->getSceneManager();
+    m_line                         = sceneMgr->createManualObject(this->getID() + "_line");
+    m_cone                         = sceneMgr->createManualObject(this->getID() + "_cone");
+
+    // Line
+    ::fwRenderOgre::helper::ManualObject::createCylinder(m_line, m_materialAdaptor->getMaterialName(),
+                                                         ogreColor,
+                                                         cylinderRadius,
+                                                         cylinderLength,
+                                                         sample);
+    ::Ogre::SceneNode* lineNode = m_sceneNode->createChildSceneNode(this->getID() + "_lineNode");
+    m_line->setBoundingBox(::Ogre::AxisAlignedBox(::Ogre::Vector3(0.f, -coneRadius, -coneRadius),
+                                                  ::Ogre::Vector3(m_length, coneRadius, coneRadius)));
+    lineNode->attachObject(m_line);
+    // Rotate around y axis to create the cylinder on z Axis (consistent with SLine adaptor)
+    lineNode->yaw(::Ogre::Degree(-90));
+
+    // Cone
+    ::fwRenderOgre::helper::ManualObject::createCone(m_cone, m_materialAdaptor->getMaterialName(),
+                                                     ogreColor,
+                                                     coneRadius,
+                                                     coneLength,
+                                                     sample);
+    ::Ogre::SceneNode* coneNode = m_sceneNode->createChildSceneNode(this->getID() + "_coneNode");
+
+    coneNode->attachObject(m_cone);
+    coneNode->translate(0.f, 0.f, cylinderLength);
+    coneNode->yaw(::Ogre::Degree(-90));
+
+}
+
+//-----------------------------------------------------------------------------
+
+void SVector::deleteVector()
+{
+    if(m_sceneNode != nullptr)
+    {
+        m_sceneNode->removeAndDestroyChild(this->getID() + "_lineNode");
+        m_sceneNode->removeAndDestroyChild(this->getID() + "_coneNode");
+    }
+
+    ::Ogre::SceneManager* sceneMgr = this->getSceneManager();
+
+    sceneMgr->destroyManualObject(m_line);
+    sceneMgr->destroyManualObject(m_cone);
+}
+
+//-----------------------------------------------------------------------------
+
+void SVector::updateVisibility(bool _isVisible)
+{
+    m_isVisible = _isVisible;
+
+    if(m_sceneNode)
+    {
+        m_sceneNode->setVisible(m_isVisible);
+    }
+
+    this->updating();
+}
+
+//------------------------------------------------------------------------------
+
+void SVector::toggleVisibility()
+{
+    this->updateVisibility(!m_isVisible);
+}
+
+//-----------------------------------------------------------------------------
+
+void SVector::updateLength(float _length)
+{
+    m_length = _length;
+
+    this->update();
+}
+
+//-----------------------------------------------------------------------------
+
+} //visuOgreAdaptor
