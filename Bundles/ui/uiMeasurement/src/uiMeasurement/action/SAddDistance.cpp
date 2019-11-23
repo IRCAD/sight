5961f3816a43ee6501b730dd135b7316c81eacfb
rmanciaux <romain.manciaux@gmail.com>
2019-11-23 19:46:26 +0100

    refactor(AddDistance): deprecate the service.

--- Bundles/ui/uiMeasurement/src/uiMeasurement/action/SAddDistance.cpp
+++ Bundles/ui/uiMeasurement/src/uiMeasurement/action/SAddDistance.cpp
@@ -1,7 +1,7 @@
 /************************************************************************
  *
- * Copyright (C) 2009-2019 IRCAD France
- * Copyright (C) 2012-2019 IHU Strasbourg
+ * Copyright (C) 2019 IRCAD France
+ * Copyright (C) 2019 IHU Strasbourg
  *
  * This file is part of Sight.
  *
@@ -20,16 +20,13 @@
  *
  ***********************************************************************/
 
-#include "uiMeasurement/action/AddDistance.hpp"
+#include "uiMeasurement/action/SAddDistance.hpp"
 
-#include <fwCom/Signal.hpp>
-#include <fwCom/Signal.hxx>
 #include <fwCom/Signals.hpp>
-
-#include <fwCore/base.hpp>
 
 #include <fwData/Boolean.hpp>
 #include <fwData/Image.hpp>
+#include <fwData/mt/ObjectWriteLock.hpp>
 #include <fwData/Point.hpp>
 #include <fwData/PointList.hpp>
 #include <fwData/Vector.hpp>
@@ -37,69 +34,80 @@
 #include <fwDataTools/fieldHelper/Image.hpp>
 #include <fwDataTools/fieldHelper/MedicalImageHelpers.hpp>
 
-#include <fwServices/macros.hpp>
-
-#include <exception>
-
 namespace uiMeasurement
 {
 namespace action
 {
 
-fwServicesRegisterMacro( ::fwGui::IActionSrv, ::uiMeasurement::action::AddDistance, ::fwData::Image )
-
 static const ::fwServices::IService::KeyType s_IMAGE_INOUT = "image";
 
 //------------------------------------------------------------------------------
 
-AddDistance::AddDistance( ) noexcept
-{
-    FW_DEPRECATED("::uiMeasurement::action::AddDistance", "::uiMeasurement::action::SAddDistance", "21.0");
-}
-
-//------------------------------------------------------------------------------
-
-AddDistance::~AddDistance() noexcept
+SAddDistance::SAddDistance( ) noexcept
 {
 }
 
 //------------------------------------------------------------------------------
 
-void AddDistance::updating()
+SAddDistance::~SAddDistance() noexcept
 {
-    ::fwData::Image::sptr image = this->getInOut< ::fwData::Image >(s_IMAGE_INOUT);
+}
+
+//------------------------------------------------------------------------------
+
+void SAddDistance::configuring()
+{
+    this->::fwGui::IActionSrv::initialize();
+}
+
+//------------------------------------------------------------------------------
+
+void SAddDistance::starting()
+{
+    this->::fwGui::IActionSrv::actionServiceStarting();
+}
+
+//------------------------------------------------------------------------------
+
+void SAddDistance::updating()
+{
+    const ::fwData::Image::sptr image = this->getInOut< ::fwData::Image >(s_IMAGE_INOUT);
+    SLM_ASSERT("'" + s_IMAGE_INOUT + "' does not exist.", image);
+    const ::fwData::mt::ObjectWriteLock lock(image);
+
     if (!::fwDataTools::fieldHelper::MedicalImageHelpers::checkImageValidity(image))
     {
-        SLM_WARN("The image '" + s_IMAGE_INOUT + "' is invalid.");
+        SLM_ASSERT("'" + s_IMAGE_INOUT + "' is invalid.", image);
         return;
     }
-    ::fwData::Point::sptr pt1 = ::fwData::Point::New();
-    std::copy( image->getOrigin().begin(),  image->getOrigin().begin() +3, pt1->getCoord().begin() );
 
-    ::fwData::Point::sptr pt2 = ::fwData::Point::New();
-    std::copy( image->getSize().begin(),  image->getSize().begin() +3, pt2->getCoord().begin() );
+    const ::fwData::Point::sptr pt1 = ::fwData::Point::New();
+    std::copy( image->getOrigin2().begin(), image->getOrigin2().begin() +3, pt1->getCoord().begin() );
+
+    const ::fwData::Point::sptr pt2 = ::fwData::Point::New();
+    std::copy( image->getSize2().begin(), image->getSize2().begin() +3, pt2->getCoord().begin() );
 
     std::transform( pt2->getCoord().begin(), pt2->getCoord().end(),
-                    image->getSpacing().begin(),
+                    image->getSpacing2().begin(),
                     pt2->getCoord().begin(),
                     std::multiplies<double>() );
     std::transform( pt2->getCoord().begin(), pt2->getCoord().end(),
-                    image->getOrigin().begin(),
+                    image->getOrigin2().begin(),
                     pt2->getCoord().begin(),
                     std::plus<double>() );
 
-    ::fwData::PointList::sptr pl = ::fwData::PointList::New();
+    const ::fwData::PointList::sptr pl = ::fwData::PointList::New();
 
     pl->getPoints().push_back( pt1 );
     pl->getPoints().push_back( pt2 );
 
-    ::fwData::Vector::sptr vectDist;
-    vectDist = image->setDefaultField(::fwDataTools::fieldHelper::Image::m_imageDistancesId, ::fwData::Vector::New());
+    const ::fwData::Vector::sptr vectDist
+        = image->setDefaultField(::fwDataTools::fieldHelper::Image::m_imageDistancesId, ::fwData::Vector::New());
 
     vectDist->getContainer().push_back(pl);
 
     // force distance to be shown
-    image->setField("ShowDistances",  ::fwData::Boolean::New(true));
+    image->setField(::fwDataTools::fieldHelper::Image::m_distanceVisibility,  ::fwData::Boolean::New(true));
 
     auto sig = image->signal< ::fwData::Image::DistanceAddedSignalType >(::fwData::Image::s_DISTANCE_ADDED_SIG);
     sig->asyncEmit(pl);
@@ -107,21 +115,7 @@ void AddDistance::updating()
 
 //------------------------------------------------------------------------------
 
-void AddDistance::configuring()
-{
-    this->::fwGui::IActionSrv::initialize();
-}
-
-//------------------------------------------------------------------------------
-
-void AddDistance::starting()
-{
-    this->::fwGui::IActionSrv::actionServiceStarting();
-}
-
-//------------------------------------------------------------------------------
-
-void AddDistance::stopping()
+void SAddDistance::stopping()
 {
     this->::fwGui::IActionSrv::actionServiceStopping();
 }
