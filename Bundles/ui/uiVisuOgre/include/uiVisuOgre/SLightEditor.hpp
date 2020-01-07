256eb01f7c00b54ece61abd4a23d792d07997d04
Romain <romain.manciaux@ihu-strasbourg.eu>
2020-01-07 09:35:48 +0100

    docs(SLightEditor): fix the typo

--- Bundles/ui/uiVisuOgre/include/uiVisuOgre/SLightEditor.hpp
+++ Bundles/ui/uiVisuOgre/include/uiVisuOgre/SLightEditor.hpp
@@ -1,7 +1,7 @@
 /************************************************************************
  *
- * Copyright (C) 2009-2019 IRCAD France
- * Copyright (C) 2012-2019 IHU Strasbourg
+ * Copyright (C) 2009-2020 IRCAD France
+ * Copyright (C) 2012-2020 IHU Strasbourg
  *
  * This file is part of Sight.
  *
@@ -83,7 +83,7 @@ private:
 
     /**
      * @brief Gets the current light node.
-     * @return Ths node where the current light is attached.
+     * @return The node where the current light is attached.
      */
     ::Ogre::Node* getLightNode() const;
 
