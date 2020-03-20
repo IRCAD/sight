02d749ae08bddd7567a99921c75c5aaa7d37c6f1
bfahrer <benedicte.fahrer@ihu-strasbourg.eu>
2020-03-20 09:18:25 +0100

    fix(SVector): call requestRender instead of update to modify vector visibility

--- Bundles/visu/visuOgreAdaptor/src/visuOgreAdaptor/SVector.cpp
+++ Bundles/visu/visuOgreAdaptor/src/visuOgreAdaptor/SVector.cpp
@@ -1,7 +1,7 @@
 /************************************************************************
  *
- * Copyright (C) 2019 IRCAD France
- * Copyright (C) 2019 IHU Strasbourg
+ * Copyright (C) 2019-2020 IRCAD France
+ * Copyright (C) 2019-2020 IHU Strasbourg
  *
  * This file is part of Sight.
  *
@@ -230,7 +230,7 @@ void SVector::updateVisibility(bool _isVisible)
         m_sceneNode->setVisible(m_isVisible);
     }
 
-    this->updating();
+    this->requestRender();
 }
 
 //------------------------------------------------------------------------------
