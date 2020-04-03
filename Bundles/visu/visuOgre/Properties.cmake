da80e0954eedc0b55904382f77c823d0c5c024da
rmanciaux <romain.manciaux@gmail.com>
2020-04-03 22:53:41 +0200

    refactor(visuOgre): clean the bundle

--- Bundles/visu/visuOgre/Properties.cmake
+++ Bundles/visu/visuOgre/Properties.cmake
@@ -1,19 +1,11 @@
-
 set( NAME visuOgre )
 set( VERSION 0.1 )
 set( TYPE BUNDLE )
 set( START ON )
 set( DEPENDENCIES
-    fwCore
-    fwRender
     fwRuntime
-    fwServices
     fwRenderOgre
-    fwCom
-    fwData
-    fwTools
 )
-set( REQUIREMENTS
-    material
-)
+set( REQUIREMENTS )
+set( REQUIREMENTS )
 set( WARNINGS_AS_ERRORS ON )
