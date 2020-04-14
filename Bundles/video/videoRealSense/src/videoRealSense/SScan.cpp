09d03722a5427b8947918ef23a036540935da923
Emilie Wernert <emilie.harquel@ihu-strasbourg.eu>
2020-04-14 09:28:46 +0200

    refactor(video): remove useless includes

--- Bundles/video/videoRealSense/src/videoRealSense/SScan.cpp
+++ Bundles/video/videoRealSense/src/videoRealSense/SScan.cpp
@@ -1,7 +1,7 @@
 /************************************************************************
  *
- * Copyright (C) 2019 IRCAD France
- * Copyright (C) 2019 IHU Strasbourg
+ * Copyright (C) 2019-2020 IRCAD France
+ * Copyright (C) 2019-2020 IHU Strasbourg
  *
  * This file is part of Sight.
  *
@@ -36,7 +36,6 @@
 #include <fwData/mt/ObjectWriteLock.hpp>
 #include <fwData/TransformationMatrix3D.hpp>
 
-#include <fwDataTools/helper/Image.hpp>
 #include <fwDataTools/helper/Mesh.hpp>
 
 #include <fwGui/dialog/LocationDialog.hpp>
