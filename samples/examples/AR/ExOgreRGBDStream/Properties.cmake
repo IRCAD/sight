33b5a93d7b48231dae9daf1c55ee7da8e51ef403
Romain MANCIAUX <romain.manciaux@ihu-strasbourg.eu>
2020-12-16 16:49:17 +0100

    refactor(graphics): remove material module from apps

--- samples/examples/AR/ExOgreRGBDStream/Properties.cmake
+++ samples/examples/AR/ExOgreRGBDStream/Properties.cmake
@@ -9,7 +9,6 @@ set( REQUIREMENTS
 
     preferences             # Start the module, load file location or window preferences
     visuOgre                # Start the module, allow to use fwRenderOgre
-    material                # Start the module, load Ogre's materials
     guiQt                   # Start the module, allow dark theme
     visuOgreQt              # Enable Ogre to render things in Qt window
     scene2D                 # Start the module, allow to use fwRenderQt
