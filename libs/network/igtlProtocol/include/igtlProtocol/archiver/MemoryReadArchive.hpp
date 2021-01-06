f814f11a7cd51148492d810dc303060db2752f6b
Alexandre Ancel <alexandre.ancel@ircad.fr>
2021-01-06 11:25:59 +0100

    fix(core): add missing header

--- libs/network/igtlProtocol/include/igtlProtocol/archiver/MemoryReadArchive.hpp
+++ libs/network/igtlProtocol/include/igtlProtocol/archiver/MemoryReadArchive.hpp
@@ -1,7 +1,7 @@
 /************************************************************************
  *
- * Copyright (C) 2009-2019 IRCAD France
- * Copyright (C) 2012-2019 IHU Strasbourg
+ * Copyright (C) 2009-2021 IRCAD France
+ * Copyright (C) 2012-2021 IHU Strasbourg
  *
  * This file is part of Sight.
  *
@@ -34,6 +34,7 @@
 #include <boost/iostreams/categories.hpp>
 
 #include <map>
+#include <vector>
 
 namespace igtlProtocol
 {
