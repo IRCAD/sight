d2ed172f6a05baa2e2e5d7aac6b8305db891aa80
Flavien Bridault <fbridault@ircad.fr>
2021-01-23 07:19:46 +0100

    refactor(core): move around some libraries

---
+++ libs/geometry/fwMath/test/tu/include/LineFunctionsTest.hpp
@@ -0,0 +1,56 @@
+/************************************************************************
+ *
+ * Copyright (C) 2009-2021 IRCAD France
+ * Copyright (C) 2012-2015 IHU Strasbourg
+ *
+ * This file is part of Sight.
+ *
+ * Sight is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sight. If not, see <https://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************/
+
+#pragma once
+
+#include <cppunit/extensions/HelperMacros.h>
+
+namespace fwMath
+{
+namespace ut
+{
+
+class LineFunctionsTest : public CPPUNIT_NS::TestFixture
+{
+private:
+    CPPUNIT_TEST_SUITE( LineFunctionsTest );
+    CPPUNIT_TEST( checkGetClosestPoint);
+    CPPUNIT_TEST( checkGetClosestPoints);
+    CPPUNIT_TEST( checkIntersect1);
+    CPPUNIT_TEST( checkIntersect2);
+    CPPUNIT_TEST( checkIntersect3);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+
+    void setUp();
+    void tearDown();
+
+    void checkGetClosestPoint();
+    void checkGetClosestPoints();
+    void checkIntersect1();
+    void checkIntersect2();
+    void checkIntersect3();
+};
+
+} //namespace ut
+} //namespace fwMath
---
+++ libs/geometry/fwMath/test/tu/include/MatrixFunctionsTest.hpp
@@ -0,0 +1,52 @@
+/************************************************************************
+ *
+ * Copyright (C) 2009-2021 IRCAD France
+ * Copyright (C) 2012-2015 IHU Strasbourg
+ *
+ * This file is part of Sight.
+ *
+ * Sight is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sight. If not, see <https://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************/
+
+#pragma once
+
+#include <cppunit/extensions/HelperMacros.h>
+
+namespace fwMath
+{
+namespace ut
+{
+
+class MatrixFunctionsTest : public CPPUNIT_NS::TestFixture
+{
+private:
+    CPPUNIT_TEST_SUITE( MatrixFunctionsTest );
+    CPPUNIT_TEST( checkMultVecMatrix);
+    CPPUNIT_TEST( checkRotationMatrix);
+    CPPUNIT_TEST( checkInverse);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+
+    void setUp();
+    void tearDown();
+
+    void checkMultVecMatrix();
+    void checkRotationMatrix();
+    void checkInverse();
+};
+
+} //namespace ut
+} //namespace fwMath
