40a5e59ea735b3d05d0cf87a234a6b9891658f01
Flavien Bridault <fbridault@ircad.fr>
2021-02-01 14:38:40 +0100

    refactor(build): move examples to the root folder

--- examples/core/ExTimeLine/include/ExTimeLine/Namespace.hpp
+++ examples/core/ExTimeLine/include/ExTimeLine/Namespace.hpp
@@ -1,6 +1,6 @@
 /************************************************************************
  *
- * Copyright (C) 2014-2020 IRCAD France
+ * Copyright (C) 2014-2021 IRCAD France
  * Copyright (C) 2014-2020 IHU Strasbourg
  *
  * This file is part of Sight.
