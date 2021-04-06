0fe743b5bd70913980bf99b24d56f4c80208e44f
Flavien Bridault <fbridault@ircad.fr>
2021-04-06 21:24:21 +0200

    fix(viz): improve negato activity layout and fix snapshot action

--- modules/ui/qt/metrics/Distance.cpp
+++ modules/ui/qt/metrics/Distance.cpp
@@ -32,6 +32,7 @@
 #include <data/Composite.hpp>
 #include <data/Image.hpp>
 #include <data/String.hpp>
+#include <data/fieldHelper/Image.hpp>
 
 #include <service/IService.hpp>
 #include <service/macros.hpp>
@@ -119,7 +120,7 @@ void Distance::onDistanceButton()
     SIGHT_ASSERT("'image' key is not found.", image);
 
     // force distance to be shown
-    image->setField("ShowDistances",  data::Boolean::New(true));
+    image->setField(data::fieldHelper::Image::m_distanceVisibility,  data::Boolean::New(true));
 
     m_sigDistanceRequested->asyncEmit();
 }
