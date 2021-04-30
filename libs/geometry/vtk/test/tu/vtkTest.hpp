fbaf08b850f7da572aa65ca2ed9c85d888961539
bMalgarini <benedicte.fahrer@ihu-strasbourg.eu>
2021-04-30 09:23:12 +0200

    feat(geometry): create geometry_vtk library
    - compute the mesh center of mass
    - create unit test

---
+++ libs/geometry/vtk/test/tu/vtkTest.hpp
@@ -0,0 +1,59 @@
+/************************************************************************
+ *
+ * Copyright (C) 2021 IRCAD France
+ * Copyright (C) 2017 IHU Strasbourg
+ *
+ * This file is part of Sight.
+ *
+ * Sight is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sight. If not, see <https://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************/
+
+#pragma once
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <algorithm>
+#include <cmath>
+
+namespace sight::geometry::vtk
+{
+namespace ut
+{
+
+class vtkTest : public CPPUNIT_NS::TestFixture
+{
+private:
+    CPPUNIT_TEST_SUITE( vtkTest );
+    CPPUNIT_TEST(computeCenterOfMass);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    // interface
+    void setUp();
+    void tearDown();
+
+    void computeCenterOfMass();
+
+    //------------------------------------------------------------------------------
+
+    template<class T>
+    static const T random(const T& a, const T& b)
+    {
+        return static_cast<T>(rand()) / static_cast<T>(RAND_MAX) * (b - a) + a;
+    }
+};
+
+} //namespace ut
+} //namespace sight::geometry::vtk
