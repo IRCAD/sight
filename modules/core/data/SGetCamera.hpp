e0f9c9d7ad5ed9832770bc55efc45f5ad199701e
Aimable MUZURI <aimable.muzuri@ircad.africa>
2022-03-01 15:08:35 +0100

    feat(core): new service sight::module::data::SGetCamera
    This new service extracts cameras and extrinsic matrices from a camera series.
    
    Closes #813

---
+++ modules/core/data/SGetCamera.hpp
@@ -0,0 +1,98 @@
+/************************************************************************
+ *
+ * Copyright (C) 2022 IRCAD France
+ *
+ * This file is part of Sight.
+ *
+ * Sight is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sight. If not, see <https://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************/
+
+#pragma once
+
+#include "modules/data/config.hpp"
+
+#include <core/base.hpp>
+
+#include <data/Camera.hpp>
+#include <data/CameraSeries.hpp>
+
+#include <service/IController.hpp>
+
+namespace sight::module::data
+{
+
+/**
+ * @brief This service extracts camera parameters from a camera series
+ *
+ *
+ * @section XML XML Configuration
+ *
+ * @code{.xml}
+   <service uid="..." type="sight::module::data::SGetCamera" >
+    <in key="cameraSeries" uid="..."/>
+    <out group="camera" >
+        <key index="0" uid="..." />
+        <key index="1" uid="..." />
+    </out>
+    <out group="extrinsic" >
+        <key index="1" uid="..." />
+    </out>
+   </service>
+       @endcode
+ * @subsection In In
+ * - \b cameraSeries [sight::data::SGetCamera]: camera series from which the parameters will be extracted
+ *
+ * @subsection Output Output
+ * - \b camera [sight::data::Camera]: output camera extracted from the input Camera series.
+ * - \b extrinsic [sight::data::Matrix4]: define the extrinsic matrix data.
+ */
+
+class MODULE_DATA_CLASS_API SGetCamera : public service::IController
+{
+public:
+
+    SIGHT_DECLARE_SERVICE(SGetCamera, service::IController);
+
+    /// Constructor
+    MODULE_DATA_API SGetCamera() = default;
+
+    /// Destructor
+    MODULE_DATA_API ~SGetCamera() = default;
+
+protected:
+
+    /// Does nothing
+    MODULE_DATA_API void starting() override;
+
+    /// Does nothing
+    MODULE_DATA_API void configuring() override;
+
+    /// Does nothing
+    MODULE_DATA_API void stopping() override;
+
+    /// Extract an image
+    MODULE_DATA_API void updating() override;
+
+private:
+
+    std::vector<std::size_t> m_cameraIndexNumbers;
+    std::vector<std::size_t> m_extrinsicIndexNumbers;
+
+    sight::data::ptr<sight::data::CameraSeries, sight::data::Access::in> m_cameraSeries {this, "cameraSeries"};
+    sight::data::ptr_vector<sight::data::Camera, sight::data::Access::out> m_camera {this, "camera"};
+    sight::data::ptr_vector<sight::data::Matrix4, sight::data::Access::out> m_extrinsic {this, "extrinsic"};
+};
+
+} // namespace sight::module::data
