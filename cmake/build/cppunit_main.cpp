434c7a6d125736ce703f8e9fcd0c4e0c73dacf62
Lucas SCHMIDT <lucas.schmidt@ircad.fr>
2022-10-14 11:39:32 +0200

    enh(ci): disable debug dialog on Windows on the CI
    When a unit test fails with an assertion failure in Windows Debug, a debug dialog will appear. This is useless in the CI, where physical access is required to view the dialog and know where is the problem. These changes remedy this, by disabling debug dialog and printing messages in the console when the environment variable DISABLE_ABORT_DIALOG is set.

--- cmake/build/cppunit_main.cpp
+++ cmake/build/cppunit_main.cpp
@@ -20,7 +20,12 @@
  *
  ***********************************************************************/
 
-// cspell:ignore NOLINT
+// cspell:ignore NOLINT SEM_NOGPFAULTERRORBOX
+
+#ifdef _WIN32
+#include <cstdlib>
+#include <windows.h>
+#endif
 
 #include <core/log/SpyLogger.hpp>
 #include <core/runtime/operations.hpp>
@@ -181,6 +186,23 @@ void shutdown_runtime()
 
 int main(int argc, char* argv[])
 {
+#ifdef _WIN32
+    // This allows to disable debug dialogs on Windows Debug during unit tests.
+    // It is especially useful in the CI, where these debug dialogs block the process and causes a
+    // timeout, and makes it impossible to find out the problem if physical access isn't possible.
+    // The messages are logged in the console instead.
+    if(const char* disableAbortDialog = std::getenv("DISABLE_ABORT_DIALOG"); disableAbortDialog != nullptr)
+    {
+        _set_error_mode(_OUT_TO_STDERR);
+        SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX);
+        _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
+        _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG);
+        _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
+        _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG);
+        _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
+    }
+#endif
+
     init_log_output();
     init_runtime();
 
