a949bb0b1ba16c02625e1bf4b7326750786594dc
Flavien BRIDAULT <fbridault@ircad.fr>
2023-10-11 15:40:00 +0200

    enh(viz): add an adaptor to display a background grid
    A new adaptor that displays the grid, with a customizable size, elevation and color is now available:
    
    ```xml
        <service uid="..." type="sight::module::viz::scene3d::adaptor::SGrid">
            <config transform="..." size="30" dashLength="2.5" color="#0000FF" elevation="200"/>
        </service>
    ```

---
+++ modules/viz/scene3d/adaptor/SGrid.cpp
@@ -0,0 +1,237 @@
+/************************************************************************
+ *
+ * Copyright (C) 2023 IRCAD France
+ *
+ * This file is part of Sight.
+ *
+ * Sight is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sight. If not, see <https://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************/
+
+#include "modules/viz/scene3d/adaptor/SGrid.hpp"
+
+#include "modules/viz/scene3d/adaptor/STransform.hpp"
+
+#include <core/com/Slots.hxx>
+
+#include <data/tools/Color.hpp>
+
+#include <viz/scene3d/helper/ManualObject.hpp>
+#include <viz/scene3d/ogre.hpp>
+
+#include <Ogre.h>
+#include <OgreAxisAlignedBox.h>
+
+namespace sight::module::viz::scene3d::adaptor
+{
+
+static const core::com::Slots::SlotKeyType s_UPDATE_LENGTH_SLOT = "updateLength";
+
+//-----------------------------------------------------------------------------
+
+SGrid::SGrid() noexcept
+{
+    newSlot(s_UPDATE_LENGTH_SLOT, &SGrid::updateSize, this);
+}
+
+//-----------------------------------------------------------------------------
+
+void SGrid::configuring()
+{
+    this->configureParams();
+
+    const ConfigType config = this->getConfiguration();
+
+    // parsing transform or create an "empty" one
+    this->setTransformId(
+        config.get<std::string>(
+            sight::viz::scene3d::ITransformable::s_TRANSFORM_CONFIG,
+            this->getID() + "_transform"
+        )
+    );
+
+    m_size = config.get<float>(s_CONFIG + "size", m_size);
+
+    const std::string color = config.get(s_CONFIG + "color", "#FFFFFF");
+    std::array<std::uint8_t, 4> rgba {};
+    data::tools::Color::hexaStringToRGBA(color, rgba);
+    m_color.r = static_cast<float>(rgba[0]) / 255.F;
+    m_color.g = static_cast<float>(rgba[1]) / 255.F;
+    m_color.b = static_cast<float>(rgba[2]) / 255.F;
+    m_color.a = static_cast<float>(rgba[3]) / 255.F;
+
+    m_dashLength = config.get(s_CONFIG + "dashLength", m_dashLength);
+    m_step       = config.get(s_CONFIG + "step", m_step);
+    m_elevation  = config.get(s_CONFIG + "elevation", m_elevation);
+}
+
+//-----------------------------------------------------------------------------
+
+void SGrid::starting()
+{
+    this->initialize();
+    this->getRenderService()->makeCurrent();
+
+    Ogre::SceneManager* sceneMgr = this->getSceneManager();
+
+    m_line = sceneMgr->createManualObject(this->getID() + "_grid");
+    // Set the line as dynamic, so we can update it later on, when the length changes
+    m_line->setDynamic(true);
+
+    // Set the material
+    m_material = data::Material::New();
+
+    m_materialAdaptor = this->registerService<module::viz::scene3d::adaptor::SMaterial>(
+        "sight::module::viz::scene3d::adaptor::SMaterial"
+    );
+    m_materialAdaptor->setInOut(m_material, module::viz::scene3d::adaptor::SMaterial::s_MATERIAL_INOUT, true);
+    m_materialAdaptor->configure(
+        this->getID() + m_materialAdaptor->getID(),
+        this->getID() + m_materialAdaptor->getID(),
+        this->getRenderService(),
+        m_layerID,
+        "ambient"
+    );
+    m_materialAdaptor->start();
+
+    m_materialAdaptor->getMaterialFw()->setHasVertexColor(true);
+    m_materialAdaptor->update();
+
+    // Draw the line
+    this->drawGrid(false);
+
+    this->attachNode(m_line);
+
+    this->setVisible(m_isVisible);
+}
+
+//-----------------------------------------------------------------------------
+
+void SGrid::updating()
+{
+    if(m_isVisible)
+    {
+        this->getRenderService()->makeCurrent();
+        // Draw
+        this->drawGrid(true);
+    }
+
+    this->requestRender();
+}
+
+//-----------------------------------------------------------------------------
+
+void SGrid::stopping()
+{
+    this->getRenderService()->makeCurrent();
+    this->unregisterServices();
+    m_material = nullptr;
+    if(m_line != nullptr)
+    {
+        m_line->detachFromParent();
+        this->getSceneManager()->destroyManualObject(m_line);
+        m_line = nullptr;
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+void SGrid::attachNode(Ogre::MovableObject* object)
+{
+    Ogre::SceneNode* rootSceneNode = this->getSceneManager()->getRootSceneNode();
+    Ogre::SceneNode* transNode     = this->getOrCreateTransformNode(rootSceneNode);
+    SIGHT_ASSERT("Transform node shouldn't be null", transNode);
+
+    transNode->setVisible(m_isVisible);
+    transNode->attachObject(object);
+}
+
+//-----------------------------------------------------------------------------
+
+void SGrid::drawGrid(bool _existingLine)
+{
+    if(!_existingLine)
+    {
+        m_line->begin(
+            m_materialAdaptor->getMaterialName(),
+            Ogre::RenderOperation::OT_LINE_LIST,
+            sight::viz::scene3d::RESOURCE_GROUP
+        );
+    }
+    else
+    {
+        m_line->beginUpdate(0);
+    }
+
+    const auto halfSize = m_size * 0.5F;
+    const auto stepSize = m_size / static_cast<float>(m_step);
+
+    float x = -halfSize;
+    for(std::size_t i = 0 ; i <= m_step ; ++i)
+    {
+        sight::viz::scene3d::helper::ManualObject::drawDashedLine(
+            m_line,
+            Ogre::Vector3(x, m_elevation, -halfSize),
+            Ogre::Vector3(x, m_elevation, halfSize),
+            m_dashLength,
+            m_dashLength,
+            m_color
+        );
+        x += stepSize;
+    }
+
+    float y = -halfSize;
+    for(std::size_t i = 0 ; i <= m_step ; ++i)
+    {
+        sight::viz::scene3d::helper::ManualObject::drawDashedLine(
+            m_line,
+            Ogre::Vector3(-halfSize, m_elevation, y),
+            Ogre::Vector3(halfSize, m_elevation, y),
+            m_dashLength,
+            m_dashLength,
+            m_color
+        );
+        y += stepSize;
+    }
+
+    m_line->end();
+
+    // Set the bounding box
+    Ogre::Vector3 bbMin(-halfSize, 0.F, -halfSize);
+    Ogre::Vector3 bbMax(halfSize, 0.F, halfSize);
+    Ogre::AxisAlignedBox box(bbMin, bbMax);
+    m_line->setBoundingBox(box);
+}
+
+//-----------------------------------------------------------------------------
+
+void SGrid::setVisible(bool /*_visible*/)
+{
+    Ogre::SceneNode* rootSceneNode = this->getSceneManager()->getRootSceneNode();
+    Ogre::SceneNode* transNode     = this->getOrCreateTransformNode(rootSceneNode);
+    transNode->setVisible(m_isVisible);
+    this->updating();
+}
+
+//-----------------------------------------------------------------------------
+
+void SGrid::updateSize(float _size)
+{
+    m_size = _size;
+    this->updating();
+}
+
+//-----------------------------------------------------------------------------
+
+} // namespace sight::module::viz::scene3d::adaptor.
---
+++ modules/viz/scene3d/adaptor/SGrid.hpp
@@ -0,0 +1,136 @@
+/************************************************************************
+ *
+ * Copyright (C) 2023 IRCAD France
+ *
+ * This file is part of Sight.
+ *
+ * Sight is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sight. If not, see <https://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************/
+
+#pragma once
+
+#include "modules/viz/scene3d/adaptor/SMaterial.hpp"
+#include "modules/viz/scene3d/config.hpp"
+
+#include <viz/scene3d/IAdaptor.hpp>
+#include <viz/scene3d/ITransformable.hpp>
+
+#include <Ogre.h>
+
+namespace sight::module::viz::scene3d::adaptor
+{
+
+/**
+ * @brief This adaptor shows a grid that is intended to provide a horizon.
+ *
+ * @section Slots Slots
+ * - \b updateVisibility(bool): Sets whether the line is shown or not.
+ * - \b toggleVisibility(): Toggle whether the line is shown or not.
+ * - \b show(): shows the line.
+ * - \b hide(): hides the line.
+ * - \b updateSize(float): Update the size of the grid
+ *
+ * @section XML XML Configuration
+ * @code{.xml}
+    <service uid="..." type="sight::module::viz::scene3d::adaptor::SGrid">
+        <config transform="transformUID" size="30" dashLength="2.5" color="#0000FF" elevation="200"/>
+    </service>
+   @endcode
+ *
+ * @subsection Configuration Configuration:
+ * - \b transform (optional, string, default=""): the name of the Ogre transform node where to attach the mesh, as it
+ *      was specified in the STransform adaptor
+ * - \b size (optional, float, default=50.0): size of a side of the grid, in mm.
+ * - \b dashLength (optional, float, default=2.5): length of a dash, in mm.
+ * - \b color (optional, hexadecimal, default=#FFFFFFFF): color of the grid lines.
+ * - \b visible (optional, bool, default=true): the visibility of the adaptor.
+ */
+class MODULE_VIZ_SCENE3D_CLASS_API SGrid final :
+    public sight::viz::scene3d::IAdaptor,
+    public sight::viz::scene3d::ITransformable
+{
+public:
+
+    SIGHT_DECLARE_SERVICE(SGrid, sight::viz::scene3d::IAdaptor);
+
+    MODULE_VIZ_SCENE3D_API SGrid() noexcept;
+    MODULE_VIZ_SCENE3D_API ~SGrid() noexcept override = default;
+
+protected:
+
+    /// Configures the adaptor
+    MODULE_VIZ_SCENE3D_API void configuring() override;
+
+    /// Creates a mesh in the Default Ogre resource group
+    MODULE_VIZ_SCENE3D_API void starting() override;
+
+    /// Checks if the data::Mesh has changed, and updates it
+    MODULE_VIZ_SCENE3D_API void updating() override;
+
+    /// Deletes the mesh after unregistering the service, and shutting down connections
+    MODULE_VIZ_SCENE3D_API void stopping() override;
+
+    /**
+     * @brief Sets the line visibility.
+     * @param _visible the visibility status of the line.
+     */
+    MODULE_VIZ_SCENE3D_API void setVisible(bool _visible) override;
+
+private:
+
+    /**
+     * @brief Attaches a node in the scene graph.
+     * @param _node node to attach.
+     */
+    void attachNode(Ogre::MovableObject* object);
+
+    /**
+     * @brief Draws the grid.
+     * @param _existingLine use true if the line already exists.
+     */
+    void drawGrid(bool _existingLine);
+
+    /**
+     * @brief SLOT: updates the size of the grid.
+     * @param _size size of one side of the grid.
+     */
+    void updateSize(float _size);
+
+    /// Contains the Ogre material adaptor.
+    module::viz::scene3d::adaptor::SMaterial::sptr m_materialAdaptor {nullptr};
+
+    /// Contains the material data.
+    data::Material::sptr m_material {nullptr};
+
+    /// Contains the manual object of the line.
+    Ogre::ManualObject* m_line {nullptr};
+
+    /// Defines the size of a side of the grid, in mm.
+    float m_size {50.F};
+
+    /// Number of lines in each direction (x,z) of the grid
+    std::size_t m_step {10};
+
+    /// Elevation of the grid in the Y axis, in mm.
+    float m_elevation {-500.F};
+
+    /// Defines the color of the line.
+    Ogre::ColourValue m_color;
+
+    /// Defines the length of one dash, in mm.
+    float m_dashLength {2.5F};
+};
+
+} // namespace sight::module::viz::scene3d::adaptor.
