40d677e3d2680a4af8436ea3ef5f6e6bdeed4cb6
luis MENDOZA <luis.mendoza@ircad.fr>
2024-10-15 10:23:10 +0200

    feat(core): add camera parser for default video path
    - new camera parser that uses either sight resources path or an absolute file system path to default the camera to video file playing
    - update ex_video_recorder to use that parser
    feat(test): unitary test of camera parser

---
+++ libs/__/app/test/ut/parser/camera_test.hpp
@@ -0,0 +1,42 @@
+/************************************************************************
+ *
+ * Copyright (C) 2024 IRCAD France
+ *
+ * This file is part of Sight.
+ *
+ * Sight is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sight. If not, see <https://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************/
+
+#pragma once
+
+#include <cppunit/extensions/HelperMacros.h>
+
+namespace sight::app::parser::ut
+{
+
+class camera_test : public CPPUNIT_NS::TestFixture
+{
+CPPUNIT_TEST_SUITE(camera_test);
+CPPUNIT_TEST(resource_test);
+CPPUNIT_TEST(file_test);
+CPPUNIT_TEST_SUITE_END();
+
+public:
+
+    static void resource_test();
+    static void file_test();
+};
+
+} // namespace sight::app::parser::ut
