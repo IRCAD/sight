IRCAD/sight

commit c7a5a36919878bc46ecaac00c383b3783e3125af

enh(build): migrate most linux build jobs to Ubuntu 24.04

libs/viz/scene3d/detail/resource_manager.hpp
@@ -107,8 +107,8 @@ private:
     {
         std::shared_ptr<RESOURCE> resource;
         CWPTR(OBJECT) object;
-        std::size_t use_count;
-        std::uint64_t last_modified;
+        std::size_t use_count {0};
+        std::uint64_t last_modified {0UL};
         typename LOADER::return_t loading_result {0}; // Extra attribute to store resource-specific loading data
     };
 
@@ -149,7 +149,7 @@ std::shared_ptr<RESOURCE> resource_manager<OBJECT, RESOURCE, LOADER>::instantiat
         true
     );
 
-    m_registry[full_id] = {resource, _object, 1, ~0UL, {}};
+    m_registry[full_id] = {.resource = resource, .object = _object, .use_count = 1};
 
     return resource;
 }

.gitlab-ci.yml
@@ -16,13 +16,6 @@ include:
 variables:
     TEST_DATA_REF: "dev"
 
-lint:sheldon:
-    extends: .lint
-    variables:
-        ENABLE_CLANG_TIDY: "ON"
-    tags:
-        - linux-power-runner
-
 lint:ubuntu-24.04:
     extends: .lint
     image: "${SIGHT_UBUNTU_24_IMAGE}"
@@ -35,7 +28,7 @@ lint:ubuntu-24.04:
     extends: .linux_before
     stage: build
     needs:
-        - job: lint:sheldon
+        - job: lint:ubuntu-24.04
           optional: true
     variables:
         SIGHT_BUILD_DOC: "OFF"
@@ -129,7 +122,7 @@ lint:ubuntu-24.04:
             metrics: .build/metrics.txt
 
 .linux_deploy_test:
-    image: "${SIGHT_UBUNTU_22_IMAGE}"
+    image: "${SIGHT_UBUNTU_24_IMAGE}"
     stage: deploy-test
     variables:
         BUILD_TYPE: "Debug"
@@ -150,28 +143,15 @@ lint:ubuntu-24.04:
         - >
             if [ "${SIGHT_TEST_SDK}" == "ON" ]; then
               tar -xf packages/sight-*.tar.zst
-              PACKAGE_NAME=$(cmake -DGET_ARCHIVE_FOLDER=ON -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -P "${CI_PROJECT_DIR}/cmake/build/download_deps.cmake" 2>&1)
               for program in sight-*/bin/{ex*,tuto*,sight_viewer,sight_calibrator,dicomxplorer}; do
                   echo Testing $(basename $program):
-                  LD_LIBRARY_PATH=/cache/.sight-deps/$PACKAGE_NAME/lib timeout 5s xvfb-run -a $program || [ $? -eq 124 ]
+                  timeout 5s xvfb-run -a $program || [ $? -eq 124 ]
               done
               for program in sight-*/bin/{aruco_marker,dicom_anonymizer,network_proxy}; do
                   echo Testing $(basename $program):
                   $program --help
               done
             fi
-
-build:linux-22.04-debug-gcc:
-    extends: .linux_build
-    variables:
-        BUILD_TYPE: "Debug"
-        SIGHT_BUILD_SDK: "ON"
-        SIGHT_ENABLE_GDB: "ON"
-        CC: "gcc"
-        CXX: "g++"
-    rules:
-        - if: $CI_PIPELINE_SOURCE == 'merge_request_event' && $CI_MERGE_REQUEST_TITLE !~ "/^Draft.*/"
-        - if: $CI_COMMIT_BRANCH == "master" || $CI_COMMIT_BRANCH == "dev"
 
 build:ubuntu-24.04-debug-gcc:
     extends: .linux_build
@@ -186,8 +166,9 @@ build:ubuntu-24.04-debug-gcc:
         - if: $CI_PIPELINE_SOURCE == 'merge_request_event' && $CI_MERGE_REQUEST_TITLE !~ "/^Draft.*/"
         - if: $CI_COMMIT_BRANCH == "master" || $CI_COMMIT_BRANCH == "dev"
 
-build:linux-22.04-release-gcc:
+build:ubuntu-24.04-release-gcc:
     extends: .linux_build
+    image: "${SIGHT_UBUNTU_24_IMAGE}"
     variables:
         BUILD_TYPE: "Release"
         SIGHT_BUILD_SDK: "ON"
@@ -212,8 +193,21 @@ build:ubuntu-24.04-RelWithDebInfo-clang:
         - if: $CI_PIPELINE_SOURCE == 'merge_request_event' && $CI_MERGE_REQUEST_TITLE !~ "/^Draft.*/"
         - if: $CI_COMMIT_BRANCH == "master" || $CI_COMMIT_BRANCH == "dev"
 
-build:linux-22.04-coverage-gcc:
+build:ubuntu-22.04-RelWithDebInfo-clang:
     extends: .linux_build
+    image: "${SIGHT_UBUNTU_22_IMAGE}"
+    variables:
+        BUILD_TYPE: "RelWithDebInfo"
+        SIGHT_IGNORE_UNSTABLE_TESTS: 1
+        CC: "clang"
+        CXX: "clang++"
+    rules:
+        - if: $CI_PIPELINE_SOURCE == 'merge_request_event' && $CI_MERGE_REQUEST_TITLE !~ "/^Draft.*/"
+        - if: $CI_COMMIT_BRANCH == "master" || $CI_COMMIT_BRANCH == "dev"
+
+build:ubuntu-24.04-coverage-gcc:
+    extends: .linux_build
+    image: "${SIGHT_UBUNTU_24_IMAGE}"
     variables:
         BUILD_TYPE: "RelWithDebInfo"
         SIGHT_BUILD_DOC: "ON"
@@ -225,8 +219,9 @@ build:linux-22.04-coverage-gcc:
         - if: $CI_PIPELINE_SOURCE == 'merge_request_event' && $CI_MERGE_REQUEST_TITLE !~ "/^Draft.*/"
         - if: $CI_COMMIT_BRANCH == "master" || $CI_COMMIT_BRANCH == "dev"
 
-build:linux-22.04-coverage-gcc-draft:
+build:ubuntu-24.04-coverage-gcc-draft:
     extends: .linux_build
+    image: "${SIGHT_UBUNTU_24_IMAGE}"
     variables:
         BUILD_TYPE: "RelWithDebInfo"
         SIGHT_ENABLE_COVERAGE: "ON"
@@ -239,7 +234,7 @@ build:linux-22.04-coverage-gcc-draft:
 deploy-test:linux-debug:
     extends: .linux_deploy_test
     needs:
-        - job: build:linux-22.04-debug-gcc
+        - job: build:ubuntu-24.04-debug-gcc
           artifacts: true
     variables:
         BUILD_TYPE: "Debug"
@@ -250,8 +245,9 @@ deploy-test:linux-debug:
 
 deploy:linux-debug:
     extends: .linux_deploy
+    image: "${SIGHT_UBUNTU_24_IMAGE}"
     needs:
-        - job: build:linux-22.04-debug-gcc
+        - job: build:ubuntu-24.04-debug-gcc
           artifacts: true
         - job: deploy-test:linux-debug
     variables:
@@ -263,7 +259,7 @@ deploy:linux-debug:
 deploy-test:linux-release:
     extends: .linux_deploy_test
     needs:
-        - job: build:linux-22.04-release-gcc
+        - job: build:ubuntu-24.04-release-gcc
           artifacts: true
     variables:
         BUILD_TYPE: "Release"
@@ -275,8 +271,9 @@ deploy-test:linux-release:
 
 deploy:linux-release:
     extends: .linux_deploy
+    image: "${SIGHT_UBUNTU_24_IMAGE}"
     needs:
-        - job: build:linux-22.04-release-gcc
+        - job: build:ubuntu-24.04-release-gcc
           artifacts: true
         - job: deploy-test:linux-release
     variables:
@@ -287,8 +284,9 @@ deploy:linux-release:
 
 deploy:apps-linux-release:
     extends: .linux_deploy
+    image: "${SIGHT_UBUNTU_24_IMAGE}"
     needs:
-        - job: build:linux-22.04-release-gcc
+        - job: build:ubuntu-24.04-release-gcc
           artifacts: true
         - job: deploy-test:linux-release
     variables:
@@ -300,7 +298,7 @@ deploy:apps-linux-release:
 .windows_build:
     extends: .windows_before
     needs:
-        - job: lint:sheldon
+        - job: lint:ubuntu-24.04
           optional: true
         - job: prebuild:windows
     variables:
@@ -504,10 +502,10 @@ deploy:apps-windows-release:
         - if: $CI_COMMIT_BRANCH == "master" || $CI_COMMIT_BRANCH == "dev"
 
 pages:
-    image: "${SIGHT_UBUNTU_22_IMAGE}"
+    image: "${SIGHT_UBUNTU_24_IMAGE}"
     stage: deploy
     needs:
-        - job: build:linux-22.04-coverage-gcc
+        - job: build:ubuntu-24.04-coverage-gcc
           artifacts: true
     script:
         - mv .build/Documentation/Doxygen/html/ public/

cmake/build/macros.cmake
@@ -1123,16 +1123,24 @@ macro(fw_manage_warnings PROJECT)
         ${PROJECT} PRIVATE "$<$<CXX_COMPILER_ID:GNU,Clang>:-Werror;-Wno-error=deprecated-declarations>"
     )
 
-    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 12
-       AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 13
-    )
-        # disable specific buggy warnings with GCC12, see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105329
-        target_compile_options(
-            ${PROJECT}
-            PRIVATE
-                "$<$<CONFIG:Release,RelWithDebInfo,MinSizeRel>:-Wno-restrict;-Wno-stringop-overflow;-Wno-array-bounds>"
-        )
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 12)
+            # disable specific buggy warnings with GCC12, see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105329
+            target_compile_options(
+                ${PROJECT} PRIVATE "$<$<CONFIG:Release,RelWithDebInfo,MinSizeRel>:-Wno-restrict;-Wno-stringop-overflow;"
+                                   "-Wno-array-bounds>"
+            )
+        endif()
+
+        if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 13)
+            # disable specific buggy warnings with GCC12, see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105329
+            target_compile_options(
+                ${PROJECT} PRIVATE "$<$<CONFIG:Release,RelWithDebInfo,MinSizeRel>:"
+                                   "-Wno-stringop-overread;-Wno-error=nonnull>"
+            )
+        endif()
     endif()
+
 endmacro()
 
 # Find link and manual dependencies for a target

libs/__/data/detail/series_impl.cpp
@@ -1,0 +1,62 @@
+/************************************************************************
+ *
+ * Copyright (C) 2024 IRCAD France
+ *
+ * This file is part of Sight.
+ *
+ * Sight is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sight. If not, see <https://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************/
+
+#include "series_impl.hpp"
+
+namespace sight::data::detail
+{
+
+//------------------------------------------------------------------------------
+
+std::string shrink(const std::string& _source)
+{
+    std::string result(_source);
+    result.erase(result.find_last_not_of(NULL_PADDING_CHAR) + 1);
+    return result;
+}
+
+//------------------------------------------------------------------------------
+
+std::optional<std::string> get_private_string_value(const gdcm::DataSet& _dataset, const gdcm::Tag& _tag)
+{
+    if(!_dataset.FindDataElement(_tag))
+    {
+        return std::nullopt;
+    }
+
+    const auto& data_element = _dataset.GetDataElement(_tag);
+
+    if(data_element.IsEmpty())
+    {
+        return std::nullopt;
+    }
+
+    const auto* byte_value = data_element.GetByteValue();
+
+    if(byte_value == nullptr || byte_value->GetPointer() == nullptr)
+    {
+        return std::nullopt;
+    }
+
+    return shrink(gdcm::String<>(byte_value->GetPointer(), byte_value->GetLength()).Trim());
+}
+
+} // namespace sight::data::detail

libs/__/data/detail/series_impl.hpp
@@ -1,0 +1,59 @@
+/************************************************************************
+ *
+ * Copyright (C) 2024 IRCAD France
+ *
+ * This file is part of Sight.
+ *
+ * Sight is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sight. If not, see <https://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************/
+
+#pragma once
+
+#include  <optional>
+
+#ifdef WIN32
+#pragma warning( push )
+#pragma warning( disable : 4702) // warning C4702: unreachable code
+#endif
+
+#include <gdcmDataSet.h>
+
+#ifdef WIN32
+#pragma warning( pop )
+#endif
+
+namespace sight::data::detail
+{
+
+static constexpr char SPACE_PADDING_CHAR  = ' ';
+static constexpr char NULL_PADDING_CHAR   = '\0';
+static constexpr auto BACKSLASH_SEPARATOR = "\\";
+
+/// @see https://dicom.nema.org/medical/dicom/current/output/html/part05.html#sect_7.8
+static constexpr std::uint16_t PRIVATE_GROUP {0x0099};
+static constexpr std::uint16_t PRIVATE_CREATOR_ELEMENT {0x0099};
+static constexpr std::uint16_t PRIVATE_DATA_ELEMENT {0x9910};
+static const std::string PRIVATE_CREATOR {"Sight"};
+
+/// Remove the trailing padding \0 characters from a string.
+/// @param[in] _source The string to be trimmed.
+/// @return The trimmed string.
+std::string shrink(const std::string& _source);
+
+//------------------------------------------------------------------------------
+
+std::optional<std::string> get_private_string_value(const gdcm::DataSet& _dataset, const gdcm::Tag& _tag);
+
+} // sight::data::detail

libs/__/data/detail/series_impl.hxx
@@ -23,6 +23,7 @@
 
 #include "data/series.hpp"
 
+#include "series_impl.hpp"
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/join.hpp>
 #include <boost/algorithm/string/split.hpp>
@@ -59,16 +60,6 @@ namespace sight::data::detail
 using series_dataset = std::pair<gdcm::DataSet, std::filesystem::path>;
 using frame_datasets = std::vector<series_dataset>;
 
-static constexpr char SPACE_PADDING_CHAR  = ' ';
-static constexpr char NULL_PADDING_CHAR   = '\0';
-static constexpr auto BACKSLASH_SEPARATOR = "\\";
-
-/// @see https://dicom.nema.org/medical/dicom/current/output/html/part05.html#sect_7.8
-static constexpr std::uint16_t PRIVATE_GROUP {0x0099};
-static constexpr std::uint16_t PRIVATE_CREATOR_ELEMENT {0x0099};
-static constexpr std::uint16_t PRIVATE_DATA_ELEMENT {0x9910};
-static const std::string PRIVATE_CREATOR {"Sight"};
-
 /// Allows to check if a VM is fixed (NOT 1..n). Since GDCM Attribute API differs if VM is fixed or not, we need to
 /// check to choose the right gdcm::Attributes::SetValues() version.
 /// @{
@@ -93,16 +84,6 @@ struct has_fixed_multiplicity<
     >
 >: std::false_type {};
 /// @}
-
-/// Remove the trailing padding \0 characters from a string.
-/// @param[in] _source The string to be trimmed.
-/// @return The trimmed string.
-static inline std::string shrink(const std::string& _source)
-{
-    std::string result(_source);
-    result.erase(result.find_last_not_of(NULL_PADDING_CHAR) + 1);
-    return result;
-}
 
 /// Returns the maximum or fixed size of a Value Representation and its padding character.
 /// @note the data come from https://dicom.nema.org/medical/dicom/current/output/chtml/part05/sect_6.2.html
@@ -230,32 +211,6 @@ static inline std::string arithmetic_to_string(const V& _value, gdcm::VR::VRType
     }
 
     return oss.str();
-}
-
-//------------------------------------------------------------------------------
-
-inline static std::optional<std::string> get_private_string_value(const gdcm::DataSet& _dataset, const gdcm::Tag& _tag)
-{
-    if(!_dataset.FindDataElement(_tag))
-    {
-        return std::nullopt;
-    }
-
-    const auto& data_element = _dataset.GetDataElement(_tag);
-
-    if(data_element.IsEmpty())
-    {
-        return std::nullopt;
-    }
-
-    const auto* byte_value = data_element.GetByteValue();
-
-    if(byte_value == nullptr || byte_value->GetPointer() == nullptr)
-    {
-        return std::nullopt;
-    }
-
-    return shrink(gdcm::String<>(byte_value->GetPointer(), byte_value->GetLength()).Trim());
 }
 
 //------------------------------------------------------------------------------

commit 9e68374bf4f0260e97df6aa438647ff6e64e6d8d

fix(viz): wrong initialisation value in resource manager
This fixes a regression introduced in c7a5a36919878bc46ecaac00c383b3783e3125af and prevented some graphic resources to be updated properly.

libs/viz/scene3d/detail/resource_manager.hpp
@@ -108,7 +108,7 @@ private:
         std::shared_ptr<RESOURCE> resource;
         CWPTR(OBJECT) object;
         std::size_t use_count {0};
-        std::uint64_t last_modified {0UL};
+        std::uint64_t last_modified {~0UL};
         typename LOADER::return_t loading_result {0}; // Extra attribute to store resource-specific loading data
     };
 