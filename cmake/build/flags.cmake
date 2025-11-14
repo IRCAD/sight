aa7a2645cee9fb4884555fb3ffa2db521c364bfb
Didier Weckmann <didier.weckmann@ircad.fr>
2025-11-14 11:09:46 +0100

    enh(ci): use variables to limit the number of parallel build and test jobs

--- cmake/build/flags.cmake
+++ cmake/build/flags.cmake
@@ -260,7 +260,7 @@ if(NOT TARGET coverage)
 
         string(
             CONCAT coverage_script
-                   "gcovr -j$RUNNER_THREADS -r .. --filter ../lib --filter ../module --exclude '.*test.*' "
+                   "gcovr -j $LINUX_PARALLEL_BUILDS -r .. --filter ../lib --filter ../module --exclude '.*test.*' "
                    "--exclude-noncode-lines --html --html-details coverage/index.html --html-theme github.dark-blue "
                    "--xml coverage/cobertura-coverage.xml --print-summary ."
                    "| grep lines"
