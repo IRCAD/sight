bc6aae97713425966968dfbc9f91bc68aedd8497
luis MENDOZA <luis.mendoza@ircad.fr>
2025-12-10 11:28:45 +0100

    fix(build): ignore suspicious hits coming from multi-threaded tests

--- cmake/build/flags.cmake
+++ cmake/build/flags.cmake
@@ -262,7 +262,9 @@ if(NOT TARGET coverage)
             CONCAT coverage_script
                    "gcovr -j $LINUX_PARALLEL_BUILDS -r .. --filter ../lib --filter ../module --exclude '.*test.*' "
                    "--exclude-noncode-lines --html --html-details coverage/index.html --html-theme github.dark-blue "
-                   "--xml coverage/cobertura-coverage.xml --print-summary ."
+                   "--xml coverage/cobertura-coverage.xml --print-summary "
+                   # suspicious hits are sometimes generated from big multithreaded tests
+                   "--gcov-ignore-parse-errors=suspicious_hits.warn ."
                    "| grep lines"
                    [=[| sed -sE 's/.* \((.*) out of (.*)\)/\1\/\2/']=]
                    "| xargs -i echo 'scale=4;a={}*100;scale=2;a/1'"
